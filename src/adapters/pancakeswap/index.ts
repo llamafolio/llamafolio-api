e18b996e32029e45b25b44ccf902584d1e43b302
0xsign <0xsign@protonmail.com>
2022-08-04 16:26:55 +0200

    pancakeswap pools

--- src/adapters/pancakeswap/index.ts
+++ src/adapters/pancakeswap/index.ts
@@ -1,20 +1,37 @@
 import { Adapter } from "@lib/adapter";
+import { getERC20Balances } from "@lib/erc20";
+import { getPairsInfo } from "@lib/uniswap/v2/factory";
 
 const adapter: Adapter = {
   name: "PancakeSwap",
   description: "",
+  defillama: "pancakeswap",
+  coingecko: "pancakeswap-token",
   links: {
     website: "https://pancakeswap.finance/",
+    doc: "https://docs.pancakeswap.finance/",
   },
   async getContracts() {
     return {
-      contracts: [],
-      revalidate: 60 * 60, // 1 hour
+      contracts: await getPairsInfo({
+        chain: "bsc",
+        factoryAddress: "0xca143ce32fe78f1f7019d7d551a6402fc5350c73",
+      }),
+      revalidate: 60 * 60,
     };
   },
-  async getBalances(ctx) {
+  async getBalances(ctx, contracts) {
+    const balances = await getERC20Balances(
+      ctx,
+      "bsc",
+      contracts.map((c) => c.address)
+    );
+
     return {
-      balances: [],
+      balances: balances.map((balance) => ({
+        ...balance,
+        category: "farm",
+      })),
     };
   },
 };
