951b476191768b988eac23e1dc58630cb26a36d9
charlie-eth <charlie_eth+main@protonmail.ch>
2022-08-05 16:00:30 +0100

    general: add categories in lib

---
+++ src/lib/category.ts
@@ -0,0 +1,47 @@
+export const CATEGORIES = [
+  "wallet": {
+    id: 0,
+    category: "wallet",
+    description: "Assets being held in wallet"
+  },
+  "lend": {
+    id: 1,
+    category: "lend",
+    description: "Assets being lent on a protocol"
+  },
+  "borrow": {
+    id: 2,
+    category: "borrow",
+    description: "Assets borrowed from a protocol"
+  },
+  "stake": {
+    id: 3,
+    category: "stake",
+    description: "Assets staked on a protocol to generate yield and rewards"
+  },
+  "vest": {
+    id: 4,
+    category: "vest",
+    description: "Assets being vested from a protocol "
+  },
+  "lock": {
+    id: 5,
+    category: "lock",
+    description: "Assets locked in a protocol"
+  },
+  "lp": {
+    id: 6,
+    category: "lp",
+    description: "Assets being used to provide liquidity to a protocol"
+  },
+  "lp-stable": {
+    id: 7,
+    category: "lp-stable",
+    description: "Assets being used to provide liquidity to a protocol on pool with minimal or no impermanent loss"
+  },
+  "farm": {
+    id: 8,
+    category: "farm",
+    description: "Assets used to yield rewards on a protocol"
+  },
+]
