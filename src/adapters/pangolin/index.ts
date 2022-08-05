afce7d6c661383e55450a1ffdfb397d1cebbd5ea
0xsign <0xsign@protonmail.com>
2022-08-05 17:37:17 +0200

    pangolin pools

---
+++ src/adapters/pangolin/index.ts
@@ -0,0 +1,39 @@
+import { Adapter } from "@lib/adapter";
+import { getERC20Balances } from "@lib/erc20";
+import { getPairsInfo } from "@lib/uniswap/v2/factory";
+
+const adapter: Adapter = {
+  name: "Pangolin",
+  description: "Pangolin is a community-driven DEX that runs on Avalanche.",
+  coingecko: "pangolin",
+  defillama: "pangolin",
+  links: {
+    website: "https://pangolin.exchange",
+    doc: "https://docs.pangolin.exchange",
+  },
+  async getContracts() {
+    return {
+      contracts: await getPairsInfo({
+        chain: "avax",
+        factoryAddress: "0xefa94DE7a4656D787667C749f7E1223D71E9FD88",
+      }),
+      revalidate: 60 * 60,
+    };
+  },
+  async getBalances(ctx, contracts) {
+    const balances = await getERC20Balances(
+      ctx,
+      "avax",
+      contracts.map((c) => c.address)
+    );
+
+    return {
+      balances: balances.map((balance) => ({
+        ...balance,
+        category: "farm",
+      })),
+    };
+  },
+};
+
+export default adapter;
