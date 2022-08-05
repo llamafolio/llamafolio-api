5bf714421e22c9e7482ae59195a15106c4155aa7
0xsign <0xsign@protonmail.com>
2022-08-05 17:18:46 +0200

    traderjoe pools

---
+++ src/adapters/traderjoe/index.ts
@@ -0,0 +1,39 @@
+import { Adapter } from "@lib/adapter";
+import { getERC20Balances } from "@lib/erc20";
+import { getPairsInfo } from "@lib/uniswap/v2/factory";
+
+const adapter: Adapter = {
+  name: "Trader Joe",
+  description: "One-stop-shop decentralized trading on Avalanche",
+  coingecko: "joe",
+  defillama: "trader-joe",
+  links: {
+    website: "https://traderjoexyz.com",
+    doc: "https://docs.traderjoexyz.com",
+  },
+  async getContracts() {
+    return {
+      contracts: await getPairsInfo({
+        chain: "avax",
+        factoryAddress: "0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10",
+      }),
+      revalidate: 60 * 60,
+    };
+  },
+  async getBalances(ctx, contracts) {
+    const balances = await getERC20Balances(
+      ctx,
+      "avax",
+      contracts.map((c) => c.address)
+    );
+
+    return {
+      balances: balances.map((balance) => ({
+        ...balance,
+        category: "farm",
+      })),
+    };
+  },
+};
+
+export default adapter;
