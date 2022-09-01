d5a8da916706e79a6e044fa6471315c382467cf9
charlie-eth <charlie_eth+main@protonmail.ch>
2022-09-02 00:12:54 +0100

    add blank template

---
+++ src/adapters/aatemplate/abis/index.js
@@ -0,0 +1,39 @@
+import { Adapter, Balance, Contract } from "@lib/adapter";
+
+//example contract object
+const contract: Contract = {
+  name: "",
+  displayName: "",
+  chain: "ethereum",
+  address: "0x3cf54f3a1969be9916dad548f3c084331c4450b5",
+};
+
+const adapter: Adapter = {
+  id: "",
+  name: "",
+  description:
+    "",
+  coingecko: "",
+  defillama: "",
+  links: {
+    website: "",
+  },
+  async getContracts() {
+    return {
+      contracts: [contract],  //this should be an array of all contracts getBalances will look at
+      revalidate: 60 * 60,
+    };
+  },
+  async getBalances(ctx, contracts) {
+    let balances = await getBalances(ctx, "ethereum", contracts); //any method to check all the contracts retrieved above
+
+    return {
+      balances: balances.map((balance) => ({
+        ...balance,
+        category: "stake",
+      })),
+    };
+  },
+};
+
+export default adapter;
