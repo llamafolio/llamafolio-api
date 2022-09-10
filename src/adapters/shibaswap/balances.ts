2ac0cd6eaa211279081ae9701abd22e12352c693
0xsign <99134502+0xsign@users.noreply.github.com>
2022-09-10 21:15:56 +0100

    masterchef farm pools and underlyings (#31)

--- src/adapters/shibaswap/balances.ts
+++ src/adapters/shibaswap/balances.ts
@@ -1,106 +1,54 @@
-import { Contract } from "@lib/adapter";
 import { ethers, BigNumber } from "ethers";
+import { Contract, BaseContext } from "@lib/adapter";
 import { Chain, providers } from "@defillama/sdk/build/general";
 import { getERC20Balances, getERC20Details } from "@lib/erc20";
-import { returnMasterChefDetails } from "@lib/masterchef/index";
 import { getUnderlyingBalances } from "@lib/uniswap/v2/pair";
+import { Token } from "@lib/token";
 
 import LockerAbi from "./abis/Locker.json";
 import StakerAbi from "./abis/Staker.json";
-import { BaseContext } from "@lib/adapter";
 
-export async function getBalances(ctx: BaseContext, chain: Chain, contracts: Contract[]) {
+const bone: Token = {
+  chain: "ethereum",
+  symbol: "BONE",
+  decimals: 18,
+  address: "0x9813037ee2218799597d83d4a5b6f3b6778218d9",
+};
+
+export async function getStakerBalances(
+  ctx: BaseContext,
+  chain: Chain,
+  address: string
+) {
   const provider = providers[chain];
-  const balances = [];
+  const Staker = new ethers.Contract(address, StakerAbi, provider);
 
-  for (let index = 0; index < contracts.length; index++) {
-    const contract = contracts[index];
+  const stakedBone = await Staker.balanceOf(ctx.address);
 
-    if (contract.name === "locker") {
-      const Locker = new ethers.Contract(contract.address, LockerAbi, provider);
+  return [
+    {
+      ...bone,
+      category: "stake",
+      amount: BigNumber.from(stakedBone),
+    },
+  ];
+}
 
-      const remainingLocker = await Locker.unclaimedTokensByUser(ctx.address);
+export async function getLockerBalances(
+  ctx: BaseContext,
+  chain: Chain,
+  address: string
+) {
+  const provider = providers[chain];
+  const Locker = new ethers.Contract(address, LockerAbi, provider);
 
-      balances.push({
-        chain: chain,
-        category: "lock",
-        symbol: "BONE",
-        decimals: 18,
-        address: "0x9813037ee2218799597d83D4a5B6F3b6778218d9",
-        amount: BigNumber.from(remainingLocker),
-      });
-    }
+  const remainingLocker = await Locker.unclaimedTokensByUser(ctx.address);
 
-    if (contract.name === "staker") {
-      const Staker = new ethers.Contract(contract.address, StakerAbi, provider);
-
-      const stakedBone = await Staker.balanceOf(ctx.address);
-
-      balances.push({
-        chain: chain,
-        category: "stake",
-        symbol: "BONE",
-        decimals: 18,
-        address: "0x9813037ee2218799597d83D4a5B6F3b6778218d9",
-        amount: BigNumber.from(stakedBone),
-      });
-    }
-
-    if (contract.name === "masterChef") {
-      const masterDetails = await returnMasterChefDetails(
-        ctx,
-        chain,
-        contract.address,
-        "pendingBone"
-      );
-
-      const fetchUnderlyings = [];
-
-      for (let yy = 0; yy < masterDetails.length; yy++) {
-        const masterRow = masterDetails[yy];
-        balances.push({
-          chain: chain,
-          category: "lp",
-          symbol: masterRow.token.symbol,
-          decimals: masterRow.token.decimals,
-          address: masterRow.token.address,
-          amount: BigNumber.from(masterRow.amount),
-        });
-
-        fetchUnderlyings.push({
-          ...masterRow.token,
-          amount: BigNumber.from(masterRow.amount),
-        });
-
-        if (masterRow.rewardsPending > 0) {
-          balances.push({
-            chain: chain,
-            category: "rewards",
-            symbol: "BONE",
-            decimals: 18,
-            address: "0x9813037ee2218799597d83D4a5B6F3b6778218d9",
-            amount: BigNumber.from(masterRow.rewardsPending),
-            reward: true,
-            parent: masterRow.token.address,
-          });
-        }
-      }
-
-      const underlyingBalances = await getUnderlyingBalances(
-        chain,
-        fetchUnderlyings
-      );
-
-      for (let t = 0; t < balances.length; t++) {
-        const underlyingDetail = underlyingBalances.find(
-          (o) => o.address.toLowerCase() === balances[t].address.toLowerCase()
-        );
-        if (underlyingDetail) {
-          balances[t].underlyingDetails = underlyingDetail.details;
-        }
-      }
-    }
-  }
-
-  return balances;
+  return [
+    {
+      ...bone,
+      category: "lock",
+      amount: BigNumber.from(remainingLocker),
+    },
+  ];
 }
--- src/adapters/shibaswap/index.ts
+++ src/adapters/shibaswap/index.ts
@@ -1,15 +1,19 @@
 import { Adapter, Contract } from "@lib/adapter";
 import { getERC20BalanceOf } from "@lib/erc20";
+import { getUnderlyingBalances } from "@lib/uniswap/v2/pair";
 import { getPairsInfo } from "@lib/uniswap/v2/factory";
-import { getBalances } from "./balances";
+import { getMasterChefPoolsInfo, getMasterChefBalances } from "@lib/masterchef";
+import { isNotNullish } from "@lib/type";
+import { Token } from "@lib/token";
+import { getLockerBalances, getStakerBalances } from "./balances";
 
-const lockerContract: Contract = {
+const locker: Contract = {
   name: "locker",
   displayName: "Locker",
   chain: "ethereum",
   address: "0xa404f66b9278c4ab8428225014266b4b239bcdc7",
 };
-const stakerContract: Contract = {
+const staker: Contract = {
   name: "staker",
   displayName: "Staker tBone",
   chain: "ethereum",
@@ -23,27 +27,101 @@ const masterChef: Contract = {
   address: "0x94235659cf8b805b2c658f9ea2d6d6ddbb17c8d7",
 };
 
+const bone: Token = {
+  chain: "ethereum",
+  symbol: "BONE",
+  decimals: 18,
+  address: "0x9813037ee2218799597d83d4a5b6f3b6778218d9",
+};
+
 const adapter: Adapter = {
   id: "shibaswap",
   async getContracts() {
-    return {
-      contracts: await getPairsInfo({
+    const [pairsInfo, masterChefPoolsInfo] = await Promise.all([
+      getPairsInfo({
         chain: "ethereum",
         factoryAddress: "0x115934131916C8b277DD010Ee02de363c09d037c",
-        length: 10,
+        length: 100,
       }),
+
+      getMasterChefPoolsInfo({
+        chain: "ethereum",
+        masterChefAddress: masterChef.address,
+      }),
+    ]);
+
+    // retrieve master chef pools details from lpToken addresses
+    const pairByAddress: { [key: string]: Contract } = {};
+    for (const pair of pairsInfo) {
+      pairByAddress[pair.address.toLowerCase()] = pair;
+    }
+
+    const masterChefPools = masterChefPoolsInfo
+      .map((pool) => {
+        const pair = pairByAddress[pool.lpToken.toLowerCase()];
+        if (!pair) {
+          return null;
+        }
+        return { ...pair, pid: pool.pid };
+      })
+      .filter(isNotNullish);
+
+    const contracts = [
+      ...pairsInfo.map((c) => ({ ...c, category: "lp" })),
+      ...masterChefPools.map((c) => ({ ...c, category: "farm" })),
+    ];
+
+    return {
+      contracts,
       revalidate: 60 * 60,
     };
   },
   async getBalances(ctx, contracts) {
-    let balances = await getERC20BalanceOf(ctx, "ethereum", contracts);
+    let balances = [];
+    const lp = [];
+    const farm = [];
 
-    const stakeBalances = await getBalances(ctx, "ethereum", [
-      lockerContract,
-      stakerContract,
-      masterChef,
-    ]);
-    balances = balances.concat(stakeBalances);
+    for (const contract of contracts) {
+      if (contract.category === "lp") {
+        lp.push(contract);
+      } else if (contract.category === "farm") {
+        farm.push(contract);
+      }
+    }
+
+    let stakerBalances = await getStakerBalances(
+      ctx,
+      "ethereum",
+      staker.address
+    );
+
+    balances = balances.concat(stakerBalances);
+
+    let lockerBalances = await getLockerBalances(
+      ctx,
+      "ethereum",
+      locker.address
+    );
+
+    balances = balances.concat(lockerBalances);
+
+    let lpBalances = await getERC20BalanceOf(ctx, "ethereum", lp);
+    lpBalances = await getUnderlyingBalances("ethereum", lpBalances);
+
+    balances = balances.concat(lpBalances);
+
+    let masterChefBalances = await getMasterChefBalances(ctx, {
+      chain: "ethereum",
+      masterChefAddress: masterChef.address,
+      tokens: farm,
+      rewardToken: bone,
+    });
+    masterChefBalances = await getUnderlyingBalances(
+      "ethereum",
+      masterChefBalances
+    );
+
+    balances = balances.concat(masterChefBalances);
 
     return {
       balances,
