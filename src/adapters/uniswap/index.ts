ed64eb8d4414ccbc5112e18d7540e657e4d07836
0xsign <0xsign@protonmail.com>
2022-09-12 18:04:15 +0200

    uniswap: fix underlyings

--- src/adapters/uniswap/index.ts
+++ src/adapters/uniswap/index.ts
@@ -1,27 +1,30 @@
-import { Adapter } from "@lib/adapter";
+import { Adapter, Contract } from "@lib/adapter";
 import { getERC20BalanceOf } from "@lib/erc20";
 import { getPairsInfo } from "@lib/uniswap/v2/factory";
+import { getUnderlyingBalances } from "@lib/uniswap/v2/pair";
 
 const adapter: Adapter = {
   id: "uniswap",
   async getContracts() {
-    return {
-      contracts: await getPairsInfo({
+    const pairs: Contract[] = (
+      await getPairsInfo({
         chain: "ethereum",
         factoryAddress: "0x5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f",
         length: 100,
-      }),
+      })
+    ).map((contract) => ({ ...contract, category: "farm" }));
+
+    return {
+      contracts: pairs,
       revalidate: 60 * 60,
     };
   },
   async getBalances(ctx, contracts) {
-    const balances = await getERC20BalanceOf(ctx, "ethereum", contracts);
+    let lpBalances = await getERC20BalanceOf(ctx, "ethereum", contracts);
+    lpBalances = await getUnderlyingBalances("ethereum", lpBalances);
 
     return {
-      balances: balances.map((balance) => ({
-        ...balance,
-        category: "farm",
-      })),
+      balances: lpBalances,
     };
   },
 };
