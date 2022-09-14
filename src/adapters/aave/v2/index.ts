59eb6cae3ed15b798a10637a55eb5dfb3ad18c55
0xsign <99134502+0xsign@users.noreply.github.com>
2022-09-14 22:57:27 +0100

    fix aave v2 fetch all contracts info in getContracts (#32)

--- src/adapters/aave/v2/index.ts
+++ src/adapters/aave/v2/index.ts
@@ -1,51 +1,53 @@
 import { Chain } from "@defillama/sdk/build/general";
-import { Adapter, Contract, resolveContractsBalances } from "@lib/adapter";
-import { getLendingPoolBalances } from "@lib/aave/v2/lending";
-
-function getChainContracts(
-  chain: Chain,
-  { lendingPool }: { lendingPool: string }
-) {
-  const lendingPoolContract: Contract = {
-    name: "LendingPool",
-    displayName: "AAVE Lending",
-    chain,
-    address: lendingPool,
-  };
-
-  return [lendingPoolContract];
-}
+import { Adapter, Contract } from "@lib/adapter";
+import {
+  getLendingPoolContracts,
+  getLendingPoolBalances,
+} from "@lib/aave/v2/lending";
 
 const adapter: Adapter = {
   id: "aave-v2",
-  getContracts() {
-    const chainsContracts = [
-      getChainContracts("avax", {
-        lendingPool: "0x4f01aed16d97e3ab5ab2b501154dc9bb0f1a5a2c",
-      }),
-      getChainContracts("ethereum", {
-        lendingPool: "0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9",
-      }),
-      getChainContracts("polygon", {
-        lendingPool: "0x8dff5e27ea6b7ac08ebfdf9eb090f32ee9a30fcf",
-      }),
-    ];
+  async getContracts() {
+    const chainsContracts = await Promise.all([
+      getLendingPoolContracts(
+        "avax",
+        "0x4f01aed16d97e3ab5ab2b501154dc9bb0f1a5a2c"
+      ),
+      getLendingPoolContracts(
+        "ethereum",
+        "0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9"
+      ),
+      getLendingPoolContracts(
+        "polygon",
+        "0x8dff5e27ea6b7ac08ebfdf9eb090f32ee9a30fcf"
+      ),
+    ]);
 
     return {
       contracts: chainsContracts.flat(),
     };
   },
   async getBalances(ctx, contracts) {
-    function resolver(contract: Contract) {
-      if (contract.name === "LendingPool") {
-        return getLendingPoolBalances(ctx, {
-          chain: contract.chain,
-          lendingPoolAddress: contract.address,
-        });
+    const contractsByChain: Partial<Record<Chain, Contract[]>> = {};
+
+    for (const contract of contracts) {
+      if (!contractsByChain[contract.chain]) {
+        contractsByChain[contract.chain] = [];
       }
+      contractsByChain[contract.chain]?.push(contract);
     }
 
-    return { balances: await resolveContractsBalances(resolver, contracts) };
+    const chainsBalances = await Promise.all(
+      Object.keys(contractsByChain).map((chain) =>
+        getLendingPoolBalances(
+          ctx,
+          chain as Chain,
+          contractsByChain[chain as Chain] || []
+        )
+      )
+    );
+
+    return { balances: chainsBalances.flat() };
   },
 };
 
