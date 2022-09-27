1b843db0c0a86d1eb1be455062d541eb12255654
0xsign <0xsign@protonmail.com>
2022-09-27 18:59:24 +0200

    multicall and export adapter

--- src/adapters/hex/index.ts
+++ src/adapters/hex/index.ts
@@ -1,10 +1,18 @@
-import { Chain, providers } from "@defillama/sdk/build/general";
-import { Adapter, Contract } from "@lib/adapter";
-import { ethers } from "ethers";
-import abi from "./abi/hex.json";
 import { BigNumber } from "ethers";
+import { ethers } from "ethers";
+import { Chain, providers } from "@defillama/sdk/build/general";
+import {
+  Adapter,
+  Balance,
+  Contract,
+  resolveContractsBalances,
+} from "@lib/adapter";
+import abi from "./abi/hex.json";
+import { multicall } from "@lib/multicall";
+import { range } from "@lib/array";
+import { sumBN } from "@lib/math";
 
-const stake_hex: Contract = {
+const HEX: Contract = {
   name: "HEX",
   chain: "ethereum",
   address: "0x2b591e99afe9f32eaa6214f7b7629768c40eeb39",
@@ -12,44 +20,72 @@ const stake_hex: Contract = {
   symbol: "HEX",
 };
 
-const getStakeContracts = async (ctx: any, chain: Chain) => {
-  const balances = []
+const getStakeBalances = async (ctx: any, chain: Chain) => {
   let stakeCount: number = 0;
   const provider = providers[chain];
-  const stakeContracts = new ethers.Contract(stake_hex.address, abi, provider);
+  const stakeContracts = new ethers.Contract(HEX.address, abi, provider);
   stakeCount = await stakeContracts.stakeCount(ctx.address);
-  
-  for (let i = 0; i < stakeCount; i++ ) {
-    const balanceOf = await stakeContracts.stakeLists(ctx.address, i)
-    balances.push(balanceOf.stakedHearts)
-  }
 
-  const stakeBalances = balances.reduce((acc, num) => BigNumber.from(acc).add(num))
+  const stakeListsRes = await multicall({
+    chain: HEX.chain,
+    calls: range(0, stakeCount).map((i) => ({
+      target: HEX.address,
+      params: [ctx.address, i],
+    })),
+    abi: {
+      constant: true,
+      inputs: [
+        { internalType: "address", name: "", type: "address" },
+        { internalType: "uint256", name: "", type: "uint256" },
+      ],
+      name: "stakeLists",
+      outputs: [
+        { internalType: "uint40", name: "stakeId", type: "uint40" },
+        { internalType: "uint72", name: "stakedHearts", type: "uint72" },
+        { internalType: "uint72", name: "stakeShares", type: "uint72" },
+        { internalType: "uint16", name: "lockedDay", type: "uint16" },
+        { internalType: "uint16", name: "stakedDays", type: "uint16" },
+        { internalType: "uint16", name: "unlockedDay", type: "uint16" },
+        { internalType: "bool", name: "isAutoStake", type: "bool" },
+      ],
+      payable: false,
+      stateMutability: "view",
+      type: "function",
+    },
+  });
 
-  return stakeBalances
+  const stakeAmount = sumBN(
+    stakeListsRes
+      .filter((res) => res.success)
+      .map((res) => BigNumber.from(res.output.stakedHearts))
+  );
+
+  const stakeBalance: Balance = {
+    ...HEX,
+    amount: stakeAmount,
+    category: "stake",
+  };
+
+  return [stakeBalance];
 };
-
 
 const adapter: Adapter = {
   id: "hex",
-  async getContracts() {
+  getContracts() {
     return {
-      contracts: stake_hex,
+      contracts: [HEX],
     };
   },
 
   async getBalances(ctx, contracts) {
-
-    const stakebBalances = [{
-      ...contracts,
-      amount: await getStakeContracts(ctx, "ethereum"),
-      category: "stake"
-    }]
-
-    let balances = stakebBalances
+    function resolver(contract: Contract) {
+      if (contract.address === HEX.address) {
+        return getStakeBalances(ctx, "ethereum");
+      }
+    }
 
     return {
-      balances,
+      balances: await resolveContractsBalances(resolver, contracts),
     };
   },
 };
