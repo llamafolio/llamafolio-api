49f8535d8a147249b986b07c594006ecd57d221b
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2022-09-29 21:09:01 +0200

    Synthetix adapter (#54)

---
+++ src/adapters/synthetix/index.ts
@@ -0,0 +1,89 @@
+import { Adapter, Contract, resolveContractsBalances } from "@lib/adapter";
+import { Token } from "@lib/token";
+import { getLendBorrowBalances } from "./lend";
+
+const SNXEthereum: Token = {
+  chain: "ethereum",
+  decimals: 18,
+  address: "0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f",
+  symbol: "SNX",
+};
+
+const sUSDEthereum: Token = {
+  symbol: "sUSD",
+  address: "0x57Ab1ec28D129707052df4dF418D58a2D46d5f51",
+  decimals: 18,
+  chain: "ethereum",
+};
+
+const SynthetixEthereum: Contract = {
+  name: "Synthetix",
+  chain: "ethereum",
+  symbol: "SNX",
+  decimals: 18,
+  address: "0x08f30ecf2c15a783083ab9d5b9211c22388d0564",
+  underlyings: [SNXEthereum],
+};
+
+const SNXOptimism: Token = {
+  chain: "optimism",
+  decimals: 18,
+  address: "0x8700daec35af8ff88c16bdf0418774cb3d7599b4",
+  symbol: "SNX",
+};
+
+const sUSDOptimism: Token = {
+  symbol: "sUSD",
+  address: "0x8c6f28f2F1A3C87F0f938b96d27520d9751ec8d9",
+  decimals: 18,
+  chain: "optimism",
+};
+
+const SynthetixOptimism: Contract = {
+  name: "Synthetix",
+  chain: "optimism",
+  symbol: "SNX",
+  decimals: 18,
+  address: "0xfe8e48bf36ccc3254081ec8c65965d1c8b2e744d",
+  underlyings: [SNXOptimism],
+};
+
+const adapter: Adapter = {
+  id: "synthetix",
+  async getContracts() {
+    return {
+      contracts: [SynthetixEthereum, SynthetixOptimism],
+    };
+  },
+  async getBalances(ctx, contracts) {
+    function resolver(contract: Contract) {
+      if (
+        contract.chain === SynthetixEthereum.chain &&
+        contract.address === SynthetixEthereum.address
+      ) {
+        return getLendBorrowBalances(ctx, "ethereum", {
+          synthetixContract: SynthetixEthereum,
+          feePoolAddress: "0x3b2f389aee480238a49e3a9985cd6815370712eb",
+          sUSD: sUSDEthereum,
+        });
+      }
+
+      if (
+        contract.chain === SynthetixOptimism.chain &&
+        contract.address === SynthetixOptimism.address
+      ) {
+        return getLendBorrowBalances(ctx, "optimism", {
+          synthetixContract: SynthetixOptimism,
+          feePoolAddress: "0xD3739A5F06747e148E716Dcb7147B9BA15b70fcc",
+          sUSD: sUSDOptimism,
+        });
+      }
+    }
+
+    return {
+      balances: await resolveContractsBalances(resolver, contracts),
+    };
+  },
+};
+
+export default adapter;
---
+++ src/adapters/synthetix/lend.ts
@@ -0,0 +1,119 @@
+import { Chain } from "@defillama/sdk/build/general";
+import { call } from "@defillama/sdk/build/abi";
+import { BigNumber } from "ethers";
+import { Token } from "@lib/token";
+import { Balance, BaseContext, Contract } from "@lib/adapter";
+
+export type GetLendBorrowBalancesParams = {
+  synthetixContract: Contract;
+  feePoolAddress: string;
+  sUSD: Token;
+};
+
+export async function getLendBorrowBalances(
+  ctx: BaseContext,
+  chain: Chain,
+  { synthetixContract, feePoolAddress, sUSD }: GetLendBorrowBalancesParams
+) {
+  let balances: Balance[] = [];
+
+  const [suppliedRes, borrowedRes, feesAvailableRes] = await Promise.all([
+    call({
+      chain,
+      target: synthetixContract.address,
+      params: [ctx.address],
+      abi: {
+        constant: true,
+        inputs: [{ internalType: "address", name: "account", type: "address" }],
+        name: "collateral",
+        outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+    }),
+
+    call({
+      chain,
+      target: synthetixContract.address,
+      params: [ctx.address],
+      abi: {
+        constant: true,
+        inputs: [{ internalType: "address", name: "account", type: "address" }],
+        name: "remainingIssuableSynths",
+        outputs: [
+          { internalType: "uint256", name: "maxIssuable", type: "uint256" },
+          { internalType: "uint256", name: "alreadyIssued", type: "uint256" },
+          {
+            internalType: "uint256",
+            name: "totalSystemDebt",
+            type: "uint256",
+          },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+    }),
+
+    call({
+      chain,
+      target: feePoolAddress,
+      params: [ctx.address],
+      abi: {
+        constant: true,
+        inputs: [{ internalType: "address", name: "account", type: "address" }],
+        name: "feesAvailable",
+        outputs: [
+          { internalType: "uint256", name: "", type: "uint256" },
+          { internalType: "uint256", name: "", type: "uint256" },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+    }),
+  ]);
+
+  const lendAmount = BigNumber.from(suppliedRes.output);
+  const lendingBalance: Balance = {
+    ...synthetixContract,
+    amount: lendAmount,
+    underlyings: [{ ...synthetixContract.underlyings[0], amount: lendAmount }],
+    category: "lend",
+  };
+  balances.push(lendingBalance);
+
+  const borrowAmount = BigNumber.from(borrowedRes.output.alreadyIssued);
+  const borrowBalance: Balance = {
+    ...sUSD,
+    amount: borrowAmount,
+    category: "borrow",
+  };
+  balances.push(borrowBalance);
+
+  const sUSDRewardAmount = BigNumber.from(feesAvailableRes.output[0]);
+  const sUSDRewardBalance: Balance = {
+    ...sUSD,
+    amount: sUSDRewardAmount,
+    category: "reward",
+  };
+  balances.push(sUSDRewardBalance);
+
+  const SNXRewardAmount = BigNumber.from(feesAvailableRes.output[1]);
+  const SNXRewardBalance: Balance = {
+    ...synthetixContract,
+    amount: SNXRewardAmount,
+    underlyings: [
+      {
+        ...synthetixContract.underlyings[0],
+        amount: SNXRewardAmount,
+        claimable: SNXRewardAmount,
+      },
+    ],
+    category: "reward",
+  };
+  balances.push(SNXRewardBalance);
+
+  return balances;
+}
