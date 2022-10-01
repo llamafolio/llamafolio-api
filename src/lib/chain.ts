5f91cf4756768397fc68c56dc1f6f7b8ca413b69
0xsign <0xsign@protonmail.com>
2022-10-01 11:36:36 +0200

    initial aurora support

--- src/lib/chain.ts
+++ src/lib/chain.ts
@@ -2,14 +2,14 @@ import { Chain } from "@defillama/sdk/build/general";
 
 // Currently supported chains
 export const chains: Chain[] = [
-  "ethereum",
+  "arbitrum",
   "avax",
   "bsc",
-  "fantom",
-  "polygon",
-  "arbitrum",
-  "harmony",
   "celo",
-  "xdai",
+  "ethereum",
+  "fantom",
+  "harmony",
+  "polygon",
   "optimism",
+  "xdai",
 ];
