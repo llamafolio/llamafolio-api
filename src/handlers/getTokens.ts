3100a3c317428119d9c3e1480cab2a18fd0c79b1
0xsign <0xsign@protonmail.com>
2022-10-05 21:39:58 +0200

    add /tokens endpoint

---
+++ src/handlers/getTokens.ts
@@ -0,0 +1,46 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { success } from "@handlers/response";
+import { Token } from "@lib/token";
+import { getToken } from "@llamafolio/tokens";
+
+/**
+ * Get tokens of given addresses
+ * Tokens are comma separated and should be prefixed by their chain: `{chain}:{address}`
+ * Or only the name of the chain to get the native coin. Ex: `arbitrum,avax,ethereum`
+ * (Chain 'ethereum' is used by default if no chain is specified)
+ */
+export const handler: APIGatewayProxyHandler = async (event) => {
+  const chainAddresses = event.pathParameters?.address?.split(",") ?? [];
+  const data: { [key: string]: Token } = {};
+
+  for (const chainAddress of chainAddresses) {
+    const split = chainAddress.split(":");
+    let chain = "ethereum";
+    let address: string | undefined;
+
+    // chain or address
+    if (split.length === 1) {
+      if (split[0].startsWith("0x")) {
+        address = split[0];
+      } else {
+        chain = split[0];
+      }
+    } else {
+      chain = split[0];
+      address = split[1];
+    }
+
+    const token = getToken(chain, address?.toLowerCase()) as Token;
+
+    if (token) {
+      data[chainAddress] = token;
+    }
+  }
+
+  return success(
+    {
+      data,
+    },
+    { maxAge: 10 * 60 }
+  );
+};
