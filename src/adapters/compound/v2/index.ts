b62f9c3b04240fcabe27d88dbfe5f3164668739d
0xsign <0xsign@protonmail.com>
2022-10-10 16:23:13 +0200

    add option in coumpound lib to specify missing underlyings for markets

--- src/adapters/compound/v2/index.ts
+++ src/adapters/compound/v2/index.ts
@@ -18,6 +18,11 @@ const adapter: Adapter = {
   async getContracts() {
     const markets = await getMarketsContracts("ethereum", {
       comptrollerAddress: "0x3d9819210a31b4961b30ef54be2aed79b9c9cd3b",
+      underlyingAddressByMarketAddress: {
+        // cETH -> WETH
+        "0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5":
+          "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+      },
     });
 
     return {
