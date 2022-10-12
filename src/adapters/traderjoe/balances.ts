b1ec122dd06548ca1d734890d2f2e91c80a260ec
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2022-10-12 14:13:05 +0200

    TraderJoe add stake/rewards + compound fork (#92)

---
+++ src/adapters/traderjoe/balances.ts
@@ -0,0 +1,192 @@
+import { Chain } from "@defillama/sdk/build/general";
+import { BaseContext } from "@lib/adapter";
+import { BigNumber } from "ethers";
+import { Contract, Balance } from "@lib/adapter";
+import { call } from "@defillama/sdk/build/abi";
+interface Token extends Contract {
+  name: string;
+}
+
+const pools = [
+  "0x1a731b2299e22fbac282e7094eda41046343cb51", // sJOE contract
+  "0x25D85E17dD9e544F6E9F8D44F99602dbF5a97341", // veJOE contract
+  "0x102D195C3eE8BF8A9A89d63FB3659432d3174d81", // rJOE contract
+];
+
+const USDC: Token = {
+  name: "USD Coin",
+  chain: "avax",
+  decimals: 6,
+  address: "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e",
+  symbol: "USDC",
+};
+
+const veJOE: Token = {
+  name: "VeJoeToken",
+  chain: "avax",
+  decimals: 18,
+  address: "0x3cabf341943Bc8466245e4d6F1ae0f8D071a1456",
+  symbol: "veJOE",
+};
+
+const rJOE: Token = {
+  name: "RocketJoeToken",
+  chain: "avax",
+  decimals: 18,
+  address: "0x5483ce08659fABF0277f9314868Cc4f78687BD08",
+  symbol: "rJOE",
+};
+
+const JOE: Token = {
+  name: "TraderJoe Token",
+  chain: "avax",
+  address: "0x6e84a6216ea6dacc71ee8e6b0a5b7322eebc0fdd",
+  symbol: "JOE",
+  decimals: 18,
+  rewards: [USDC, veJOE, rJOE],
+  coingeckoId: "joe",
+};
+
+export async function getStakeBalance(ctx: BaseContext, chain: Chain) {
+  let balances: Balance[] = [];
+
+  const [sJOEbalanceOfRes, veJOEbalanceOfRes, rJOEbalanceOfRes] =
+    await Promise.all([
+      call({
+        chain,
+        target: pools[0],
+        params: [ctx.address, USDC.address],
+        abi: {
+          inputs: [
+            { internalType: "address", name: "_user", type: "address" },
+            {
+              internalType: "contract IERC20Upgradeable",
+              name: "_rewardToken",
+              type: "address",
+            },
+          ],
+          name: "getUserInfo",
+          outputs: [
+            { internalType: "uint256", name: "", type: "uint256" },
+            { internalType: "uint256", name: "", type: "uint256" },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+      }),
+
+      call({
+        chain,
+        target: pools[1],
+        params: [ctx.address],
+        abi: {
+          inputs: [{ internalType: "address", name: "", type: "address" }],
+          name: "userInfos",
+          outputs: [
+            { internalType: "uint256", name: "balance", type: "uint256" },
+            { internalType: "uint256", name: "rewardDebt", type: "uint256" },
+            {
+              internalType: "uint256",
+              name: "lastClaimTimestamp",
+              type: "uint256",
+            },
+            {
+              internalType: "uint256",
+              name: "speedUpEndTimestamp",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+      }),
+
+      call({
+        chain,
+        target: pools[2],
+        params: [ctx.address],
+        abi: {
+          inputs: [{ internalType: "address", name: "", type: "address" }],
+          name: "userInfo",
+          outputs: [
+            { internalType: "uint256", name: "amount", type: "uint256" },
+            { internalType: "uint256", name: "rewardDebt", type: "uint256" },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+      }),
+    ]);
+
+  const sJOEbalanceOf = BigNumber.from(sJOEbalanceOfRes.output[0]);
+  const veJOEbalanceOf = BigNumber.from(veJOEbalanceOfRes.output.balance);
+  const rJOEbalanceOf = BigNumber.from(rJOEbalanceOfRes.output.amount);
+
+  const stakeAmount = [sJOEbalanceOf, veJOEbalanceOf, rJOEbalanceOf];
+
+  const [sJOErewardsRes, veJOErewardsRes, rJOErewardsRes] = await Promise.all([
+    call({
+      chain,
+      target: pools[0],
+      params: [ctx.address, USDC.address],
+      abi: {
+        inputs: [
+          { internalType: "address", name: "_user", type: "address" },
+          {
+            internalType: "contract IERC20Upgradeable",
+            name: "_token",
+            type: "address",
+          },
+        ],
+        name: "pendingReward",
+        outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+        stateMutability: "view",
+        type: "function",
+      },
+    }),
+
+    call({
+      chain,
+      target: pools[1],
+      params: [ctx.address],
+      abi: {
+        inputs: [{ internalType: "address", name: "_user", type: "address" }],
+        name: "getPendingVeJoe",
+        outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+        stateMutability: "view",
+        type: "function",
+      },
+    }),
+
+    call({
+      chain,
+      target: pools[2],
+      params: [ctx.address],
+      abi: {
+        inputs: [{ internalType: "address", name: "_user", type: "address" }],
+        name: "pendingRJoe",
+        outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+        stateMutability: "view",
+        type: "function",
+      },
+    }),
+  ]);
+
+  const sJOErewards = BigNumber.from(sJOErewardsRes.output);
+  const veJOErewards = BigNumber.from(veJOErewardsRes.output);
+  const rJOErewards = BigNumber.from(rJOErewardsRes.output);
+
+  const rewardsAmount = [sJOErewards, veJOErewards, rJOErewards];
+
+  for (let i = 0; i < pools.length; i++) {
+    const balance: Balance = {
+      ...JOE,
+      address: pools[i],
+      amount: stakeAmount[i],
+      rewards: [{ ...JOE.rewards?.[i], amount: rewardsAmount[i] }],
+      category: "stake",
+    };
+    balances.push(balance);
+  }
+  return balances;
+}
--- src/adapters/traderjoe/index.ts
+++ src/adapters/traderjoe/index.ts
@@ -1,26 +1,82 @@
 import { Adapter } from "@lib/adapter";
+import {
+  getMarketsBalances,
+  getMarketsContracts,
+} from "@lib/compound/v2/lending";
+import { Contract } from "@lib/adapter";
+import { getStakeBalance } from "./balances";
 import { getPairsContracts } from "@lib/uniswap/v2/factory";
 import { getPairsBalances } from "@lib/uniswap/v2/pair";
+
+const sJOE: Contract = {
+  name: "sJOE staking",
+  chain: "avax",
+  address: "0x1a731b2299e22fbac282e7094eda41046343cb51",
+  types: "stake",
+};
+
+const veJOE: Contract = {
+  name: "veJOE staking",
+  chain: "avax",
+  address: "0x25D85E17dD9e544F6E9F8D44F99602dbF5a97341",
+  types: "stake",
+};
+
+const rJOE: Contract = {
+  name: "rJOE staking",
+  chain: "avax",
+  address: "0x102D195C3eE8BF8A9A89d63FB3659432d3174d81",
+  types: "stake",
+};
 
 const adapter: Adapter = {
   id: "trader-joe",
   async getContracts() {
+    const markets = await getMarketsContracts("avax", {
+      comptrollerAddress: "0xdc13687554205E5b89Ac783db14bb5bba4A1eDaC",
+    });
+
+    const poolsMarkets: Contract[] = markets.map((contract) => {
+      return {
+        ...contract,
+        type: "market",
+      };
+    });
+
     const pairs = await getPairsContracts({
       chain: "avax",
       factoryAddress: "0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10",
       length: 100,
     });
 
+    const uniPools: Contract[] = pairs.map((contract) => {
+      return {
+        ...contract,
+        type: "pool",
+      };
+    });
+
     return {
-      contracts: pairs,
+      contracts: [...poolsMarkets, ...uniPools, sJOE, veJOE, rJOE],
       revalidate: 60 * 60,
     };
   },
+
   async getBalances(ctx, contracts) {
-    const pairs = await getPairsBalances(ctx, "avax", contracts);
+    const marketsPool = contracts.filter(
+      (contract) => contract.type === "market"
+    );
+    const marketsBalances = await getMarketsBalances(ctx, "avax", marketsPool);
+
+    const uniPools = contracts.filter((contract) => contract.type === "pool");
+    const pairsBalances = await getPairsBalances(ctx, "avax", uniPools);
+
+    const stakeBalances = await getStakeBalance(ctx, "avax");
+
+    const balances = [...marketsBalances, ...stakeBalances, ...pairsBalances];
 
     return {
-      balances: pairs,
+      balances,
     };
   },
 };
