3e3f1e08d45dc180d11d665938768b8c060df05d
0xpeluche <0xpeluche@protonmail.com>
2022-10-17 10:48:51 +0200

    refacto

--- src/adapters/hundred-finance/index.ts
+++ src/adapters/hundred-finance/index.ts
@@ -7,55 +7,59 @@ import {
 const getContracts = async () => {
   const comptrollerAddress = "0x0f390559f258eb8591c8e31cf0905e97cf36ace2";
 
-  const poolsMarketsETH = await getMarketsContracts("ethereum", {
-    // hundred-finance comptroller on ETH chain
-    comptrollerAddress,
-    underlyingAddressByMarketAddress: {
-      // hETH -> wETH
-      "0xfcd8570ad81e6c77b8d252bebeba62ed980bd64d":
-        "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-    },
-  });
-
-  const poolsMarketsFTM = await getMarketsContracts("fantom", {
-    // hundred-finance comptroller on Fantom chain
-    comptrollerAddress,
-    underlyingAddressByMarketAddress: {
-      // hFTM -> wFTM
-      "0xfcd8570ad81e6c77b8d252bebeba62ed980bd64d":
-        "0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83",
-    },
-  });
-
-  const poolsMarketsPOLY = await getMarketsContracts("polygon", {
-    // hundred-finance comptroller on Polygon chain
-    comptrollerAddress: "0xEdBA32185BAF7fEf9A26ca567bC4A6cbe426e499",
-    underlyingAddressByMarketAddress: {
-      // hMATIC -> wMATIC
-      "0xebd7f3349aba8bb15b897e03d6c1a4ba95b55e31":
-        "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270",
-    },
-  });
-
-  const poolsMarketsARB = await getMarketsContracts("arbitrum", {
-    // hundred-finance comptroller on Arbitrum chain
-    comptrollerAddress,
-    underlyingAddressByMarketAddress: {
-      // hETH -> wETH
-      "0x8e15a22853a0a60a0fbb0d875055a8e66cff0235":
-        "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
-    },
-  });
-
-  const poolsMarketsOPT = await getMarketsContracts("optimism", {
-    // hundred-finance comptroller on Optimism chain
-    comptrollerAddress,
-    underlyingAddressByMarketAddress: {
-      // hETH -> wETH
-      "0xe8f12f5492ec28609d2932519456b7436d6c93bd":
-        "0x4200000000000000000000000000000000000006",
-    },
-  });
+  const [
+    poolsMarketsETH,
+    poolsMarketsFTM,
+    poolsMarketsPOLY,
+    poolsMarketsARB,
+    poolsMarketsOPT,
+  ] = await Promise.all([
+    getMarketsContracts("ethereum", {
+      // hundred-finance comptroller on ETH chain
+      comptrollerAddress,
+      underlyingAddressByMarketAddress: {
+        // hETH -> wETH
+        "0xfcd8570ad81e6c77b8d252bebeba62ed980bd64d":
+          "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+      },
+    }),
+    getMarketsContracts("fantom", {
+      // hundred-finance comptroller on Fantom chain
+      comptrollerAddress,
+      underlyingAddressByMarketAddress: {
+        // hFTM -> wFTM
+        "0xfcd8570ad81e6c77b8d252bebeba62ed980bd64d":
+          "0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83",
+      },
+    }),
+    getMarketsContracts("polygon", {
+      // hundred-finance comptroller on Polygon chain
+      comptrollerAddress: "0xEdBA32185BAF7fEf9A26ca567bC4A6cbe426e499",
+      underlyingAddressByMarketAddress: {
+        // hMATIC -> wMATIC
+        "0xebd7f3349aba8bb15b897e03d6c1a4ba95b55e31":
+          "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270",
+      },
+    }),
+    getMarketsContracts("optimism", {
+      // hundred-finance comptroller on Optimism chain
+      comptrollerAddress,
+      underlyingAddressByMarketAddress: {
+        // hETH -> wETH
+        "0xe8f12f5492ec28609d2932519456b7436d6c93bd":
+          "0x4200000000000000000000000000000000000006",
+      },
+    }),
+    getMarketsContracts("arbitrum", {
+      // hundred-finance comptroller on Arbitrum chain
+      comptrollerAddress,
+      underlyingAddressByMarketAddress: {
+        // hETH -> wETH
+        "0x8e15a22853a0a60a0fbb0d875055a8e66cff0235":
+          "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
+      },
+    }),
+  ]);
 
   return {
     contracts: {
