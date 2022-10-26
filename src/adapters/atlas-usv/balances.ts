55a447163465ca02098adb635d00cbc3b7fbfcc8
0xsign <0xsign@protonmail.com>
2022-10-26 09:43:35 +0200

    smol changes

--- src/adapters/atlas-usv/balances.ts
+++ src/adapters/atlas-usv/balances.ts
@@ -5,69 +5,34 @@ import { abi } from "@lib/erc20";
 import { call } from "@defillama/sdk/build/abi";
 import { BigNumber } from "ethers/lib/ethers";
 
-const USV_ETH: Contract = {
-  name: "Universal Store of Value",
-  chain: "ethereum",
-  address: "0x88536C9B2C4701b8dB824e6A16829D5B5Eb84440",
-  symbol: "USV",
-  decimals: 9,
-};
-
-const USV_POLYGON: Contract = {
-  name: "Universal Store of Value",
-  chain: "polygon",
-  address: "0xac63686230f64bdeaf086fe6764085453ab3023f",
-  symbol: "USV",
-  decimals: 9,
-};
-
-const USV_AVAX: Contract = {
-  name: "Universal Store of Value",
-  chain: "avax",
-  address: "0xb0a8E082E5f8d2a04e74372c1bE47737d85A0E73",
-  symbol: "USV",
-  decimals: 9,
-};
-
-const USV_BSC: Contract = {
-  name: "Universal Store of Value",
-  chain: "bsc",
-  address: "0xaf6162DC717CFC8818eFC8d6f46a41Cf7042fCBA",
-  symbol: "USV",
-  decimals: 9,
-};
-
-export async function getStakeBalances(
+export async function getStakeBalance(
   ctx: BaseContext,
   chain: Chain,
-  contract: Contract
+  contract?: Contract
 ) {
-  const balances: Balance[] = [];
-  const underlyings: Contract[] = [USV_ETH, USV_POLYGON, USV_AVAX, USV_BSC];
+  if (!contract) {
+    return;
+  }
 
-  const balanceOfRes = await call({
-    chain,
-    target: contract.address,
-    params: [ctx.address],
-    abi: abi.balanceOf,
-  });
+  try {
+    const balanceOfRes = await call({
+      chain,
+      target: contract.address,
+      params: [ctx.address],
+      abi: abi.balanceOf,
+    });
 
-  const amount = BigNumber.from(balanceOfRes.output);
+    const amount = BigNumber.from(balanceOfRes.output);
 
-  const underlyingToken = underlyings
-    .filter((token) => token.chain === chain)
-    .map((token) => (token.amount = { ...token, amount }));
+    const balance: Balance = {
+      ...contract,
+      amount,
+      underlyings: [{ ...contract.underlyings?.[0], amount }],
+      category: "stake",
+    };
 
-  const balance: Balance = {
-    chain,
-    address: contract.address,
-    decimals: contract.decimals,
-    symbol: contract.symbol,
-    amount,
-    underlyings: underlyingToken,
-    category: "stake",
-  };
-  balances.push(balance);
-
-  return balances;
+    return balance;
+  } catch (error) {
+    return;
+  }
 }
--- src/adapters/atlas-usv/index.ts
+++ src/adapters/atlas-usv/index.ts
@@ -1,5 +1,6 @@
 import { Adapter, Contract, GetBalancesHandler } from "@lib/adapter";
-import { getStakeBalances } from "./balances";
+import { isNotNullish } from "@lib/type";
+import { getStakeBalance } from "./balances";
 
 const sUSV_ETH: Contract = {
   name: "Staked Universal Store of Value",
@@ -7,6 +8,15 @@ const sUSV_ETH: Contract = {
   address: "0x0Fef13242390F6bB115Df09D8b5FdC4Bc7D16693",
   symbol: "sUSV",
   decimals: 9,
+  underlyings: [
+    {
+      name: "Universal Store of Value",
+      chain: "ethereum",
+      address: "0x88536C9B2C4701b8dB824e6A16829D5B5Eb84440",
+      symbol: "USV",
+      decimals: 9,
+    },
+  ],
 };
 
 const sUSV_POLYGON: Contract = {
@@ -15,6 +25,15 @@ const sUSV_POLYGON: Contract = {
   address: "0x01D119e2F0441eA442e3ab84e0dBbf04bd993556",
   symbol: "sUSV",
   decimals: 9,
+  underlyings: [
+    {
+      name: "Universal Store of Value",
+      chain: "polygon",
+      address: "0xac63686230f64bdeaf086fe6764085453ab3023f",
+      symbol: "USV",
+      decimals: 9,
+    },
+  ],
 };
 
 const sUSV_AVAX: Contract = {
@@ -23,6 +42,15 @@ const sUSV_AVAX: Contract = {
   address: "0x4022227eBaDa365AeC96FC89E89316E0696C770D",
   symbol: "sUSV",
   decimals: 9,
+  underlyings: [
+    {
+      name: "Universal Store of Value",
+      chain: "avax",
+      address: "0xb0a8E082E5f8d2a04e74372c1bE47737d85A0E73",
+      symbol: "USV",
+      decimals: 9,
+    },
+  ],
 };
 
 const sUSV_BSC: Contract = {
@@ -31,9 +59,18 @@ const sUSV_BSC: Contract = {
   address: "0x238A7349A4815604D33665684E3D41E5E00AA015",
   symbol: "sUSV",
   decimals: 9,
+  underlyings: [
+    {
+      name: "Universal Store of Value",
+      chain: "bsc",
+      address: "0xaf6162DC717CFC8818eFC8d6f46a41Cf7042fCBA",
+      symbol: "USV",
+      decimals: 9,
+    },
+  ],
 };
 
-const getContracts = async () => {
+const getContracts = () => {
   return {
     contracts: { sUSV_ETH, sUSV_POLYGON, sUSV_AVAX, sUSV_BSC },
   };
@@ -43,14 +80,14 @@ const getBalances: GetBalancesHandler<typeof getContracts> = async (
   ctx,
   { sUSV_ETH, sUSV_POLYGON, sUSV_AVAX, sUSV_BSC }
 ) => {
-  const [stakeBalances_ETH, stakeBalances_POLYGON] = await Promise.all([
-    getStakeBalances(ctx, "ethereum", sUSV_ETH || []),
-    getStakeBalances(ctx, "polygon", sUSV_POLYGON || []),
-    getStakeBalances(ctx, "avax", sUSV_AVAX || []),
-    getStakeBalances(ctx, "bsc", sUSV_BSC || []),
-  ]);
-
-  const balances = [...stakeBalances_ETH, ...stakeBalances_POLYGON];
+  const balances = (
+    await Promise.all([
+      getStakeBalance(ctx, "ethereum", sUSV_ETH),
+      getStakeBalance(ctx, "polygon", sUSV_POLYGON),
+      getStakeBalance(ctx, "avax", sUSV_AVAX),
+      getStakeBalance(ctx, "bsc", sUSV_BSC),
+    ])
+  ).filter(isNotNullish);
 
   return {
     balances,
