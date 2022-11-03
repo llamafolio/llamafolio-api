c69e20fd0a1d87f47a81ba2f89957cc8c1d5b057
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2022-11-03 15:34:14 +0100

    Lido: fixes + add arbitrum and optimism (#155)
    * stake on multichain, some refacto & QoL
    
    * update weth on optimism and arbitrum
    
    Co-authored-by: 0xpeluche <0xpeluche@protonmail.com>
    Co-authored-by: 0xsign <0xsign@protonmail.com>

--- src/adapters/lido/index.ts
+++ src/adapters/lido/index.ts
@@ -1,14 +1,49 @@
 import { Adapter, Contract, GetBalancesHandler } from "@lib/adapter";
-import { getERC20BalanceOf } from "@lib/erc20";
 import { Token } from "@lib/token";
-import { getStMaticBalances } from "./balances";
+import {
+  getWStEthStakeBalances,
+  getStEthStakeBalances,
+  getStMaticBalances,
+} from "./balances";
 
-const underlyings: Token = {
+/**
+ *  ========== Underlyings ==========
+ */
+
+const MATIC: Token = {
   address: "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0",
   chain: "ethereum",
   symbol: "MATIC",
   decimals: 18,
 };
+
+const WETH_Ethereum: Token = {
+  address: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+  chain: "ethereum",
+  symbol: "WETH",
+  decimals: 18,
+  coingeckoId: "weth",
+};
+
+const WETH_Arbitrum: Token = {
+  address: "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
+  chain: "ethereum",
+  symbol: "WETH",
+  decimals: 18,
+  coingeckoId: "weth",
+};
+
+const WETH_Optimism: Token = {
+  address: "0x4200000000000000000000000000000000000006",
+  chain: "ethereum",
+  symbol: "WETH",
+  decimals: 18,
+  coingeckoId: "weth",
+};
+
+/**
+ *  ========== Ethereum ==========
+ */
 
 const stETH: Contract = {
   name: "stETH",
@@ -18,6 +53,7 @@ const stETH: Contract = {
   symbol: "stETH",
   decimals: 18,
   coingeckoId: "staked-ether",
+  underlyings: [WETH_Ethereum],
 };
 
 const wstETH: Contract = {
@@ -28,6 +64,7 @@ const wstETH: Contract = {
   symbol: "wstETH",
   decimals: 18,
   coingeckoId: "wrapped-steth",
+  underlyings: [WETH_Ethereum],
 };
 
 const stMATIC: Contract = {
@@ -38,35 +75,77 @@ const stMATIC: Contract = {
   symbol: "stMATIC",
   decimals: 18,
   coingeckoId: "staked-ether",
-  underlyings: [underlyings],
+  underlyings: [MATIC],
 };
+
+/**
+ *  ========== Arbitrum ==========
+ */
+
+const wstETH_Arbitrum: Contract = {
+  name: "wstETH",
+  displayName: "Wrapped liquid staked Ether 2.0",
+  chain: "arbitrum",
+  address: "0x5979D7b546E38E414F7E9822514be443A4800529",
+  symbol: "wstETH",
+  decimals: 18,
+  coingeckoId: "wrapped-steth",
+  underlyings: [WETH_Arbitrum],
+};
+
+/**
+ *  ========== Optimism ==========
+ */
+
+const wstETH_Optimism: Contract = {
+  name: "wstETH",
+  displayName: "Wrapped liquid staked Ether 2.0",
+  chain: "optimism",
+  address: "0x1F32b1c2345538c0c6f582fCB022739c4A194Ebb",
+  symbol: "wstETH",
+  decimals: 18,
+  coingeckoId: "wrapped-steth",
+  underlyings: [WETH_Optimism],
+};
+
+/**
+ *  ==============================
+ */
 
 const getContracts = () => {
   return {
-    contracts: [stETH, wstETH, stMATIC],
+    contracts: { stETH, wstETH, stMATIC, wstETH_Arbitrum, wstETH_Optimism },
   };
 };
 
 const getBalances: GetBalancesHandler<typeof getContracts> = async (
   ctx,
-  contracts
+  { stETH, wstETH, stMATIC, wstETH_Arbitrum, wstETH_Optimism }
 ) => {
-  const ethereumBalances = await getERC20BalanceOf(
-    ctx,
-    "ethereum",
-    contracts.filter((contract) => contract.chain === "ethereum")
-  );
+  const [
+    stakeBalances_wstEth,
+    stakeBalances_stEth,
+    stakeBalances_stMatic,
+    stakeBalances_wstEth_Arbitrum,
+    stakeBalances_wstEth_Optimism,
+  ] = await Promise.all([
+    getWStEthStakeBalances(ctx, "ethereum", wstETH),
+    getStEthStakeBalances(ctx, "ethereum", stETH),
+    getStMaticBalances(ctx, "ethereum", stMATIC),
+    getWStEthStakeBalances(ctx, "arbitrum", wstETH_Arbitrum),
+    getWStEthStakeBalances(ctx, "optimism", wstETH_Optimism),
+  ]);
 
-  const polygonBalances = await getStMaticBalances(
-    ctx,
-    "ethereum",
-    contracts.filter((contract) => contract.chain === "polygon")
-  );
-
-  const balances = [...ethereumBalances, ...polygonBalances];
+  const balances = [
+    ...stakeBalances_wstEth,
+    ...stakeBalances_stEth,
+    ...stakeBalances_stMatic,
+    ...stakeBalances_wstEth_Arbitrum,
+    ...stakeBalances_wstEth_Optimism,
+  ];
 
   return {
-    balances: balances.map((bal) => ({ ...bal, category: "stake" })),
+    balances,
   };
 };
 
