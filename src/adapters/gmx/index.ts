e0f487ba6425d53e2eb785baf767d837ffd88b60
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2022-11-19 12:10:37 +0100

    Gmx enhancements (#169)

--- src/adapters/gmx/index.ts
+++ src/adapters/gmx/index.ts
@@ -1,40 +1,13 @@
-import { Adapter, Contract, GetBalancesHandler } from '@lib/adapter'
+import * as arbitrum from '@adapters/gmx/arbitrum'
+import * as avax from '@adapters/gmx/avax'
+import { Adapter, mergeAdapters } from '@lib/adapter'
 
-import { getStakeBalances } from './balances'
-
-const glpStaker: Contract = {
-  name: 'sGLP',
-  displayName: 'GLP Staker',
-  chain: 'arbitrum',
-  address: '0x1addd80e6039594ee970e5872d247bf0414c8903',
-}
-
-const gmxStaker: Contract = {
-  name: 'sGMX',
-  displayName: 'GMX Staker',
-  chain: 'arbitrum',
-  address: '0x908c4d94d34924765f1edc22a1dd098397c59dd4',
-}
-
-const getContracts = async () => {
-  return {
-    contracts: { stake: [glpStaker, gmxStaker] },
-    revalidate: 60 * 60,
-  }
-}
-
-const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, { stake }) => {
-  const balances = await getStakeBalances(ctx, 'arbitrum', stake || [])
-
-  return {
-    balances,
-  }
-}
+const multiChainAdapter = mergeAdapters({ arbitrum, avax })
 
 const adapter: Adapter = {
   id: 'gmx',
-  getContracts,
-  getBalances,
+  getContracts: multiChainAdapter.getContracts,
+  getBalances: multiChainAdapter.getBalances,
 }
 
 export default adapter
