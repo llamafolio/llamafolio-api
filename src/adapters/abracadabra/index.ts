8c124c4759b3caeb57ff5906df886823ff71aebe
0xsign <99134502+0xsign@users.noreply.github.com>
2022-11-19 10:31:49 +0100

    use mergeAdapters helper (#191)

--- src/adapters/abracadabra/index.ts
+++ src/adapters/abracadabra/index.ts
@@ -1,43 +1,14 @@
 import * as avax from '@adapters/abracadabra/avax'
 import * as ethereum from '@adapters/abracadabra/ethereum'
 import * as fantom from '@adapters/abracadabra/fantom'
-import { Adapter, GetBalancesHandler } from '@lib/adapter'
+import { Adapter, mergeAdapters } from '@lib/adapter'
 
-const getContracts = async () => {
-  const [avaxContracts, ethereumContracts, fantomContracts] = await Promise.all([
-    avax.getContracts(),
-    ethereum.getContracts(),
-    fantom.getContracts(),
-  ])
-
-  return {
-    contracts: {
-      ...avaxContracts.contracts,
-      ...ethereumContracts.contracts,
-      ...fantomContracts.contracts,
-    },
-  }
-}
-
-const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
-  const [avaxBalances, ethereumBalances, fantomBalances] = await Promise.all([
-    avax.getBalances(ctx, contracts),
-    ethereum.getBalances(ctx, contracts),
-    fantom.getBalances(ctx, contracts),
-  ])
-
-  return {
-    ...ethereumBalances,
-    ...avaxBalances,
-    ...fantomBalances,
-    balances: [...ethereumBalances.balances, ...avaxBalances.balances, ...fantomBalances.balances],
-  }
-}
+const multiChainAdapter = mergeAdapters({ avax, ethereum, fantom })
 
 const adapter: Adapter = {
   id: 'abracadabra',
-  getContracts,
-  getBalances,
+  getContracts: multiChainAdapter.getContracts,
+  getBalances: multiChainAdapter.getBalances,
 }
 
 export default adapter
--- src/adapters/granary-finance/index.ts
+++ src/adapters/granary-finance/index.ts
@@ -2,52 +2,14 @@ import * as avax from '@adapters/granary-finance/avax'
 import * as ethereum from '@adapters/granary-finance/ethereum'
 import * as fantom from '@adapters/granary-finance/fantom'
 import * as optimism from '@adapters/granary-finance/optimism'
-import { Adapter, GetBalancesHandler } from '@lib/adapter'
+import { Adapter, mergeAdapters } from '@lib/adapter'
 
-const getContracts = async () => {
-  const [avaxContracts, ethereumContracts, fantomContracts, optimismContracts] = await Promise.all([
-    avax.getContracts(),
-    ethereum.getContracts(),
-    fantom.getContracts(),
-    optimism.getContracts(),
-  ])
-
-  return {
-    contracts: {
-      ...avaxContracts.contracts,
-      ...ethereumContracts.contracts,
-      ...fantomContracts.contracts,
-      ...optimismContracts.contracts,
-    },
-  }
-}
-
-const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
-  const [avaxBalances, ethereumBalances, fantomBalances, optimismBalances] = await Promise.all([
-    avax.getBalances(ctx, contracts),
-    ethereum.getBalances(ctx, contracts),
-    fantom.getBalances(ctx, contracts),
-    optimism.getBalances(ctx, contracts),
-  ])
-
-  return {
-    ...avaxBalances,
-    ...ethereumBalances,
-    ...fantomBalances,
-    ...optimismBalances,
-    balances: [
-      ...avaxBalances.balances,
-      ...ethereumBalances.balances,
-      ...fantomBalances.balances,
-      ...optimismBalances.balances,
-    ],
-  }
-}
+const multiChainAdapter = mergeAdapters({ avax, ethereum, fantom, optimism })
 
 const adapter: Adapter = {
   id: 'granary-finance',
-  getContracts,
-  getBalances,
+  getContracts: multiChainAdapter.getContracts,
+  getBalances: multiChainAdapter.getBalances,
 }
 
 export default adapter
