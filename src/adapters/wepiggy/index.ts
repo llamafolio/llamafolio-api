514a192fcca12b3f1e8b449ece837358b4c2d547
0xpeluche <0xpeluche@protonmail.com>
2022-11-20 11:08:06 +0100

    comment optimism

--- src/adapters/wepiggy/index.ts
+++ src/adapters/wepiggy/index.ts
@@ -1,11 +1,11 @@
 import * as arbitrum from '@adapters/wepiggy/arbitrum'
 import * as bsc from '@adapters/wepiggy/bsc'
 import * as ethereum from '@adapters/wepiggy/ethereum'
-import * as optimism from '@adapters/wepiggy/optimism'
+// import * as optimism from '@adapters/wepiggy/optimism'
 import * as polygon from '@adapters/wepiggy/polygon'
 import { Adapter, mergeAdapters } from '@lib/adapter'
 
-const multiChainAdapter = mergeAdapters({ arbitrum, bsc, ethereum, optimism, polygon })
+const multiChainAdapter = mergeAdapters({ arbitrum, bsc, ethereum, /*optimism,*/ polygon })
 
 const adapter: Adapter = {
   id: 'wepiggy',
