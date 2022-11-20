cb0543c8c8413d5f0979dafe59f7b95860a1ff19
eabz <eabz@kindynos.mx>
2022-11-19 19:02:27 -0600

    Frax

--- src/adapters/fraxlend/index.ts
+++ src/adapters/fraxlend/index.ts
@@ -1,17 +1,10 @@
-import { Adapter, BaseContext, Contract, GetBalancesHandler, GetContractsHandler } from '@lib/adapter'
+import { Adapter, Balance, BaseContext, Contract, GetBalancesHandler, GetContractsHandler } from '@lib/adapter'
 import { Chain } from '@lib/chains'
 import { getERC20Details } from '@lib/erc20'
 import { multicall } from '@lib/multicall'
-import { Token } from '@lib/token'
+import { BigNumber } from 'ethers'
 
-const fraxToken: Token = {
-  chain: 'ethereum',
-  address: '0x853d955aCEf822Db058eb8505911ED77F175b99e',
-  symbol: 'FRAX',
-  decimals: 18,
-}
-
-let pools = [
+const pools = [
   '0x794F6B13FBd7EB7ef10d1ED205c9a416910207Ff',
   '0x32467a5fc2d72D21E8DCe990906547A2b012f382',
   '0xa1D100a5bf6BFd2736837c97248853D989a9ED84',
@@ -20,8 +13,100 @@ let pools = [
   '0x50E627a1DF8D665524942aD7eC6392b6BA60293a',
 ]
 
+export const getLendBorrowBalances = async (ctx: BaseContext, chain: Chain, pools: Contract[]) => {
+  const balances: Balance[] = []
+
+  try {
+    const [userCollateralBalancesRes, poolTokenBalanceRes, poolCollateralTokenRes] = await Promise.all([
+      multicall({
+        chain,
+        calls: pools.map((pool) => ({
+          target: pool.address,
+          params: [ctx.address],
+        })),
+        abi: {
+          inputs: [{ internalType: 'address', name: '', type: 'address' }],
+          name: 'userCollateralBalance',
+          outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+          stateMutability: 'view',
+          type: 'function',
+        },
+      }),
+
+      multicall({
+        chain,
+        calls: pools.map((pool) => ({
+          target: pool.address,
+          params: [ctx.address],
+        })),
+        abi: {
+          inputs: [{ internalType: 'address', name: '', type: 'address' }],
+          name: 'balanceOf',
+          outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+          stateMutability: 'view',
+          type: 'function',
+        },
+      }),
+
+      multicall({
+        chain,
+        calls: pools.map((pool) => ({
+          target: pool.address,
+          params: [],
+        })),
+        abi: {
+          inputs: [],
+          name: 'collateralContract',
+          outputs: [{ internalType: 'contract IERC20', name: '', type: 'address' }],
+          stateMutability: 'view',
+          type: 'function',
+        },
+      }),
+    ])
+
+    const poolCollateralTokensAddress = poolCollateralTokenRes.filter((res) => res.success).map((res) => res.output)
+
+    const poolCollateralTokens = await getERC20Details(chain, poolCollateralTokensAddress)
+
+    const poolTokenBalances = poolTokenBalanceRes.filter((res) => res.success).map((res) => BigNumber.from(res.output))
+
+    const userCollateralBalances = userCollateralBalancesRes
+      .filter((res) => res.success)
+      .map((res) => BigNumber.from(res.output))
+
+    for (let i = 0; i < pools.length; i++) {
+      const asset = pools[i]
+
+      const poolTokenBalance = poolTokenBalances[i]
+
+      const userCollateral = userCollateralBalances[i]
+
+      const collateralToken = poolCollateralTokens[i]
+
+      console.log(userCollateral.toString(), poolTokenBalance.toString())
+      const supply: Balance = {
+        chain,
+        decimals: asset.decimals,
+        symbol: asset.symbol,
+        address: asset.address,
+        amount: poolTokenBalance,
+        category: 'lend',
+        underlyings: [{ amount: userCollateral, ...collateralToken }],
+      }
+
+      balances.push(supply)
+    }
+
+    return balances
+  } catch (error) {
+    console.log('Failed to get lend/borrow balance')
+
+    return []
+  }
+}
+
 const getPools = async (chain: Chain): Promise<Contract[]> => {
-  let contracts: Contract[] = []
+  const contracts: Contract[] = []
 
   try {
     const underlyingToken = await multicall({
@@ -81,7 +166,9 @@ const getContracts: GetContractsHandler = async () => {
 }
 
 const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx: BaseContext, { poolsContracts }) => {
-  return { balances: [] }
+  const poolTokenBalances = await getLendBorrowBalances(ctx, 'ethereum', poolsContracts as Contract[])
+
+  return { balances: [...poolTokenBalances] }
 }
 
 const adapter: Adapter = {
