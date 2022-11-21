d10c20b3c302a478de104f0f93576f87d9e9c7ea
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2022-11-21 18:18:38 +0100

    Vector fixes (#196)

--- src/adapters/vector/avax/index.ts
+++ src/adapters/vector/avax/index.ts
@@ -1,23 +1,37 @@
 import { Contract, GetBalancesHandler } from '@lib/adapter'
 import { resolveBalances } from '@lib/balance'
 
-import { getLockerBalances } from '../common/balances'
+import { getFarmBalances, getFarmContracts } from '../common/farm'
+import { getLockerBalances } from '../common/locker'
 
 const vtxLocker: Contract = {
-  name: 'vectorLocker',
+  name: 'Locked VTX',
   displayName: 'VTX Locker',
   chain: 'avax',
-  address: '0x574679Ec54972cf6d705E0a71467Bb5BB362919D',
+  address: '0xf99264cbf9652824b3412fa21e8cbeb69c3ea0a7',
+  decimals: 18,
+  symbol: 'LVTXv2',
 }
 
-export const getContracts = () => {
+const masterChef: Contract = {
+  name: 'Vector MasterChef',
+  chain: 'avax',
+  address: '0x423D0FE33031aA4456a17b150804aA57fc157d97',
+}
+
+export const getContracts = async () => {
+  const farmContracts = await getFarmContracts('avax', masterChef)
+
   return {
-    contracts: { vtxLocker },
+    contracts: { vtxLocker, farmContracts },
   }
 }
 
 export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
-  const balances = await resolveBalances<typeof getContracts>(ctx, 'avax', contracts, { vtxLocker: getLockerBalances })
+  const balances = await resolveBalances<typeof getContracts>(ctx, 'avax', contracts, {
+    farmContracts: (...args) => getFarmBalances(...args, masterChef),
+    vtxLocker: getLockerBalances,
+  })
 
   return {
     balances,
