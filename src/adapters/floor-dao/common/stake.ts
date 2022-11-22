fb3d2dce69979b593933db360e5447bf202b2e88
0xpeluche <0xpeluche@protonmail.com>
2022-11-22 09:39:32 +0100

    floor-dao

---
+++ src/adapters/floor-dao/common/stake.ts
@@ -0,0 +1,83 @@
+import { Balance, Contract } from '@lib/adapter'
+import { BaseContext } from '@lib/adapter'
+import { call } from '@lib/call'
+import { Chain } from '@lib/chains'
+import { abi } from '@lib/erc20'
+import { BigNumber } from 'ethers/lib/ethers'
+
+const FLOOR: Contract = {
+  name: 'Floor',
+  chain: 'ethereum',
+  address: '0xf59257E961883636290411c11ec5Ae622d19455e',
+  decimals: 9,
+  symbol: 'FLOOR ',
+}
+
+export async function getStakeBalances(ctx: BaseContext, chain: Chain, contract: Contract): Promise<Balance[]> {
+  const balances: Balance[] = []
+
+  const balanceOfRes = await call({
+    chain,
+    target: contract.address,
+    params: [ctx.address],
+    abi: abi.balanceOf,
+  })
+
+  const amount = BigNumber.from(balanceOfRes.output)
+
+  balances.push({
+    chain,
+    address: contract.address,
+    symbol: contract.symbol,
+    decimals: 9,
+    amount,
+    underlyings: [{ ...FLOOR, amount }],
+    category: 'stake',
+  })
+
+  return balances
+}
+
+export async function getFormattedStakeBalances(
+  ctx: BaseContext,
+  chain: Chain,
+  contract: Contract,
+): Promise<Balance[]> {
+  const balances: Balance[] = []
+
+  const balanceOfRes = await call({
+    chain,
+    target: contract.address,
+    params: [ctx.address],
+    abi: abi.balanceOf,
+  })
+
+  const balanceOf = balanceOfRes.output
+
+  const formattedBalanceOfRes = await call({
+    chain,
+    target: contract.address,
+    params: [balanceOf],
+    abi: {
+      inputs: [{ internalType: 'uint256', name: '_amount', type: 'uint256' }],
+      name: 'balanceFrom',
+      outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+      stateMutability: 'view',
+      type: 'function',
+    },
+  })
+
+  const formattedBalanceOf = BigNumber.from(formattedBalanceOfRes.output)
+
+  balances.push({
+    chain,
+    address: contract.address,
+    symbol: contract.symbol,
+    decimals: 9,
+    amount: formattedBalanceOf,
+    underlyings: [{ ...FLOOR, amount: formattedBalanceOf }],
+    category: 'stake',
+  })
+
+  return balances
+}
---
+++ src/adapters/floor-dao/ethereum/index.ts
@@ -0,0 +1,47 @@
+import { Contract, GetBalancesHandler } from '@lib/adapter'
+import { resolveBalances } from '@lib/balance'
+
+import { getFormattedStakeBalances, getStakeBalances } from '../common/stake'
+
+const FLOOR: Contract = {
+  name: 'Floor',
+  chain: 'ethereum',
+  address: '0xf59257E961883636290411c11ec5Ae622d19455e',
+  decimals: 9,
+  symbol: 'FLOOR ',
+}
+
+const sFLOOR: Contract = {
+  name: 'staked FLOOR',
+  chain: 'ethereum',
+  address: '0x164afe96912099543bc2c48bb9358a095db8e784',
+  decimals: 18,
+  symbol: 'sFLOOR',
+  underlyings: [FLOOR],
+}
+
+const gFLOOR: Contract = {
+  name: 'Governance FLOOR',
+  chain: 'ethereum',
+  address: '0xb1cc59fc717b8d4783d41f952725177298b5619d',
+  decimals: 18,
+  symbol: 'gFLOOR',
+  underlyings: [FLOOR],
+}
+
+export const getContracts = () => {
+  return {
+    contracts: { sFLOOR, gFLOOR },
+  }
+}
+
+export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
+  const balances = await resolveBalances<typeof getContracts>(ctx, 'ethereum', contracts, {
+    sFLOOR: getStakeBalances,
+    gFLOOR: getFormattedStakeBalances,
+  })
+
+  return {
+    balances,
+  }
+}
