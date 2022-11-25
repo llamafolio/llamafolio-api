e3a4b2700cdb69bdd160e8de42de6e2176202118
0xsign <0xsign@protonmail.com>
2022-11-25 01:16:36 +0100

    get balances response

--- src/handlers/getBalances.ts
+++ src/handlers/getBalances.ts
@@ -1,33 +1,27 @@
 import { selectBalancesByFromAddress } from '@db/balances'
+import { selectLastBalancesSnapshotsByFromAddress } from '@db/balances-snapshots'
 import pool from '@db/pool'
 import { badRequest, serverError, success } from '@handlers/response'
-import { Balance, PricedBalance } from '@lib/adapter'
+import { PricedBalance } from '@lib/adapter'
+import { groupBy } from '@lib/array'
 import { isHex } from '@lib/buf'
+import { Chain } from '@lib/chains'
 import { APIGatewayProxyHandler } from 'aws-lambda'
 
-export type AdapterBalance = Balance & { adapterId: string }
-export type PricedAdapterBalance = PricedBalance & { adapterId: string }
-
-export interface AdapterBalancesResponse {
-  id: string
-  data: PricedAdapterBalance[]
+interface BalancesProtocolChainResponse {
+  id: Chain
+  balances: PricedBalance[]
+  healthFactor?: number
 }
 
-function groupBalancesByAdapter(balances: PricedAdapterBalance[]): AdapterBalancesResponse[] {
-  const balancesByAdapterId: {
-    [key: string]: AdapterBalancesResponse
-  } = {}
-  for (const balance of balances) {
-    if (!balancesByAdapterId[balance.adapterId]) {
-      balancesByAdapterId[balance.adapterId] = {
-        id: balance.adapterId,
-        data: [],
-      }
-    }
-    balancesByAdapterId[balance.adapterId].data.push(balance)
-  }
+export interface BalancesProtocolResponse {
+  id: string
+  chains: BalancesProtocolChainResponse[]
+}
 
-  return Object.values(balancesByAdapterId)
+export interface BalancesResponse {
+  updatedAt: string
+  protocols: BalancesProtocolResponse[]
 }
 
 export const handler: APIGatewayProxyHandler = async (event, context) => {
@@ -44,12 +38,45 @@ export const handler: APIGatewayProxyHandler = async (event, context) => {
   const client = await pool.connect()
 
   try {
-    const pricedBalances = await selectBalancesByFromAddress(client, address)
+    const [pricedBalances, lastBalancesSnapshots] = await Promise.all([
+      selectBalancesByFromAddress(client, address),
+      selectLastBalancesSnapshotsByFromAddress(client, address),
+    ])
 
-    const data = groupBalancesByAdapter(pricedBalances)
-    const updatedAt = data[0]?.data?.[0].timestamp
+    const protocols: BalancesProtocolResponse[] = []
+    const balancesByAdapterId = groupBy(pricedBalances, 'adapterId')
+    const balancesSnapshotsByAdapterId = groupBy(lastBalancesSnapshots, 'adapterId')
 
-    return success({ updatedAt, data })
+    for (const adapterId in balancesByAdapterId) {
+      const balancesByChain = groupBy(balancesByAdapterId[adapterId], 'chain')
+      const balancesSnapshotsByChain = groupBy(balancesSnapshotsByAdapterId[adapterId] || [], 'chain')
+
+      const chains: BalancesProtocolChainResponse[] = []
+
+      for (const chain in balancesByChain) {
+        const balanceSnapshot = balancesSnapshotsByChain[chain]?.[0]
+
+        chains.push({
+          id: chain as Chain,
+          balances: balancesByChain[chain],
+          healthFactor: balanceSnapshot?.healthFactor,
+        })
+      }
+
+      protocols.push({
+        id: adapterId,
+        chains,
+      })
+    }
+
+    const updatedAt = pricedBalances[0]?.timestamp
+
+    const balancesResponse: BalancesResponse = {
+      updatedAt,
+      protocols,
+    }
+
+    return success(balancesResponse)
   } catch (error) {
     console.error('Failed to retrieve balances', { error, address })
     return serverError('Failed to retrieve balances')
