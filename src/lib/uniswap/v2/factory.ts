b34e24acb877ca7fd90f6d1eb6bcfdc17bf45091
0xsign <0xsign@protonmail.com>
2022-11-26 15:54:22 +0100

    refactor uniswap v2 factory

--- src/lib/uniswap/v2/factory.ts
+++ src/lib/uniswap/v2/factory.ts
@@ -1,15 +1,50 @@
 import { Contract } from '@lib/adapter'
 import { range } from '@lib/array'
+import { call } from '@lib/call'
 import { Category } from '@lib/category'
 import { Chain } from '@lib/chains'
-import { getERC20Details } from '@lib/erc20'
-import { multicall } from '@lib/multicall'
-import { providers } from '@lib/providers'
-import { Token } from '@lib/token'
+import { resolveERC20Details } from '@lib/erc20'
+import { Call, multicall } from '@lib/multicall'
 import { isNotNullish } from '@lib/type'
-import { ethers } from 'ethers'
 
-import UniswapV2Factory from './abis/UniswapV2Factory.json'
+const abi = {
+  allPairsLength: {
+    constant: true,
+    inputs: [],
+    name: 'allPairsLength',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  allPairs: {
+    constant: true,
+    inputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    name: 'allPairs',
+    outputs: [{ internalType: 'address', name: '', type: 'address' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  token0: {
+    constant: true,
+    inputs: [],
+    name: 'token0',
+    outputs: [{ internalType: 'address', name: '', type: 'address' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  token1: {
+    constant: true,
+    inputs: [],
+    name: 'token1',
+    outputs: [{ internalType: 'address', name: '', type: 'address' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+}
 
 export interface getPairsContractsParams {
   chain: Chain
@@ -18,15 +53,14 @@ export interface getPairsContractsParams {
   length?: number
 }
 
-export async function getPairsContracts({
-  chain,
-  factoryAddress,
-  length,
-}: getPairsContractsParams): Promise<Contract[]> {
-  const provider = providers[chain]
-  const factory = new ethers.Contract(factoryAddress, UniswapV2Factory, provider)
+export async function getPairsContracts({ chain, factoryAddress, length }: getPairsContractsParams) {
+  const allPairsLengthRes = await call({
+    chain,
+    abi: abi.allPairsLength,
+    target: factoryAddress,
+  })
 
-  let allPairsLength = (await factory.allPairsLength()).toNumber()
+  let allPairsLength = parseInt(allPairsLengthRes.output)
   if (length !== undefined) {
     allPairsLength = Math.min(allPairsLength, length)
   }
@@ -34,125 +68,57 @@ export async function getPairsContracts({
   const allPairsRes = await multicall({
     chain,
     calls: range(0, allPairsLength).map((_, i) => ({
-      target: factory.address,
+      target: factoryAddress,
       params: [i],
     })),
-    abi: {
-      constant: true,
-      inputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-      name: 'allPairs',
-      outputs: [{ internalType: 'address', name: '', type: 'address' }],
-      payable: false,
-      stateMutability: 'view',
-      type: 'function',
-    },
+    abi: abi.allPairs,
   })
 
-  const pairs: Contract[] = allPairsRes
-    .filter((res) => res.success)
-    .map((res) => ({ chain, address: res.output.toLowerCase(), category: 'lp' }))
+  const contracts: Contract[] = allPairsRes.filter((res) => res.success).map((res) => ({ chain, address: res.output }))
 
-  return getPairsDetails(chain, pairs)
+  return getPairsDetails(chain, contracts)
 }
 
-export async function getPairsDetails(chain: Chain, contracts: Contract[]) {
+export async function getPairsDetails(chain: Chain, contracts: Contract[]): Promise<Contract[]> {
   const addresses = contracts.map((contract) => contract.address)
 
-  const [pairs, token0sRes, token1sRes] = await Promise.all([
-    getERC20Details(chain, addresses),
+  const calls: Call[] = addresses.map((address) => ({
+    target: address,
+    params: [],
+  }))
 
+  const [token0sRes, token1sRes] = await Promise.all([
     multicall({
       chain,
-      calls: addresses.map((address) => ({
-        target: address,
-        params: [],
-      })),
-      abi: {
-        constant: true,
-        inputs: [],
-        name: 'token0',
-        outputs: [{ internalType: 'address', name: '', type: 'address' }],
-        payable: false,
-        stateMutability: 'view',
-        type: 'function',
-      },
+      calls,
+      abi: abi.token0,
     }),
 
     multicall({
       chain,
-      calls: addresses.map((address) => ({
-        target: address,
-        params: [],
-      })),
-      abi: {
-        constant: true,
-        inputs: [],
-        name: 'token1',
-        outputs: [{ internalType: 'address', name: '', type: 'address' }],
-        payable: false,
-        stateMutability: 'view',
-        type: 'function',
-      },
+      calls,
+      abi: abi.token1,
     }),
   ])
 
-  const token0Addresses = token0sRes.filter((res) => res.success).map((res) => res.output.toLowerCase())
+  const { pairs, token0s, token1s } = await resolveERC20Details(chain, {
+    pairs: calls.map((res) => res.target),
+    token0s: token0sRes.map((res) => res.output),
+    token1s: token1sRes.map((res) => res.output),
+  })
 
-  const token1Addresses = token1sRes.filter((res) => res.success).map((res) => res.output.toLowerCase())
-
-  const [token0s, token1s] = await Promise.all([
-    getERC20Details(chain, token0Addresses),
-    getERC20Details(chain, token1Addresses),
-  ])
-
-  // map token0 and token1 to their pairs
-  const underlyingsByPairAddress: {
-    [key: string]: { token0?: string; token1?: string }
-  } = {}
-  const token0ByAddress: { [key: string]: Token } = {}
-  const token1ByAddress: { [key: string]: Token } = {}
-
-  for (const token0Res of token0sRes) {
-    if (token0Res.success) {
-      if (!underlyingsByPairAddress[token0Res.input.target]) {
-        underlyingsByPairAddress[token0Res.input.target] = {}
-      }
-      underlyingsByPairAddress[token0Res.input.target].token0 = token0Res.output.toLowerCase()
-    }
-  }
-
-  for (const token1Res of token1sRes) {
-    if (token1Res.success) {
-      if (!underlyingsByPairAddress[token1Res.input.target]) {
-        underlyingsByPairAddress[token1Res.input.target] = {}
-      }
-      underlyingsByPairAddress[token1Res.input.target].token1 = token1Res.output.toLowerCase()
-    }
-  }
-
-  for (const token0 of token0s) {
-    token0ByAddress[token0.address] = token0
-  }
-
-  for (const token1 of token1s) {
-    token1ByAddress[token1.address] = token1
-  }
-
-  return pairs
-    .map((pair) => {
-      const underlyings = underlyingsByPairAddress[pair.address]
-      if (!underlyings?.token0 || !underlyings?.token1) {
+  return contracts
+    .map((contract, i) => {
+      if (!pairs[i].success || !token0s[i].success || !token1s[i].success) {
         return null
       }
 
-      const token0 = token0ByAddress[underlyings.token0]
-      const token1 = token0ByAddress[underlyings.token1]
-
-      if (!token0 || !token1) {
-        return null
-      }
+      const pair = pairs[i].output!
+      const token0 = token0s[i].output!
+      const token1 = token1s[i].output!
 
       return {
+        ...contract,
         ...pair,
         category: 'lp' as Category,
         underlyings: [token0, token1],
