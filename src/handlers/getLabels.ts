b04546dc59ed222b5a8e27d9b4b4633ef3fcc8bb
eabz <eabz@kindynos.mx>
2022-11-29 19:21:58 -0600

    Extend labels with twitter handles

--- src/handlers/getLabels.ts
+++ src/handlers/getLabels.ts
@@ -1,4 +1,6 @@
 import { success } from '@handlers/response'
+import twitterLabels from '@labels/twitter-labels.json'
+import { providers } from '@lib/providers'
 import { getLabel } from '@llamafolio/labels'
 import { APIGatewayProxyHandler } from 'aws-lambda'
 
@@ -9,11 +11,22 @@ export const handler: APIGatewayProxyHandler = async (event) => {
   const addresses = event.pathParameters?.address?.split(',') ?? []
   const data: { [key: string]: any } = {}
 
+  const provider = providers['ethereum']
+
   for (const address of addresses) {
     const label = getLabel(address.toLowerCase())
 
     if (label) {
       data[address] = label
+    }
+
+    const ensName = await provider.lookupAddress(address)
+    if (ensName) {
+      const twitter = (twitterLabels as { name: string; handle: string }[]).find((twitter) => twitter.name === ensName)
+
+      if (twitter) {
+        data[address].links = { ...data[address].links, twitter: `https://twitter.com/${twitter.handle}` }
+      }
     }
   }
 
