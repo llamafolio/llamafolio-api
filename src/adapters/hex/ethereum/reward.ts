d60611e96cc3e04a2b3b33b9e3bf35ff699bce5d
0xpeluche <0xpeluche@protonmail.com>
2022-11-30 09:33:09 +0100

    Hex

---
+++ src/adapters/hex/ethereum/reward.ts
@@ -0,0 +1,111 @@
+import { Contract } from '@lib/adapter'
+import { call } from '@lib/call'
+import { Chain } from '@lib/chains'
+import { sumBN } from '@lib/math'
+import { BigNumber } from 'ethers'
+
+interface stakeAndIndexParams {
+  stakeId: number
+  stake: string
+  share: string
+  stakedDays: string
+  lockedDays: string
+}
+
+interface decodedDataParams {
+  payout: bigint
+  shares: bigint
+  sats: bigint
+}
+
+export async function getRewardsBalances(chain: Chain, contract: Contract, stakesAndIndexes: stakeAndIndexParams[]) {
+  const rewards: BigNumber[] = []
+
+  for (const stakeAndIndex of stakesAndIndexes) {
+    const reward = await getInterestForRangeDays(chain, contract, stakeAndIndex)
+
+    rewards.push(reward)
+  }
+  return rewards
+}
+
+const HEX_DECODER = (encodedDailyData: string) => {
+  const HEARTS_UINT_SHIFT = BigInt(72)
+  const HEARTS_MASK = (BigInt(1) << HEARTS_UINT_SHIFT) - BigInt(1)
+  const SATS_UINT_SHIFT = BigInt(56)
+  const SATS_MASK = (BigInt(1) << SATS_UINT_SHIFT) - BigInt(1)
+
+  let v = BigInt(encodedDailyData)
+  const payout = v & HEARTS_MASK
+  v = v >> HEARTS_UINT_SHIFT
+  const shares = v & HEARTS_MASK
+  v = v >> HEARTS_UINT_SHIFT
+  const sats = v & SATS_MASK
+  return { payout, shares, sats }
+}
+
+const getDecodedDataFromRangeDays = async (chain: Chain, contract: Contract, stakeAndIndex: stakeAndIndexParams) => {
+  const data = []
+
+  const currentDayRes = await call({
+    chain,
+    target: contract.address,
+    params: [],
+    abi: {
+      constant: true,
+      inputs: [],
+      name: 'currentDay',
+      outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function',
+    },
+  })
+
+  const today = currentDayRes.output
+
+  const dataRangeRes = await call({
+    chain,
+    target: contract.address,
+    params: [stakeAndIndex.lockedDays, today],
+    abi: {
+      constant: true,
+      inputs: [
+        { internalType: 'uint256', name: 'beginDay', type: 'uint256' },
+        { internalType: 'uint256', name: 'endDay', type: 'uint256' },
+      ],
+      name: 'dailyDataRange',
+      outputs: [{ internalType: 'uint256[]', name: 'list', type: 'uint256[]' }],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function',
+    },
+  })
+
+  const dataRange = dataRangeRes.output
+
+  for (let i = 0; i < dataRange.length; i++) {
+    const dailyData = dataRange[i]
+
+    data.push(HEX_DECODER(dailyData))
+  }
+  return data
+}
+
+const getInterestForRangeDays = async (chain: Chain, contract: Contract, stakeAndIndex: stakeAndIndexParams) => {
+  const interest: BigNumber[] = []
+
+  const decodedData: decodedDataParams[] = await getDecodedDataFromRangeDays(chain, contract, stakeAndIndex)
+
+  for (let i = 0; i < decodedData.length; i++) {
+    const dailyDecodedData = decodedData[i]
+
+    interest.push(
+      BigNumber.from(dailyDecodedData.payout)
+        .mul(BigNumber.from(stakeAndIndex.share))
+        .div(BigNumber.from(dailyDecodedData.shares)),
+    )
+  }
+
+  return sumBN(interest)
+}
---
+++ src/adapters/hex/ethereum/stake.ts
@@ -0,0 +1,91 @@
+import { Balance, BaseContext, Contract } from '@lib/adapter'
+import { range } from '@lib/array'
+import { call } from '@lib/call'
+import { Chain } from '@lib/chains'
+import { sumBN } from '@lib/math'
+import { multicall } from '@lib/multicall'
+
+import { getRewardsBalances } from './reward'
+
+export async function getStakeBalances(ctx: BaseContext, chain: Chain, contract: Contract): Promise<Balance[]> {
+  const balances: Balance[] = []
+
+  const stakeCountRes = await call({
+    chain,
+    target: contract.address,
+    params: [ctx.address],
+    abi: {
+      constant: true,
+      inputs: [{ internalType: 'address', name: 'stakerAddr', type: 'address' }],
+      name: 'stakeCount',
+      outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function',
+    },
+  })
+
+  const findStakesAndIndexesRes = await multicall({
+    chain,
+    calls: range(0, stakeCountRes.output).map((i) => ({
+      target: contract.address,
+      params: [ctx.address, i],
+    })),
+    abi: {
+      constant: true,
+      inputs: [
+        { internalType: 'address', name: '', type: 'address' },
+        { internalType: 'uint256', name: '', type: 'uint256' },
+      ],
+      name: 'stakeLists',
+      outputs: [
+        { internalType: 'uint40', name: 'stakeId', type: 'uint40' },
+        { internalType: 'uint72', name: 'stakedHearts', type: 'uint72' },
+        { internalType: 'uint72', name: 'stakeShares', type: 'uint72' },
+        { internalType: 'uint16', name: 'lockedDay', type: 'uint16' },
+        { internalType: 'uint16', name: 'stakedDays', type: 'uint16' },
+        { internalType: 'uint16', name: 'unlockedDay', type: 'uint16' },
+        { internalType: 'bool', name: 'isAutoStake', type: 'bool' },
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function',
+    },
+  })
+
+  const stakesAndIndexes = findStakesAndIndexesRes
+    .filter((res) => res.success)
+    .map((res, i) => ({
+      id: i,
+      stakeId: res.output.stakeId,
+      stake: res.output.stakedHearts,
+      share: res.output.stakeShares,
+      stakedDays: res.output.stakedDays,
+      lockedDays: res.output.lockedDay,
+    }))
+
+  const amount = sumBN(stakesAndIndexes.map((balance) => balance.stake))
+
+  const rewards = await getRewardsBalances(chain, contract, stakesAndIndexes)
+  const totalRewards = /*amount.add*/ sumBN(rewards)
+
+  balances.push({
+    chain,
+    decimals: contract.decimals,
+    symbol: contract.symbol,
+    address: contract.address,
+    amount,
+    rewards: [
+      {
+        chain,
+        decimals: contract.decimals,
+        symbol: contract.symbol,
+        address: contract.address,
+        amount: totalRewards,
+      },
+    ],
+    category: 'stake',
+  })
+
+  return balances
+}
