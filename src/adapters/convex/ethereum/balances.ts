30bbf3d124084c8bc7c670967606910bb2648f67
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2022-12-09 19:12:15 +0100

    Convex fixes (#241)

---
+++ src/adapters/convex/ethereum/balances.ts
@@ -0,0 +1,37 @@
+import { Balance, BaseContext, Contract } from '@lib/adapter'
+import { Chain } from '@lib/chains'
+import { getUnderlyingsBalancesInPool } from '@lib/convex/underlyings'
+import { getERC20BalanceOf } from '@lib/erc20'
+import { Token } from '@lib/token'
+
+import { getCRVCVXRewards } from './rewards'
+
+export async function getPoolsBalances(ctx: BaseContext, chain: Chain, contracts: Contract[]) {
+  const balances: Balance[] = []
+  const nonZeroPools: Contract[] = (await getERC20BalanceOf(ctx, chain, contracts as Token[])).filter((pool) =>
+    pool.amount.gt(0),
+  )
+
+  for (const nonZeroPool of nonZeroPools) {
+    const underlyiedPoolBalances = await getUnderlyingsBalancesInPool(
+      chain,
+      nonZeroPool,
+      nonZeroPool.lpToken,
+      nonZeroPool.poolAddress,
+    )
+
+    const rewardsBalances = (nonZeroPool.rewards = await getCRVCVXRewards(ctx, chain, nonZeroPool))
+
+    balances.push({
+      chain,
+      address: nonZeroPool.address,
+      symbol: underlyiedPoolBalances.map((underlying) => underlying.symbol).join('-'),
+      decimals: nonZeroPool.decimals,
+      amount: nonZeroPool.amount,
+      underlyings: underlyiedPoolBalances,
+      rewards: rewardsBalances,
+      category: 'stake',
+    })
+  }
+  return balances
+}
