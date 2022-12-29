f80fc7af4d2d110f3fe7b2e45202f8fa513e8433
0xsign <0xsign@protonmail.com>
2022-12-29 19:59:54 +0100

    fix: adapters contracts props are chain specific

--- src/handlers/updateBalances.ts
+++ src/handlers/updateBalances.ts
@@ -6,19 +6,23 @@ import { getAllContractsInteractions, groupContracts } from '@db/contracts'
 import { getAllTokensInteractions } from '@db/contracts'
 import pool from '@db/pool'
 import { badRequest, serverError, success } from '@handlers/response'
-import { Balance, BalancesConfig, BalancesContext, Contract } from '@lib/adapter'
-import { keyBy } from '@lib/array'
+import { Balance, BalancesConfig, BalancesContext, PricedBalance } from '@lib/adapter'
+import { groupBy, groupBy2, keyBy2 } from '@lib/array'
 import { sanitizeBalances, sumBalances } from '@lib/balance'
 import { isHex, strToBuf } from '@lib/buf'
-import { Chain, chains } from '@lib/chains'
+import { Chain } from '@lib/chains'
 import { getPricedBalances } from '@lib/price'
 import { isNotNullish } from '@lib/type'
 import { APIGatewayProxyEvent, APIGatewayProxyHandler } from 'aws-lambda'
 import format from 'pg-format'
 
-interface ExtendedBalance extends Balance {
-  adapterId: string
-}
+type ExtendedBalance =
+  | (Balance & {
+      adapterId: string
+    })
+  | (PricedBalance & {
+      adapterId: string
+    })
 
 interface ExtendedBalancesConfig extends BalancesConfig {
   adapterId: string
@@ -51,72 +55,64 @@ export const handler: APIGatewayProxyHandler = async (event, context) => {
       getAllTokensInteractions(client, address),
     ])
 
-    const contractsByAdapterId: { [key: string]: Contract[] } = {}
-    for (const contract of contracts) {
-      if (!contract.adapterId) {
-        console.error(`Missing adapterId in contract`, contract)
-        continue
-      }
-      if (!contractsByAdapterId[contract.adapterId]) {
-        contractsByAdapterId[contract.adapterId] = []
-      }
-      contractsByAdapterId[contract.adapterId].push(contract)
-    }
-    contractsByAdapterId['wallet'] = tokens
+    const contractsByAdapterIdChain = groupBy2(contracts, 'adapterId', 'chain')
+    contractsByAdapterIdChain['wallet'] = groupBy(tokens, 'chain')
 
-    const adapterIds = Object.keys(contractsByAdapterId)
-    const adaptersProps = await selectAdaptersProps(client, adapterIds)
-    const adaptersPropsById = keyBy(adaptersProps, 'id')
+    const adapterIds = Object.keys(contractsByAdapterIdChain)
+    // list of all [adapterId, chain]
+    const adapterIdsChains = adapterIds.flatMap((adapterId) =>
+      Object.keys(contractsByAdapterIdChain[adapterId]).map((chain) => [adapterId, chain] as [string, Chain]),
+    )
+    const adaptersProps = await selectAdaptersProps(client, adapterIdsChains)
+    const adaptersPropsByIdChain = keyBy2(adaptersProps, 'id', 'chain')
 
     console.log('Interacted with protocols:', adapterIds)
 
     // Run adapters `getBalances` only with the contracts the user interacted with
     const adaptersBalancesConfigsRes = await Promise.all(
-      adapterIds.flatMap((adapterId) => {
+      adapterIdsChains.map(async ([adapterId, chain]) => {
         const adapter = adapterById[adapterId]
         if (!adapter) {
           console.error(`Could not find adapter with id`, adapterId)
-          return []
+          return
+        }
+        const handler = adapter[chain]
+        if (!handler) {
+          console.error(`Could not find chain handler for`, [adapterId, chain])
+          return
         }
 
-        return chains
-          .filter((chain) => adapter[chain.id])
-          .map(async (chain) => {
-            const handler = adapter[chain.id]!
+        try {
+          const hrstart = process.hrtime()
 
-            try {
-              const hrstart = process.hrtime()
+          const contracts = groupContracts(contractsByAdapterIdChain[adapterId][chain]) || []
+          const props = adaptersPropsByIdChain[adapterId]?.[chain]?.contractsProps || {}
 
-              const contracts =
-                groupContracts(contractsByAdapterId[adapterId].filter((contract) => contract.chain === chain.id)) || []
-              const props = adaptersPropsById[adapterId]?.contractsProps || {}
+          const ctx: BalancesContext = { address, chain, adapterId }
 
-              const ctx: BalancesContext = { address, chain: chain.id, adapterId }
+          const balancesConfig = await handler.getBalances(ctx, contracts, props)
 
-              const balancesConfig = await handler.getBalances(ctx, contracts, props)
+          const hrend = process.hrtime(hrstart)
 
-              const hrend = process.hrtime(hrstart)
+          console.log(
+            `[${adapterId}][${chain}] getBalances ${contractsByAdapterIdChain[adapterId][chain].length} contracts, found ${balancesConfig.balances.length} balances in %ds %dms`,
+            hrend[0],
+            hrend[1] / 1000000,
+          )
 
-              console.log(
-                `[${adapterId}][${chain.id}] getBalances ${contractsByAdapterId[adapterId].length} contracts, found ${balancesConfig.balances.length} balances in %ds %dms`,
-                hrend[0],
-                hrend[1] / 1000000,
-              )
+          const extendedBalancesConfig: ExtendedBalancesConfig = {
+            ...balancesConfig,
+            // Tag balances with adapterId
+            balances: balancesConfig.balances.map((balance) => ({ ...balance, adapterId })),
+            adapterId,
+            chain,
+          }
 
-              const extendedBalancesConfig: ExtendedBalancesConfig = {
-                ...balancesConfig,
-                // Tag balances with adapterId
-                balances: balancesConfig.balances.map((balance) => ({ ...balance, adapterId })),
-                adapterId,
-                chain: chain.id,
-              }
-
-              return extendedBalancesConfig
-            } catch (error) {
-              console.error(`[${adapterId}][${chain.id}]: Failed to getBalances`, error)
-              return
-            }
-          })
+          return extendedBalancesConfig
+        } catch (error) {
+          console.error(`[${adapterId}][${chain}]: Failed to getBalances`, error)
+          return
+        }
       }),
     )
 
@@ -140,16 +136,10 @@ export const handler: APIGatewayProxyHandler = async (event, context) => {
     )
 
     // Group balances back by adapter
-    const pricedBalancesByAdapterId: { [key: string]: any[] } = {}
-    for (const _pricedBalance of pricedBalances) {
-      const pricedBalance = _pricedBalance as ExtendedBalance
-      if (pricedBalance.adapterId) {
-        if (!pricedBalancesByAdapterId[pricedBalance.adapterId]) {
-          pricedBalancesByAdapterId[pricedBalance.adapterId] = []
-        }
-        pricedBalancesByAdapterId[pricedBalance.adapterId].push(pricedBalance)
-      }
-    }
+    const pricedBalancesByAdapterId = groupBy(
+      (pricedBalances as ExtendedBalance[]).filter((pricedBalance) => pricedBalance.adapterId),
+      'adapterId',
+    )
 
     const now = new Date()
 
@@ -188,7 +178,7 @@ export const handler: APIGatewayProxyHandler = async (event, context) => {
     // TODO: insert all at once
     await Promise.all(
       Object.keys(pricedBalancesByAdapterId).map((adapterId) =>
-        insertBalances(client, pricedBalancesByAdapterId[adapterId], adapterId, address, now),
+        insertBalances(client, pricedBalancesByAdapterId[adapterId] as PricedBalance[], adapterId, address, now),
       ),
     )
 
