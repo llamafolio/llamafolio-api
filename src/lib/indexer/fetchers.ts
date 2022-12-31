ba96527c5d27d9249607e93d4b01acbdb16459b3
eabz <eabz@kindynos.mx>
2022-12-31 11:57:54 -0700

    Remove history and revert changes

--- src/lib/indexer/fetchers.ts
+++ src/lib/indexer/fetchers.ts
@@ -1,30 +1,10 @@
 import request from 'graphql-request'
 
-import {
-  getContractsInteractedQuery,
-  getTokensDetailsQuery,
-  getTokensInteractedQuery,
-  getTransactionHistoryQuery,
-} from './queries'
-import { IndexerContractsInteracted, IndexerTokenInteraction, IndexerTransaction } from './types'
+import { getContractsInteractedQuery, getTokensDetailsQuery, getTokensInteractedQuery } from './queries'
+import { IndexerContractsInteracted, IndexerTokenInteraction } from './types'
 
 export const indexer_graph = async (query: string, variables = {}, headers = {}) =>
   request('https://indexer.kindynos.mx/v1/graphql', query, variables, headers)
-
-export const getTransactionHistory = async (
-  address: string,
-  limit: number,
-  offset: number,
-  chainsFilter: string[],
-  protocolsFilter: string[],
-  variables = {},
-  headers = {},
-): Promise<{ txs: IndexerTransaction[]; txs_aggregate: { aggregate: { count: number } } }> =>
-  indexer_graph(
-    getTransactionHistoryQuery(address.toLowerCase(), limit, offset, chainsFilter, protocolsFilter),
-    variables,
-    headers,
-  )
 
 export const getTokensInteracted = async (
   address: string,
