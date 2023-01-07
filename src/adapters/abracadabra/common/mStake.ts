a8eee3b1682232ccc432af06fb3fbcb83e973ace
0xsign <0xsign@protonmail.com>
2023-01-07 14:22:37 +0100

    refactor: abracadabra stake fetch erc20 details

--- src/adapters/abracadabra/common/mStake.ts
+++ src/adapters/abracadabra/common/mStake.ts
@@ -1,46 +1,52 @@
 import { Balance, BalancesContext, BaseContext, Contract } from '@lib/adapter'
 import { call } from '@lib/call'
-import { getERC20Details } from '@lib/erc20'
 import { BigNumber } from 'ethers'
+
+const abi = {
+  mim: {
+    inputs: [],
+    name: 'mim',
+    outputs: [{ internalType: 'contract ERC20', name: '', type: 'address' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  spell: {
+    inputs: [],
+    name: 'spell',
+    outputs: [{ internalType: 'contract ERC20', name: '', type: 'address' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  userInfo: {
+    inputs: [{ internalType: 'address', name: '', type: 'address' }],
+    name: 'userInfo',
+    outputs: [
+      { internalType: 'uint128', name: 'amount', type: 'uint128' },
+      { internalType: 'uint128', name: 'rewardDebt', type: 'uint128' },
+      { internalType: 'uint128', name: 'lastAdded', type: 'uint128' },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  pendingReward: {
+    inputs: [{ internalType: 'address', name: '_user', type: 'address' }],
+    name: 'pendingReward',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+}
 
 export async function getMStakeContract(ctx: BaseContext, contract: Contract): Promise<Contract> {
   const [underlyingTokenAddressRes, rewardTokenAddressRes] = await Promise.all([
-    call({
-      ctx,
-      target: contract.address,
-      params: [],
-      abi: {
-        inputs: [],
-        name: 'spell',
-        outputs: [{ internalType: 'contract ERC20', name: '', type: 'address' }],
-        stateMutability: 'view',
-        type: 'function',
-      },
-    }),
-
-    call({
-      ctx,
-      target: contract.address,
-      params: [],
-      abi: {
-        inputs: [],
-        name: 'mim',
-        outputs: [{ internalType: 'contract ERC20', name: '', type: 'address' }],
-        stateMutability: 'view',
-        type: 'function',
-      },
-    }),
-  ])
-
-  const [underlyings, rewards] = await Promise.all([
-    getERC20Details(ctx, [underlyingTokenAddressRes.output]),
-    getERC20Details(ctx, [rewardTokenAddressRes.output]),
+    call({ ctx, target: contract.address, params: [], abi: abi.spell }),
+    call({ ctx, target: contract.address, params: [], abi: abi.mim }),
   ])
 
   const stakeContract: Contract = {
     ...contract,
-    underlyings,
-    rewards,
+    underlyings: [underlyingTokenAddressRes.output],
+    rewards: [rewardTokenAddressRes.output],
   }
 
   return stakeContract
@@ -56,30 +62,14 @@ export async function getMStakeBalance(ctx: BalancesContext, contract: Contract)
       ctx,
       target: contract.address,
       params: [ctx.address],
-      abi: {
-        inputs: [{ internalType: 'address', name: '', type: 'address' }],
-        name: 'userInfo',
-        outputs: [
-          { internalType: 'uint128', name: 'amount', type: 'uint128' },
-          { internalType: 'uint128', name: 'rewardDebt', type: 'uint128' },
-          { internalType: 'uint128', name: 'lastAdded', type: 'uint128' },
-        ],
-        stateMutability: 'view',
-        type: 'function',
-      },
+      abi: abi.userInfo,
     }),
 
     call({
       ctx,
       target: contract.address,
       params: [ctx.address],
-      abi: {
-        inputs: [{ internalType: 'address', name: '_user', type: 'address' }],
-        name: 'pendingReward',
-        outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-        stateMutability: 'view',
-        type: 'function',
-      },
+      abi: abi.pendingReward,
     }),
   ])
 
