a86ad99bd44135b5be949fa2c62584cc721c406a
0xpeluche <0xpeluche@protonmail.com>
2023-01-19 15:17:08 +0100

    univ2 + univ3 fetch pairs

--- src/adapters/uniswap/ethereum/index.ts
+++ src/adapters/uniswap/ethereum/index.ts
@@ -1,15 +1,25 @@
 import { BalancesContext, Contract, GetBalancesHandler } from '@lib/adapter'
 import { getPairsBalances } from '@lib/uniswap/v2/pair'
+import tokenlists from '@llamafolio/tokens/ethereum/tokenlist.json'
+import fs from 'fs'
 import { gql, request } from 'graphql-request'
 
-async function getPoolsHighestVolume() {
+import uniPairsV2 from './uniPairs_v2.json'
+
+async function getPoolsHighestVolume(address: string) {
   const contracts: Contract[] = []
 
-  const url = 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswapv2'
+  // const url = 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswapv2'
+  const url = 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3'
 
   const query = gql`
-    query pairsQuery {
-      pairs(first: 500, orderBy: volumeUSD, orderDirection: desc) {
+    query pairs {
+      pools(
+        first: 1000
+        orderBy: volumeUSD
+        orderDirection: desc
+        where: { token0_: {id: "${address}" } }
+      ) {
         id
         token0 {
           id
@@ -27,7 +37,7 @@ async function getPoolsHighestVolume() {
 
   const res = await request(url, query)
 
-  for (const pair of res.pairs) {
+  for (const pair of res.pools) {
     if (!pair.id || !pair.token0?.id || !pair.token1?.id) {
       continue
     }
@@ -35,8 +45,8 @@ async function getPoolsHighestVolume() {
     contracts.push({
       chain: 'ethereum',
       address: pair.id.toLowerCase(),
-      name: 'Uniswap V2',
-      symbol: 'UNIV2',
+      name: 'Uniswap V3',
+      symbol: 'UNIV3',
       decimals: 18,
       underlyings: [
         {
@@ -58,8 +68,35 @@ async function getPoolsHighestVolume() {
   return contracts
 }
 
+const getPairsFromGQL = async () => {
+  const pairs: Contract[] = []
+
+  const tokenListAddresses = tokenlists.map((token) => token.address)
+
+  for (let tokenListIdx = 0; tokenListIdx < tokenListAddresses.length; tokenListIdx++) {
+    const tokenListAddress = tokenListAddresses[tokenListIdx]
+    const pair = await getPoolsHighestVolume(tokenListAddress)
+
+    pairs.push(...pair)
+  }
+
+  const saveData = (data: any, file: any) => {
+    const finished = (error: any) => {
+      if (error) {
+        console.error(error)
+        return
+      }
+    }
+
+    const jsonData = JSON.stringify(data, null, 2)
+    fs.writeFile(file, jsonData, finished)
+  }
+
+  saveData(pairs, 'uniPairs_v3.json')
+}
+
 export const getContracts = async () => {
-  const pairs = await getPoolsHighestVolume()
+  const pairs = uniPairsV2 as Contract[]
 
   return {
     contracts: { pairs },
