277ab6e0f44977b2a057b1876d716dee290f240e
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2023-01-30 15:03:50 +0100

    Apeswap (#311)
    * apeswap_farm_lp
    
    * update build
    
    ---------
    
    Co-authored-by: 0xpeluche <0xpeluche@protonmail.com>
    Co-authored-by: 0xsign <0xsign@protonmail.com>

--- src/adapters/apeswap-amm/polygon/index.ts
+++ src/adapters/apeswap-amm/polygon/index.ts
@@ -1,7 +1,21 @@
-import { BaseContext, GetBalancesHandler } from '@lib/adapter'
+import { BalancesContext, BaseContext, Contract, GetBalancesHandler } from '@lib/adapter'
 import { resolveBalances } from '@lib/balance'
-import { getPairsContracts } from '@lib/uniswap/v2/factory'
+import { getMasterChefPoolsBalances } from '@lib/masterchef/masterchef'
+import { Token } from '@lib/token'
+import { getPairsContracts, Pair } from '@lib/uniswap/v2/factory'
 import { getPairsBalances } from '@lib/uniswap/v2/pair'
+
+const banana: Token = {
+  chain: 'polygon',
+  address: '0x5d47baba0d66083c52009271faf3f50dcc01023c',
+  decimals: 18,
+  symbol: 'BANANA',
+}
+
+const miniChef: Contract = {
+  chain: 'polygon',
+  address: '0x54aff400858dcac39797a81894d9920f16972d1d',
+}
 
 export const getContracts = async (ctx: BaseContext, props: any) => {
   const offset = props.pairOffset || 0
@@ -15,7 +29,7 @@ export const getContracts = async (ctx: BaseContext, props: any) => {
   })
 
   return {
-    contracts: { pairs },
+    contracts: { miniChef, pairs },
     revalidate: 60 * 60,
     revalidateProps: {
       pairOffset: Math.min(offset + limit, allPairsLength),
@@ -23,9 +37,23 @@ export const getContracts = async (ctx: BaseContext, props: any) => {
   }
 }
 
+function getApeswapBalances(
+  ctx: BalancesContext,
+  pairs: Pair[],
+  masterchef: Contract,
+  rewardToken: Token,
+  rewardTokenName?: string,
+  lpTokenAbi?: boolean,
+) {
+  return Promise.all([
+    getPairsBalances(ctx, pairs),
+    getMasterChefPoolsBalances(ctx, pairs, masterchef, rewardToken, rewardTokenName, lpTokenAbi),
+  ])
+}
+
 export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
   const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
-    pairs: getPairsBalances,
+    pairs: (...args) => getApeswapBalances(...args, miniChef, banana, 'Banana', true),
   })
 
   return {
