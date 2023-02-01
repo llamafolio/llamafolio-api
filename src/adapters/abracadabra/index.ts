a35758d612d72b6214247a22b77111611f7beb02
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2023-02-01 13:50:43 +0100

    Fix: Abracadabra (#316)
    * Fix on contracts function
    
    * chain
    
    * update submodules
    
    ---------
    
    Co-authored-by: 0xpeluche <0xpeluche@protonmail.com>
    Co-authored-by: 0xsign <0xsign@protonmail.com>

--- src/adapters/abracadabra/index.ts
+++ src/adapters/abracadabra/index.ts
@@ -1,3 +1,4 @@
+import * as arbitrum from '@adapters/abracadabra/arbitrum'
 import * as avax from '@adapters/abracadabra/avax'
 import * as ethereum from '@adapters/abracadabra/ethereum'
 import * as fantom from '@adapters/abracadabra/fantom'
@@ -8,9 +9,11 @@ const adapter: Adapter = {
   avax,
   ethereum,
   fantom,
+  arbitrum,
 }
 
 // TODO: healthfactor
+// Abracadabra is restructuring its docs to be more readable for developpers
 // https://docs.abracadabra.money/
 
 export default adapter
