cf78c1e78d99e055365a6a303ec9e7be2ccfb975
0xpeluche <0xpeluche@protonmail.com>
2023-02-02 13:57:20 +0100

    rm

--- src/adapters/alchemix/index.ts
+++ src/adapters/alchemix/index.ts
@@ -1,14 +1,10 @@
 import { Adapter } from '@lib/adapter'
 
 import * as ethereum from './ethereum'
-import * as fantom from './fantom'
-import * as optimism from './optimism'
 
 const adapter: Adapter = {
   id: 'alchemix',
   ethereum,
-  optimism,
-  fantom,
 }
 
 export default adapter
