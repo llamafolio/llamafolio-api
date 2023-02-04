9823db63390fe9a0b50db0dd00c9675fccf0d290
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2023-02-04 11:26:11 +0100

    feat: Set-protocol (#328)
    * store contracts, farm balances
    
    * small refacto
    
    * fix: simplify contracts and reuse lib/pools to get balances
    
    ---------
    
    Co-authored-by: 0xpeluche <0xpeluche@protonmail.com>
    Co-authored-by: 0xsign <0xsign@protonmail.com>

---
+++ src/adapters/set-protocol/ethereum/contract.ts
@@ -0,0 +1,48 @@
+import { BaseContext, Contract } from '@lib/adapter'
+import { call } from '@lib/call'
+import { multicall } from '@lib/multicall'
+import { isSuccess } from '@lib/type'
+
+const abi = {
+  getSets: {
+    inputs: [],
+    name: 'getSets',
+    outputs: [{ internalType: 'address[]', name: '', type: 'address[]' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  getComponents: {
+    inputs: [],
+    name: 'getComponents',
+    outputs: [{ internalType: 'address[]', name: '', type: 'address[]' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+}
+
+export async function getSetProtocolPools(ctx: BaseContext, controller: Contract): Promise<Contract[]> {
+  const pools: Contract[] = []
+
+  const setsContractsRes = await call({ ctx, target: controller.address, params: [], abi: abi.getSets })
+
+  const setsUnderlyingsRes = await multicall({
+    ctx,
+    calls: setsContractsRes.output.map((target: string) => ({ target })),
+    abi: abi.getComponents,
+  })
+
+  for (let idx = 0; idx < setsContractsRes.output.length; idx++) {
+    const underlyingsRes = setsUnderlyingsRes[idx]
+    if (!isSuccess(underlyingsRes)) {
+      continue
+    }
+
+    pools.push({
+      chain: ctx.chain,
+      address: setsContractsRes.output[idx],
+      underlyings: underlyingsRes.output,
+    })
+  }
+
+  return pools
+}
---
+++ src/adapters/set-protocol/ethereum/index.ts
@@ -0,0 +1,28 @@
+import { BaseContext, Contract, GetBalancesHandler } from '@lib/adapter'
+import { resolveBalances } from '@lib/balance'
+import { getPoolsBalances } from '@lib/pools'
+
+import { getSetProtocolPools } from './contract'
+
+const controller: Contract = {
+  chain: 'ethereum',
+  address: '0xa4c8d221d8BB851f83aadd0223a8900A6921A349',
+}
+
+export const getContracts = async (ctx: BaseContext) => {
+  const pools = await getSetProtocolPools(ctx, controller)
+
+  return {
+    contracts: { pools, controller },
+  }
+}
+
+export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
+  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
+    pools: (ctx, pools) => getPoolsBalances(ctx, pools, { getPoolAddress: (pool) => pool.address }),
+  })
+
+  return {
+    balances,
+  }
+}
