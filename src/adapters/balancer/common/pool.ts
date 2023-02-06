4f459f27b192162793edfc42fd2bebca6762e0e9
0xpeluche <0xpeluche@protonmail.com>
2023-02-06 16:06:11 +0100

    add id

--- src/adapters/balancer/common/pool.ts
+++ src/adapters/balancer/common/pool.ts
@@ -6,13 +6,14 @@ export async function getBalancerPools(ctx: BaseContext, url: string): Promise<C
 
   const query = gql`
     query pools {
-      pools(first: 1000, where: { totalLiquidity_gt: "100" }, orderBy: totalLiquidity, orderDirection: desc) {
+      pools(first: 1000, orderBy: totalLiquidity, orderDirection: desc, where: { totalLiquidity_gt: "100" }) {
+        id
         address
         symbol
         tokens {
-          address
-          symbol
           decimals
+          symbol
+          address
         }
       }
     }
@@ -20,13 +21,14 @@ export async function getBalancerPools(ctx: BaseContext, url: string): Promise<C
   const res = await request(url, query)
 
   for (const pool of res.pools) {
-    if (!pool.address || !pool.tokens) {
+    if (!pool.address || !pool.tokens || !pool.id) {
       continue
     }
 
     contracts.push({
       chain: ctx.chain,
       address: pool.address,
+      id: pool.id,
       symbol: pool.symbol,
       decimals: 18,
       underlyings: pool.tokens.map((underlying: Contract) => ({ ...underlying, chain: ctx.chain })),
