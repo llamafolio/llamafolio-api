e1a555d2f17a3794a4f036ea35d1c4b6b707794f
0xpeluche <0xpeluche@protonmail.com>
2023-02-17 20:49:40 +0100

    contract, farm, bsc, avax, arb

---
+++ src/adapters/synapse/common/contract.ts
@@ -0,0 +1,90 @@
+import { BaseContext, Contract } from '@lib/adapter'
+import { call } from '@lib/call'
+import { Call, multicall } from '@lib/multicall'
+import { isSuccess } from '@lib/type'
+
+const abi = {
+  poolLength: {
+    inputs: [],
+    name: 'poolLength',
+    outputs: [{ internalType: 'uint256', name: 'pools', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  lpToken: {
+    inputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    name: 'lpToken',
+    outputs: [{ internalType: 'contract IERC20', name: '', type: 'address' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  rewarder: {
+    inputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    name: 'rewarder',
+    outputs: [{ internalType: 'contract IRewarder', name: '', type: 'address' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  getToken: {
+    inputs: [{ internalType: 'uint8', name: 'index', type: 'uint8' }],
+    name: 'getToken',
+    outputs: [{ internalType: 'contract IERC20', name: '', type: 'address' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+}
+
+export async function getSynapseContracts(ctx: BaseContext, miniChef: Contract): Promise<Contract[]> {
+  const pools: Contract[] = []
+
+  const poolLengthRes = await call({ ctx, target: miniChef.address, params: [], abi: abi.poolLength })
+  const poolLength = parseInt(poolLengthRes.output)
+
+  const calls: Call[] = []
+  for (let idx = 0; idx < poolLength; idx++) {
+    calls.push({ target: miniChef.address, params: [idx] })
+  }
+
+  const [lpTokensRes, rewardersRes] = await Promise.all([
+    multicall({ ctx, calls, abi: abi.lpToken }),
+    multicall({ ctx, calls, abi: abi.rewarder }),
+  ])
+
+  for (let poolIdx = 0; poolIdx < poolLength; poolIdx++) {
+    const lpTokenRes = lpTokensRes[poolIdx]
+    const rewarderRes = rewardersRes[poolIdx]
+    const rewards = miniChef.rewards?.[0] as Contract
+
+    if (!isSuccess(lpTokenRes) || !isSuccess(rewarderRes)) {
+      continue
+    }
+
+    pools.push({
+      chain: ctx.chain,
+      address: lpTokenRes.output,
+      pid: poolIdx,
+      pool: miniChef.pool[poolIdx],
+      underlyings: [],
+      rewarder: rewarderRes.output,
+      rewards: [rewards],
+    })
+  }
+
+  return getUnderlyingsFromSynapsePool(ctx, pools)
+}
+
+async function getUnderlyingsFromSynapsePool(ctx: BaseContext, contracts: Contract[]): Promise<Contract[]> {
+  for (const contract of contracts) {
+    const pool = contract.pool
+    const calls = Array.from({ length: 4 }, (_, i) => ({ target: pool, params: [i] }))
+    const underlyingsTokensRes = await multicall({ ctx, calls, abi: abi.getToken })
+
+    underlyingsTokensRes.map((underlyingsTokenRes) => {
+      if (isSuccess(underlyingsTokenRes)) {
+        contract.underlyings?.push(underlyingsTokenRes.output)
+      }
+    })
+  }
+
+  return contracts
+}
