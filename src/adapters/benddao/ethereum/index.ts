56b0d0b59b75a2c423cce3df2f1aa61bd5eb5a6e
0xpeluche <0xpeluche@protonmail.com>
2023-03-01 15:42:56 +0100

    benddao stake lend borrow nft lock rewards

--- src/adapters/benddao/ethereum/index.ts
+++ src/adapters/benddao/ethereum/index.ts
@@ -1,10 +1,10 @@
-import { Balance, BalancesContext, BaseContext, Contract, GetBalancesHandler } from '@lib/adapter'
+import { BaseContext, Contract, GetBalancesHandler } from '@lib/adapter'
 import { resolveBalances } from '@lib/balance'
-import { getERC20BalanceOf } from '@lib/erc20'
-import { Token } from '@lib/token'
+import { groupBy } from 'lodash'
 
+import { getBendBalances } from './balance'
 import { getBendDaoLocker } from './locker'
-import { getNftBalances, getNftContracts } from './nft'
+import { getNftBalances, getNftContracts, getNFTHealthFactor, NFTBorrowBalance } from './nft'
 
 const veBend: Contract = {
   chain: 'ethereum',
@@ -20,6 +20,12 @@ const bendWeth: Contract = {
   symbol: 'bendWETH',
   decimals: 18,
   underlyings: ['0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'],
+  rewarder: '0x26FC1f11E612366d3367fc0cbFfF9e819da91C8d',
+}
+
+const lendPool: Contract = {
+  chain: 'ethereum',
+  address: '0x70b97A0da65C15dfb0FFA02aEE6FA36e507C2762',
 }
 
 const registry: Contract = {
@@ -27,26 +33,32 @@ const registry: Contract = {
   address: '0x79d922DD382E42A156bC0A354861cDBC4F09110d',
 }
 
+const apeStaker: Contract = {
+  chain: 'ethereum',
+  address: '0xDAFCe4AcC2703A24F29d1321AdAADF5768F54642',
+  underlyings: ['0x4d224452801ACEd8B2F0aebE155379bb5D594381'],
+}
+
 export const getContracts = async (ctx: BaseContext) => {
   const nfts = await getNftContracts(ctx, registry)
 
   return {
-    contracts: { bendWeth, veBend, registry, nfts },
+    contracts: { bendWeth, veBend, registry, nfts, lendPool, apeStaker },
   }
-}
-
-async function getBendBalances(ctx: BalancesContext, contract: Contract): Promise<Balance[]> {
-  return (await getERC20BalanceOf(ctx, [contract] as Token[])).map((res) => ({ ...res, category: 'farm' }))
 }
 
 export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
   const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
     bendWeth: getBendBalances,
     veBend: getBendDaoLocker,
-    nfts: getNftBalances,
+    nfts: (...args) => getNftBalances(...args, lendPool, apeStaker),
   })
+
+  const sortedBalances = groupBy(balances, '__key')
+  const healthFactor = await getNFTHealthFactor((sortedBalances.nfts as NFTBorrowBalance[]) || [])
 
   return {
     balances,
+    // healthFactor,
   }
 }
--- src/adapters/benddao/ethereum/nft.ts
+++ src/adapters/benddao/ethereum/nft.ts
@@ -3,8 +3,10 @@ import { call } from '@lib/call'
 import { abi as erc20Abi } from '@lib/erc20'
 import { isZero } from '@lib/math'
 import { Call, multicall } from '@lib/multicall'
+import { Token } from '@lib/token'
 import { isSuccess } from '@lib/type'
-import { BigNumber, utils } from 'ethers'
+import { BigNumber, ethers } from 'ethers'
+import { groupBy } from 'lodash'
 
 const abi = {
   getBNFTAssetList: {
@@ -47,10 +49,67 @@ const abi = {
     stateMutability: 'view',
     type: 'function',
   },
+  getNftDebtData: {
+    inputs: [
+      { internalType: 'address', name: 'nftAsset', type: 'address' },
+      { internalType: 'uint256', name: 'nftTokenId', type: 'uint256' },
+    ],
+    name: 'getNftDebtData',
+    outputs: [
+      { internalType: 'uint256', name: 'loanId', type: 'uint256' },
+      { internalType: 'address', name: 'reserveAsset', type: 'address' },
+      { internalType: 'uint256', name: 'totalCollateral', type: 'uint256' },
+      { internalType: 'uint256', name: 'totalDebt', type: 'uint256' },
+      { internalType: 'uint256', name: 'availableBorrows', type: 'uint256' },
+      { internalType: 'uint256', name: 'healthFactor', type: 'uint256' },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  getStakedProxies: {
+    inputs: [
+      { internalType: 'address', name: 'nftAsset', type: 'address' },
+      { internalType: 'uint256', name: 'tokenId', type: 'uint256' },
+    ],
+    name: 'getStakedProxies',
+    outputs: [{ internalType: 'address[]', name: '', type: 'address[]' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  totalStaked: {
+    inputs: [
+      { internalType: 'contract IStakeProxy', name: 'proxy', type: 'address' },
+      { internalType: 'address', name: 'staker', type: 'address' },
+    ],
+    name: 'totalStaked',
+    outputs: [{ internalType: 'uint256', name: 'amount', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  claimable: {
+    inputs: [
+      { internalType: 'contract IStakeProxy', name: 'proxy', type: 'address' },
+      { internalType: 'address', name: 'staker', type: 'address' },
+    ],
+    name: 'claimable',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
 }
 
-interface NFTBalances extends Balance {
-  uri: string
+const weth: Token = {
+  chain: 'ethereum',
+  address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+  decimals: 18,
+  symbol: 'WETH',
+}
+
+const ape: Token = {
+  chain: 'ethereum',
+  address: '0x4d224452801ACEd8B2F0aebE155379bb5D594381',
+  decimals: 18,
+  symbol: 'APE',
 }
 
 export async function getNftContracts(ctx: BaseContext, registry: Contract): Promise<Contract[]> {
@@ -91,59 +150,170 @@ export async function getNftContracts(ctx: BaseContext, registry: Contract): Pro
   return nfts
 }
 
-export async function getNftBalances(ctx: BalancesContext, nfts: Contract[]): Promise<Balance[]> {
-  const balances: Contract[] = []
-  const nftBalances: NFTBalances[] = []
+interface NFTBalance extends Balance {
+  nftId?: string
+  proxy?: string
+}
 
-  const balancesOfsRes = await multicall({
-    ctx,
-    calls: nfts.map((nft) => ({ target: nft.proxy, params: [ctx.address] })),
-    abi: erc20Abi.balanceOf,
+export interface NFTBorrowBalance extends Balance {
+  healthfactor: number | undefined
+}
+
+export async function getNftBalances(
+  ctx: BalancesContext,
+  nfts: Contract[],
+  lendPool: Contract,
+  apeStaker: Contract,
+): Promise<Balance[]> {
+  const nftContracts: Contract[] = []
+  const nftContractDetails: Contract[] = []
+
+  // Get number of nft contracts owned by the user
+  const balancesOfCalls: Call[] = nfts.map((nft) => ({ target: nft.proxy, params: [ctx.address] }))
+  const balancesOfResults = await multicall({ ctx, calls: balancesOfCalls, abi: erc20Abi.balanceOf })
+
+  // Get token ids owned by the user for each nft contract
+  const tokenOfOwnerCalls: Call[] = []
+  balancesOfResults.forEach((res, idx) => {
+    if (isSuccess(res) && !isZero(res.output)) {
+      const nftContract = nfts[idx]
+
+      for (let i = 0; i < res.output; i++) {
+        nftContracts.push({ ...nftContract, amount: 1 })
+        tokenOfOwnerCalls.push({ target: nftContract.proxy, params: [ctx.address, i] })
+      }
+    }
   })
 
-  const calls: Call[] = []
-  for (let nftIdx = 0; nftIdx < nfts.length; nftIdx++) {
-    const nft = nfts[nftIdx]
-    const balancesOfRes = balancesOfsRes[nftIdx]
+  const nftsOwnedIdxRes = await multicall({ ctx, calls: tokenOfOwnerCalls, abi: abi.tokenOfOwnerByIndex })
 
-    if (!isSuccess(balancesOfRes) || isZero(balancesOfRes.output)) {
-      continue
-    }
-
-    for (let index = 0; index < balancesOfRes.output; index++) {
-      balances.push({ ...nft, amount: 1, underlyings: undefined, rewards: undefined, id: index })
-      calls.push({ target: nft.proxy, params: [ctx.address, index] })
-    }
-  }
-
-  const nftsOwnedIdxRes = await multicall({ ctx, calls, abi: abi.tokenOfOwnerByIndex })
-
+  // Get token URIs for each token id
   const nftURIs = await multicall({
     ctx,
-    // @ts-ignore
-    calls: nftsOwnedIdxRes.map((res) => ({ target: res.input.target, params: isSuccess(res) ? [res.output] : null })),
+    calls: nftsOwnedIdxRes.map((res) => (isSuccess(res) ? { target: res.input.target, params: [res.output] } : null)),
     abi: abi.tokenURI,
   })
 
-  for (let index = 0; index < balances.length; index++) {
-    const balance = balances[index]
-    const nftsOwnedIdx = nftsOwnedIdxRes[index]
-    const nftURI = nftURIs[index]
+  nftContracts.forEach((balance, idx) => {
+    const nftOwnedIdxRes = nftsOwnedIdxRes[idx]
+    const nftURI = nftURIs[idx]
 
-    if (!isSuccess(nftsOwnedIdx) || !isSuccess(nftURI)) {
+    if (isSuccess(nftOwnedIdxRes) && isSuccess(nftURI)) {
+      nftContractDetails.push({
+        ...balance,
+        symbol: balance.symbol,
+        nftId: nftOwnedIdxRes.output,
+        uri: nftURI.output,
+      })
+    }
+  })
+
+  return getNFTLendBorrowBalances(ctx, nftContractDetails, lendPool, apeStaker)
+}
+
+const getNFTLendBorrowBalances = async (
+  ctx: BalancesContext,
+  nfts: Contract[],
+  lendPool: Contract,
+  apeStaker: Contract,
+): Promise<NFTBalance[]> => {
+  const nftLendBalances: Balance[] = []
+  const nftBorrowBalances: NFTBorrowBalance[] = []
+
+  const calls: Call[] = nfts.map((nft) => ({ target: lendPool.address, params: [nft.address, nft.nftId] }))
+  const debtBalancesOfsRes = await multicall({ ctx, calls, abi: abi.getNftDebtData })
+
+  nfts.forEach((nft, idx) => {
+    const debtBalancesOfRes = debtBalancesOfsRes[idx]
+
+    if (isSuccess(debtBalancesOfRes)) {
+      nftLendBalances.push({
+        ...nft,
+        amount: BigNumber.from(debtBalancesOfRes.output.totalCollateral),
+        underlyings: [weth],
+        rewards: undefined,
+        category: 'lend',
+      })
+
+      nftBorrowBalances.push({
+        ...nft,
+        amount: BigNumber.from(debtBalancesOfRes.output.totalDebt),
+        underlyings: [weth],
+        rewards: undefined,
+        healthfactor: undefined,
+        category: 'borrow',
+      })
+
+      for (const nftBorrowBalance of nftBorrowBalances) {
+        if (!ethers.constants.MaxUint256.eq(debtBalancesOfRes.output.healthFactor)) {
+          nftBorrowBalance.healthfactor = debtBalancesOfRes.output.healthFactor / Math.pow(10, 18)
+        }
+      }
+    }
+  })
+
+  return apeStakingBalances(ctx, [...nftLendBalances, ...nftBorrowBalances], apeStaker)
+}
+
+const apeStakingBalances = async (
+  ctx: BalancesContext,
+  nftsBalances: NFTBalance[],
+  apeStaker: Contract,
+): Promise<Balance[]> => {
+  const apeBalances: Balance[] = []
+  const sortedNFTs = groupBy(nftsBalances, 'category')
+
+  const stakedProxiesRes = await multicall({
+    ctx,
+    calls: sortedNFTs.lend.map((nft) =>
+      nft.nftId ? { target: apeStaker.address, params: [nft.address, nft.nftId] } : null,
+    ),
+    abi: abi.getStakedProxies,
+  })
+
+  const calls: Call[] = stakedProxiesRes.map((proxy) => ({
+    target: apeStaker.address,
+    params:
+      isSuccess(proxy) && proxy.output.length >= 1
+        ? [proxy.output[0], ctx.address]
+        : [ethers.constants.AddressZero, ctx.address],
+  }))
+
+  const [totalStakedBalancesRes, claimablesRes] = await Promise.all([
+    multicall({ ctx, calls, abi: abi.totalStaked }),
+    multicall({ ctx, calls, abi: abi.claimable }),
+  ])
+
+  for (let nftIdx = 0; nftIdx < sortedNFTs.lend.length; nftIdx++) {
+    const totalStakedBalanceRes = totalStakedBalancesRes[nftIdx]
+    const claimableRes = claimablesRes[nftIdx]
+
+    if (!isSuccess(totalStakedBalanceRes) || !isSuccess(claimableRes)) {
       continue
     }
 
-    nftBalances.push({
-      ...balance,
-      symbol: `${balance.symbol} - #${nftsOwnedIdx.output}`,
-      amount: BigNumber.from(balance.amount).mul(utils.parseEther('1.0')),
-      underlyings: undefined,
-      uri: nftURI.output,
-      rewards: undefined,
+    apeBalances.push({
+      ...apeStaker,
+      symbol: ape.symbol,
+      decimals: ape.decimals,
+      amount: BigNumber.from(totalStakedBalanceRes.output),
+      underlyings: [ape],
+      rewards: [{ ...ape, amount: BigNumber.from(claimableRes.output) }],
       category: 'stake',
     })
   }
 
-  return nftBalances
+  return [...nftsBalances, ...apeBalances]
+}
+
+export async function getNFTHealthFactor(nftsBalances: NFTBorrowBalance[]): Promise<number[]> {
+  const healthfactor: number[] = []
+
+  for (const nftsBalance of nftsBalances) {
+    if (nftsBalance.healthfactor) {
+      healthfactor.push(nftsBalance.healthfactor)
+    }
+  }
+
+  return healthfactor
 }
