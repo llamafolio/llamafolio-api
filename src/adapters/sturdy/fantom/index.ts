9381d48e1c33548e4273a5573e69771cbae38724
0xpeluche <0xpeluche@protonmail.com>
2023-03-03 12:33:16 +0100

    sturdy ftm

--- src/adapters/sturdy/fantom/index.ts
+++ src/adapters/sturdy/fantom/index.ts
@@ -1,14 +1,25 @@
-import { BaseContext, GetBalancesHandler } from '@lib/adapter'
+import { getLendingPoolBalances, getLendingPoolContracts } from '@lib/aave/v2/lending'
+import { BaseContext, Contract, GetBalancesHandler } from '@lib/adapter'
 import { resolveBalances } from '@lib/balance'
 
+const lendingPool: Contract = {
+  name: 'Lending Pool',
+  address: '0x7FF2520Cd7b76e8C49B5DB51505b842d665f3e9A',
+  chain: 'fantom',
+}
+
 export const getContracts = async (ctx: BaseContext) => {
+  const pools = await getLendingPoolContracts(ctx, lendingPool)
+
   return {
-    contracts: {},
+    contracts: { pools },
   }
 }
 
 export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
-  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {})
+  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
+    pools: getLendingPoolBalances,
+  })
 
   return {
     balances,
