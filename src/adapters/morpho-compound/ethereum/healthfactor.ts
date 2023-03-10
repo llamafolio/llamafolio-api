b4687ac3db86d76ec6a610151d03c22821699cad
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2023-03-10 18:14:37 +0100

    fix: morpho compound (#403)
    Co-authored-by: 0xpeluche <0xpeluche@protonmail.com>

---
+++ src/adapters/morpho-compound/ethereum/healthfactor.ts
@@ -0,0 +1,37 @@
+import { BalancesContext, Contract } from '@lib/adapter'
+import { call } from '@lib/call'
+import { ethers } from 'ethers'
+
+const abi = {
+  getUserHealthFactor: {
+    inputs: [
+      { internalType: 'address', name: '_user', type: 'address' },
+      { internalType: 'address[]', name: '_updatedMarkets', type: 'address[]' },
+    ],
+    name: 'getUserHealthFactor',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+}
+export async function getUserHealthFactor(
+  ctx: BalancesContext,
+  morphoLens: Contract,
+  markets: Contract[],
+): Promise<number | undefined> {
+  const marketsAddresses: string[] = markets.map((res) => res.address)
+
+  const userHealthFactorRes = await call({
+    ctx,
+    target: morphoLens.address,
+    //@ts-ignore
+    params: [ctx.address, marketsAddresses],
+    abi: abi.getUserHealthFactor,
+  })
+
+  if (ethers.constants.MaxUint256.eq(userHealthFactorRes.output)) {
+    return
+  }
+
+  return userHealthFactorRes.output / 1e18
+}
