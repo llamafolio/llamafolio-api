6aa350140a69a6bf994bc985329c06ee5e7236fe
0xpeluche <0xpeluche@protonmail.com>
2023-03-13 13:29:59 +0100

    stake

---
+++ src/adapters/chainlink/ethereum/index.ts
@@ -0,0 +1,35 @@
+import { Contract, GetBalancesHandler } from '@lib/adapter'
+import { resolveBalances } from '@lib/balance'
+import { Token } from '@lib/token'
+
+import { getChainlinkStakerBalances } from './balance'
+
+const link: Token = {
+  chain: 'ethereum',
+  address: '0x514910771AF9Ca656af840dff83E8264EcF986CA',
+  decimals: 18,
+  symbol: 'LINK',
+}
+
+const staker: Contract = {
+  chain: 'ethereum',
+  address: '0x3feb1e09b4bb0e7f0387cee092a52e85797ab889',
+  underlyings: [link],
+}
+
+export const getContracts = async () => {
+  return {
+    contracts: { staker },
+    props: { staker },
+  }
+}
+
+export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, _contracts, props) => {
+  const balances = await resolveBalances<typeof getContracts>(ctx, props, {
+    staker: getChainlinkStakerBalances,
+  })
+
+  return {
+    groups: [{ balances }],
+  }
+}
