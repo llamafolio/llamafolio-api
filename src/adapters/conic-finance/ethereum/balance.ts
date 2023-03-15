016746ad6b148ff45fe0d7a1859fd47355dcd9ce
0xpeluche <0xpeluche@protonmail.com>
2023-03-15 12:06:51 +0100

    CNC farm + lock

--- src/adapters/conic-finance/ethereum/balance.ts
+++ src/adapters/conic-finance/ethereum/balance.ts
@@ -1,5 +1,9 @@
 import { Balance, BalancesContext, Contract } from '@lib/adapter'
-import { multicall } from '@lib/multicall'
+import { call } from '@lib/call'
+import { abi as erc20Abi } from '@lib/erc20'
+import { BN_ZERO } from '@lib/math'
+import { Call, multicall } from '@lib/multicall'
+import { Token } from '@lib/token'
 import { isSuccess } from '@lib/type'
 import { BigNumber, utils } from 'ethers'
 
@@ -32,6 +36,60 @@ const abi = {
     stateMutability: 'view',
     type: 'function',
   },
+  lockedBalance: {
+    inputs: [{ internalType: 'address', name: '', type: 'address' }],
+    name: 'lockedBalance',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  voteLocks: {
+    inputs: [
+      { internalType: 'address', name: '', type: 'address' },
+      { internalType: 'uint256', name: '', type: 'uint256' },
+    ],
+    name: 'voteLocks',
+    outputs: [
+      { internalType: 'uint256', name: 'amount', type: 'uint256' },
+      { internalType: 'uint256', name: 'unlockTime', type: 'uint256' },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  balances: {
+    inputs: [{ internalType: 'address', name: '', type: 'address' }],
+    name: 'balances',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  claimableReward: {
+    inputs: [{ internalType: 'address', name: 'user', type: 'address' }],
+    name: 'claimableRewards',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  get_underlying_balances: {
+    stateMutability: 'view',
+    type: 'function',
+    name: 'get_underlying_balances',
+    inputs: [{ name: '_pool', type: 'address' }],
+    outputs: [{ name: '', type: 'uint256[8]' }],
+  },
+}
+
+const CNC: Token = {
+  chain: 'ethereum',
+  address: '0x9aE380F0272E2162340a5bB646c354271c0F5cFC',
+  decimals: 18,
+  symbol: 'CNC',
+}
+
+const metaRegistry: Contract = {
+  name: 'Curve Metaregistry',
+  chain: 'ethereum',
+  address: '0xF98B45FA17DE75FB1aD0e7aFD971b0ca00e379fC',
 }
 
 export async function getConicBalances(
@@ -87,4 +145,71 @@ export async function getConicBalances(
   }
 
   return balances
+}
+
+export async function getCNCLockerBalances(ctx: BalancesContext, lockers: Contract[]): Promise<Balance[]> {
+  const balances: Balance[] = []
+
+  const calls: Call[] = []
+  for (const locker of lockers) {
+    // Number of iterations is not predictable but 10 seems to be a reasonable number of positions that an user locks
+    for (let idx = 0; idx < 10; idx++) {
+      calls.push({ target: locker.address, params: [ctx.address, idx] })
+    }
+  }
+
+  const lockedBalancesRes = await multicall({ ctx, calls, abi: abi.voteLocks })
+
+  for (let lockIdx = 0; lockIdx < lockers.length; lockIdx++) {
+    const locker = lockers[lockIdx]
+    const lockedBalanceRes = lockedBalancesRes[lockIdx]
+
+    if (!isSuccess(lockedBalanceRes)) {
+      continue
+    }
+
+    balances.push({
+      ...locker,
+      symbol: CNC.symbol,
+      decimals: CNC.decimals,
+      amount: BigNumber.from(lockedBalanceRes.output.amount),
+      lock: { end: lockedBalanceRes.output.unlockTime },
+      underlyings: [CNC],
+      rewards: undefined,
+      category: 'lock',
+    })
+  }
+
+  return balances
+}
+
+export async function getConicFarmBalances(ctx: BalancesContext, contract: Contract): Promise<Balance> {
+  const underlyings = contract.underlyings as Contract[]
+
+  const [balanceOfRes, claimableRewardsRes, underlyingsBalancesRes, totalSuppliesRes, symbolsRes, decimalsRes] =
+    await Promise.all([
+      call({ ctx, target: contract.address, params: [ctx.address], abi: abi.balances }),
+      call({ ctx, target: contract.address, params: [ctx.address], abi: abi.claimableReward }),
+      call({ ctx, target: metaRegistry.address, params: [contract.pool], abi: abi.get_underlying_balances }),
+      call({ ctx, target: contract.lpToken, abi: erc20Abi.totalSupply }),
+      call({ ctx, target: contract.lpToken, abi: erc20Abi.symbol }),
+      call({ ctx, target: contract.lpToken, abi: erc20Abi.decimals }),
+    ])
+
+  underlyings.forEach((underlying, underlyingIdx) => {
+    const underlyingBalance = underlyingsBalancesRes.output[underlyingIdx]
+    ;(underlying as Balance).amount =
+      BigNumber.from(underlyingBalance).mul(balanceOfRes.output).div(totalSuppliesRes.output) || BN_ZERO
+  })
+
+  return {
+    chain: ctx.chain,
+    address: contract.lpToken,
+    symbol: symbolsRes.output,
+    decimals: decimalsRes.output,
+    amount: BigNumber.from(balanceOfRes.output),
+    underlyings,
+    rewards: [{ ...CNC, amount: BigNumber.from(claimableRewardsRes.output) }],
+    category: 'farm',
+  }
 }
--- src/adapters/conic-finance/ethereum/index.ts
+++ src/adapters/conic-finance/ethereum/index.ts
@@ -1,16 +1,35 @@
 import { BaseContext, Contract, GetBalancesHandler } from '@lib/adapter'
 import { resolveBalances } from '@lib/balance'
 
-import { getConicBalances } from './balance'
+import { getCNCLockerBalances, getConicBalances, getConicFarmBalances } from './balance'
 import { getlpTokensContracts } from './contract'
 
 const stakers: string[] = [
   '0x07b577f10d4e00f3018542d08a87f255a49175a5', // USDC
-  // '0x3f41480dd3b32f1cc579125f9570dccd07e07667', // Locker CNC
   '0xabb735648a076d570aff2a61d8d141099823eae9', // DAI
   '0x40293380f5292bb13905608b35a936c332f07f94', // FRAX
-  // '0x5f2e1ac047e6a8526f8640a7ed8ab53a0b3f4acf', // Locker CNC v2
 ]
+
+const CNCLockerV1: Contract = {
+  chain: 'ethereum',
+  address: '0x3f41480dd3b32f1cc579125f9570dccd07e07667',
+  underlyings: ['0x9aE380F0272E2162340a5bB646c354271c0F5cFC'],
+}
+
+const CNCLockerV2: Contract = {
+  chain: 'ethereum',
+  address: '0x5f2e1ac047e6a8526f8640a7ed8ab53a0b3f4acf',
+  underlyings: ['0x9aE380F0272E2162340a5bB646c354271c0F5cFC'],
+}
+
+const CNC_ETH: Contract = {
+  chain: 'ethereum',
+  address: '0xc67e9cdf599369130dd0841ee5cb8ebf9bb661c4',
+  lpToken: '0xF9835375f6b268743Ea0a54d742Aa156947f8C06',
+  pool: '0x838af967537350D2C44ABB8c010E49E32673ab94',
+  underlyings: ['0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', '0x9aE380F0272E2162340a5bB646c354271c0F5cFC'],
+  rewards: ['0x9aE380F0272E2162340a5bB646c354271c0F5cFC'],
+}
 
 const lpTokenStaker: Contract = {
   chain: 'ethereum',
@@ -21,13 +40,15 @@ export const getContracts = async (ctx: BaseContext) => {
   const lpTokens = await getlpTokensContracts(ctx, stakers)
 
   return {
-    contracts: { lpTokenStaker, lpTokens },
+    contracts: { lpTokenStaker, lpTokens, lockers: [CNCLockerV1, CNCLockerV2], CNC_ETH },
   }
 }
 
 export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
   const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
     lpTokens: (...args) => getConicBalances(...args, lpTokenStaker),
+    lockers: getCNCLockerBalances,
+    CNC_ETH: getConicFarmBalances,
   })
 
   return {
