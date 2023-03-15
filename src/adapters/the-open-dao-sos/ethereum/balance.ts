fd75b0bc25179d3c0a93c8fedac41e9ba1c13ae7
0xpeluche <0xpeluche@protonmail.com>
2023-03-15 15:51:01 +0100

    stake

---
+++ src/adapters/the-open-dao-sos/ethereum/balance.ts
@@ -0,0 +1,22 @@
+import { Balance, BalancesContext, Contract } from '@lib/adapter'
+import { getERC20BalanceOf } from '@lib/erc20'
+import { Token } from '@lib/token'
+
+const FROM_veSOS_TO_SOS = 21669 // 2.1669 * 1e4 to prevent underflow
+
+export async function getOpenDaoBalances(ctx: BalancesContext, staker: Contract): Promise<Balance[]> {
+  const balances = await getERC20BalanceOf(ctx, [staker] as Token[])
+
+  for (const balance of balances) {
+    const underlying = balance.underlyings?.[0] as Contract
+
+    if (!underlying) {
+      continue
+    }
+
+    underlying.amount = balance.amount.mul(FROM_veSOS_TO_SOS).div(1e4) // 21669 -> 2.1669
+    balance.category = 'stake'
+  }
+
+  return balances
+}
