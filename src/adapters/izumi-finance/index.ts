79f5d6f65102a5a7b845f05c98ece38bcf7ed730
0xpeluche <0xpeluche@protonmail.com>
2023-03-20 09:52:17 +0100

    wip

---
+++ src/adapters/izumi-finance/index.ts
@@ -0,0 +1,16 @@
+import { Adapter } from '@lib/adapter'
+
+import * as arbitrum from './arbitrum'
+import * as bsc from './bsc'
+import * as ethereum from './ethereum'
+import * as polygon from './polygon'
+
+const adapter: Adapter = {
+  id: 'izumi-finance',
+  bsc,
+  ethereum,
+  polygon,
+  arbitrum,
+}
+
+export default adapter
