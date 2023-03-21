f27a40fc42cc2d03f1f3db0e47e18b84c0575be1
0xpeluche <0xpeluche@protonmail.com>
2023-03-21 16:35:26 +0100

    Atlas fix stake (symbol was ? on folio) + vest

--- src/adapters/atlas-usv/common/stake.ts
+++ src/adapters/atlas-usv/common/stake.ts
@@ -2,18 +2,11 @@ import { Balance, Contract } from '@lib/adapter'
 import { BalancesContext } from '@lib/adapter'
 import { call } from '@lib/call'
 import { abi } from '@lib/erc20'
-import { Token } from '@lib/token'
 import { BigNumber } from 'ethers/lib/ethers'
 
-// multi-chain underlying token
-const USV: Token = {
-  chain: 'ethereum',
-  address: '0x88536C9B2C4701b8dB824e6A16829D5B5Eb84440',
-  symbol: 'USV',
-  decimals: 9,
-}
-
 export async function getStakeBalance(ctx: BalancesContext, contract: Contract): Promise<Balance> {
+  const underlyings = contract.underlyings?.[0] as Contract
+
   const balanceOfRes = await call({
     ctx,
     target: contract.address,
@@ -26,7 +19,7 @@ export async function getStakeBalance(ctx: BalancesContext, contract: Contract):
   const balance: Balance = {
     ...contract,
     amount,
-    underlyings: [{ ...USV, amount }],
+    underlyings: [{ ...underlyings, amount }],
     rewards: undefined,
     category: 'stake',
   }
---
+++ src/adapters/atlas-usv/polygon/vest.ts
@@ -0,0 +1,61 @@
+import { Balance, BalancesContext, Contract } from '@lib/adapter'
+import { multicall } from '@lib/multicall'
+import { providers } from '@lib/providers'
+import { Token } from '@lib/token'
+import { isSuccess } from '@lib/type'
+import { BigNumber } from 'ethers'
+
+const abi = {
+  barterInfo: {
+    inputs: [{ internalType: 'address', name: '', type: 'address' }],
+    name: 'barterInfo',
+    outputs: [
+      { internalType: 'uint256', name: 'payout', type: 'uint256' },
+      { internalType: 'uint256', name: 'vesting', type: 'uint256' },
+      { internalType: 'uint256', name: 'lastBlock', type: 'uint256' },
+      { internalType: 'uint256', name: 'pricePaid', type: 'uint256' },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+}
+
+const USV: Token = {
+  chain: 'polygon',
+  address: '0xac63686230f64bdeaf086fe6764085453ab3023f',
+  symbol: 'USV',
+  decimals: 9,
+}
+
+export async function getVestingBalances(ctx: BalancesContext, vesters: Contract[]): Promise<Balance[]> {
+  const balances: Balance[] = []
+
+  const pendingPayoutBalancesRes = await multicall({
+    ctx,
+    calls: vesters.map((vester) => ({ target: vester.address, params: [ctx.address] })),
+    abi: abi.barterInfo,
+  })
+
+  for (let vesterIdx = 0; vesterIdx < vesters.length; vesterIdx++) {
+    const vester = vesters[vesterIdx]
+    const pendingPayoutBalanceRes = pendingPayoutBalancesRes[vesterIdx]
+    const provider = providers[ctx.chain]
+    const block = (await provider.getBlock(parseInt(pendingPayoutBalanceRes.output.lastBlock))).timestamp
+
+    if (!isSuccess(pendingPayoutBalanceRes)) {
+      continue
+    }
+    balances.push({
+      ...vester,
+      decimals: USV.decimals,
+      symbol: USV.symbol,
+      amount: BigNumber.from(pendingPayoutBalanceRes.output.payout),
+      lock: { end: block },
+      underlyings: [USV],
+      rewards: undefined,
+      category: 'vest',
+    })
+  }
+
+  return balances
+}
