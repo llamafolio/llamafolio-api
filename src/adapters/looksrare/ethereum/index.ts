a335931e2eb4ca031d89b298a254ffea90be9c8f
0xpeluche <0xpeluche@protonmail.com>
2023-03-24 12:28:59 +0100

    missing farm

--- src/adapters/looksrare/ethereum/index.ts
+++ src/adapters/looksrare/ethereum/index.ts
@@ -1,7 +1,7 @@
 import { Contract, GetBalancesHandler } from '@lib/adapter'
 import { resolveBalances } from '@lib/balance'
 
-import { getCompounderBalances, getStakeBalances } from './balances'
+import { getCompounderBalances, getFarmBalances, getStakeBalances } from './balances'
 
 const WETH: Contract = {
   name: 'Wrapped Ether',
@@ -24,9 +24,16 @@ const compounder: Contract = {
   address: '0x3ab16af1315dc6c95f83cbf522fecf98d00fd9ba',
 }
 
+const farmer: Contract = {
+  chain: 'ethereum',
+  address: '0x2a70e7f51f6cd40c3e9956aa964137668cbfadc5',
+  token: '0xDC00bA87Cc2D99468f7f34BC04CBf72E111A32f7',
+  underlyings: ['0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', '0xf4d2888d29D722226FafA5d9B24F9164c092421E'],
+}
+
 export const getContracts = () => {
   return {
-    contracts: { staking, compounder },
+    contracts: { staking, compounder, farmer },
   }
 }
 
@@ -34,6 +41,7 @@ export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, 
   const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
     staking: getStakeBalances,
     compounder: getCompounderBalances,
+    farmer: getFarmBalances,
   })
 
   return {
