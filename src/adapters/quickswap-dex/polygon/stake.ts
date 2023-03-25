e6a720beef37b2839e08291eeac58abb48e004b4
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2023-03-25 12:31:48 +0100

    feat: quickswap-dex stake (#454)
    Co-authored-by: 0xpeluche <0xpeluche@protonmail.com>

---
+++ src/adapters/quickswap-dex/polygon/stake.ts
@@ -0,0 +1,44 @@
+import { Balance, BalancesContext, Contract } from '@lib/adapter'
+import { multicall } from '@lib/multicall'
+import { isSuccess } from '@lib/type'
+import { BigNumber } from 'ethers'
+
+const abi = {
+  QUICKBalance: {
+    inputs: [{ internalType: 'address', name: '_account', type: 'address' }],
+    name: 'QUICKBalance',
+    outputs: [{ internalType: 'uint256', name: 'quickAmount_', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+}
+
+export async function getQuickswapBalances(ctx: BalancesContext, stakers: Contract[]): Promise<Balance[]> {
+  const balances: Balance[] = []
+
+  const balancesOfsRes = await multicall({
+    ctx,
+    calls: stakers.map((staker) => ({ target: staker.address, params: [ctx.address] })),
+    abi: abi.QUICKBalance,
+  })
+
+  for (let stakerIdx = 0; stakerIdx < stakers.length; stakerIdx++) {
+    const staker = stakers[stakerIdx]
+    const underlyings = staker.underlyings as Contract[]
+    const balancesOfRes = balancesOfsRes[stakerIdx]
+
+    if (!isSuccess(balancesOfRes)) {
+      continue
+    }
+
+    balances.push({
+      ...staker,
+      amount: BigNumber.from(balancesOfRes.output),
+      underlyings,
+      rewards: undefined,
+      category: 'stake',
+    })
+  }
+
+  return balances
+}
