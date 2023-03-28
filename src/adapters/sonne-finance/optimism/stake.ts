a7e6b6cf4fd1b3913bef3d390e61c0c7eb86136e
0xpeluche <0xpeluche@protonmail.com>
2023-03-28 14:47:44 +0200

    add stake

---
+++ src/adapters/sonne-finance/optimism/stake.ts
@@ -0,0 +1,40 @@
+import { Balance, BalancesContext, Contract } from '@lib/adapter'
+import { call } from '@lib/call'
+import { abi as erc20Abi } from '@lib/erc20'
+import { Token } from '@lib/token'
+import { BigNumber } from 'ethers'
+
+const abi = {
+  getClaimable: {
+    inputs: [
+      { internalType: 'address', name: 'token', type: 'address' },
+      { internalType: 'address', name: 'account', type: 'address' },
+    ],
+    name: 'getClaimable',
+    outputs: [{ internalType: 'uint256', name: 'amount', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+}
+
+const SONNE: Token = {
+  chain: 'optimism',
+  address: '0x1db2466d9f5e10d7090e7152b68d62703a2245f0',
+  decimals: 18,
+  symbol: 'SONNE',
+}
+
+export async function getSonneStakeBalances(ctx: BalancesContext, staker: Contract): Promise<Balance> {
+  const [{ output: balanceOfRes }, { output: claimableRewardRes }] = await Promise.all([
+    call({ ctx, target: staker.address, params: [ctx.address], abi: erc20Abi.balanceOf }),
+    call({ ctx, target: staker.address, params: [SONNE.address, ctx.address], abi: abi.getClaimable }),
+  ])
+
+  return {
+    ...staker,
+    amount: BigNumber.from(balanceOfRes),
+    underlyings: [SONNE],
+    rewards: [{ ...SONNE, amount: BigNumber.from(claimableRewardRes) }],
+    category: 'stake',
+  }
+}
