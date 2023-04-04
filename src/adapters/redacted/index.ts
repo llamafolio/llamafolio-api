b4d2eca9adbb374cd85d4d01455d237d3a731379
0xpeluche <0xpeluche@protonmail.com>
2023-04-04 11:56:28 +0200

    rm useless

--- src/adapters/redacted/index.ts
+++ src/adapters/redacted/index.ts
@@ -1,12 +1,10 @@
 import { Adapter } from '@lib/adapter'
 
-import * as arbitrum from './arbitrum'
 import * as ethereum from './ethereum'
 
 const adapter: Adapter = {
   id: 'redacted',
   ethereum,
-  arbitrum,
 }
 
 export default adapter
