7b0afd522a002be6e3fd1c97b4866543b51b14d9
0xpeluche <0xpeluche@protonmail.com>
2023-04-11 13:02:37 +0200

    stake + farm

---
+++ src/adapters/tokenlon/ethereum/farm.ts
@@ -0,0 +1,62 @@
+import { Balance, BalancesContext, Contract } from '@lib/adapter'
+import { abi as erc20Abi } from '@lib/erc20'
+import { multicall } from '@lib/multicall'
+import { Token } from '@lib/token'
+import { isSuccess } from '@lib/type'
+import { getUnderlyingBalances } from '@lib/uniswap/v2/pair'
+import { BigNumber } from 'ethers'
+
+const abi = {
+  earned: {
+    inputs: [{ internalType: 'address', name: 'account', type: 'address' }],
+    name: 'earned',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+}
+
+const LON: Token = {
+  chain: 'ethereum',
+  address: '0x0000000000095413afC295d19EDeb1Ad7B71c952',
+  decimals: 18,
+  symbol: 'LON',
+}
+
+export async function getLONFarmBalances(ctx: BalancesContext, farmers: Contract[]): Promise<Balance[]> {
+  const balances: Balance[] = []
+
+  const [balanceOfsRes, earnedsRes] = await Promise.all([
+    multicall({
+      ctx,
+      calls: farmers.map((farmer) => ({ target: farmer.address, params: [ctx.address] })),
+      abi: erc20Abi.balanceOf,
+    }),
+    multicall({
+      ctx,
+      calls: farmers.map((farmer) => ({ target: farmer.address, params: [ctx.address] })),
+      abi: abi.earned,
+    }),
+  ])
+
+  for (let farmerIdx = 0; farmerIdx < farmers.length; farmerIdx++) {
+    const farmer = farmers[farmerIdx]
+    const balanceOfRes = balanceOfsRes[farmerIdx]
+    const earnedRes = earnedsRes[farmerIdx]
+
+    if (!isSuccess(balanceOfRes) || !isSuccess(earnedRes)) {
+      continue
+    }
+
+    balances.push({
+      ...farmer,
+      address: farmer.token as string,
+      amount: BigNumber.from(balanceOfRes.output),
+      underlyings: farmer.underlyings as Contract[],
+      rewards: [{ ...LON, amount: BigNumber.from(earnedRes.output) }],
+      category: 'farm',
+    })
+  }
+
+  return getUnderlyingBalances(ctx, balances)
+}
--- src/adapters/tokenlon/ethereum/index.ts
+++ src/adapters/tokenlon/ethereum/index.ts
@@ -1,26 +1,75 @@
-
-import { BaseContext, GetBalancesHandler } from '@lib/adapter'
+import { Contract, GetBalancesHandler } from '@lib/adapter'
 import { resolveBalances } from '@lib/balance'
 
-export const getContracts = async (ctx: BaseContext) => {
+import { getLONFarmBalances } from './farm'
+import { getxLONstakerBalances } from './stake'
+
+const xLON: Contract = {
+  chain: 'ethereum',
+  address: '0xf88506b0f1d30056b9e5580668d5875b9cd30f23',
+  underlyings: ['0x0000000000095413afC295d19EDeb1Ad7B71c952'],
+  decimals: 18,
+  symbol: 'xLON',
+}
+
+const pools: Contract[] = [
+  {
+    chain: 'ethereum',
+    address: '0x9648b119f442a3a096c0d5a1f8a0215b46dbb547',
+    token: '0x55D31F68975E446a40a2d02FfA4B0E1bFb233c2F',
+    underlyings: ['0x0000000000095413afC295d19EDeb1Ad7B71c952', '0xdAC17F958D2ee523a2206206994597C13D831ec7'],
+  },
+  {
+    chain: 'ethereum',
+    address: '0xb6bc1a713e4b11fa31480d31c825dcfd7e8fabfd',
+    token: '0x7924a818013f39cf800F5589fF1f1f0DEF54F31F',
+    underlyings: ['0x0000000000095413afC295d19EDeb1Ad7B71c952', '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'],
+  },
+  {
+    chain: 'ethereum',
+    address: '0x74379cec6a2c9fde0537e9d9346222a724a278e4',
+    token: '0x7924a818013f39cf800F5589fF1f1f0DEF54F31F',
+    underlyings: ['0x0000000000095413afC295d19EDeb1Ad7B71c952', '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'],
+  },
+  {
+    chain: 'ethereum',
+    address: '0x539a67b6f9c3cad58f434cc12624b2d520bc03f8',
+    token: '0x55D31F68975E446a40a2d02FfA4B0E1bFb233c2F',
+    underlyings: ['0x0000000000095413afC295d19EDeb1Ad7B71c952', '0xdAC17F958D2ee523a2206206994597C13D831ec7'],
+  },
+  {
+    chain: 'ethereum',
+    address: '0xc348314f74b043ff79396e14116b6f19122d69f4',
+    token: '0x7924a818013f39cf800F5589fF1f1f0DEF54F31F',
+    underlyings: ['0x0000000000095413afC295d19EDeb1Ad7B71c952', '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'],
+  },
+  {
+    chain: 'ethereum',
+    address: '0x929cf614c917944dd278bc2134714eaa4121bc6a',
+    token: '0x7924a818013f39cf800F5589fF1f1f0DEF54F31F',
+    underlyings: ['0x0000000000095413afC295d19EDeb1Ad7B71c952', '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'],
+  },
+  {
+    chain: 'ethereum',
+    address: '0x11520d501e10e2e02a2715c4a9d3f8aeb1b72a7a',
+    token: '0x55D31F68975E446a40a2d02FfA4B0E1bFb233c2F',
+    underlyings: ['0x0000000000095413afC295d19EDeb1Ad7B71c952', '0xdAC17F958D2ee523a2206206994597C13D831ec7'],
+  },
+]
+
+export const getContracts = () => {
   return {
-    // Contracts grouped by keys. They will be passed to getBalances, filtered by user interaction
-    contracts: {},
-    // Optional revalidate time (in seconds).
-    // Contracts returned by the adapter are cached by default and can be updated by interval with this parameter.
-    // This is mostly used for Factory contracts, where the number of contracts deployed increases over time
-    // revalidate: 60 * 60,
+    contracts: { xLON, pools },
   }
 }
 
 export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
-  // Any method to check the contracts retrieved above (based on user interaction).
-  // This function will be run each time a user queries his balances.
-  // As static contracts info is filled in getContracts, this should ideally only fetch the current amount of each contract (+ underlyings and rewards)
-  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {})
+  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
+    xLON: getxLONstakerBalances,
+    pools: getLONFarmBalances,
+  })
 
   return {
     groups: [{ balances }],
   }
 }
-
