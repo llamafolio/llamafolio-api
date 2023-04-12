cfccd0d49a89fb59ea6d9d740b387877889915ca
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2023-04-12 13:09:37 +0200

    feat: ambire-wallet farm (#487)
    Co-authored-by: 0xpeluche <0xpeluche@protonmail.com>

---
+++ src/adapters/ambire-wallet/ethereum/index.ts
@@ -0,0 +1,51 @@
+import { Contract, GetBalancesHandler } from '@lib/adapter'
+import { resolveBalances } from '@lib/balance'
+import { Token } from '@lib/token'
+
+import { getADXStakeBalances } from './stake'
+
+const ADX: Token = {
+  chain: 'ethereum',
+  address: '0xADE00C28244d5CE17D72E40330B1c318cD12B7c3',
+  decimals: 18,
+  symbol: 'ADX',
+}
+
+const WALLET: Token = {
+  chain: 'ethereum',
+  address: '0x88800092fF476844f74dC2FC427974BBee2794Ae',
+  decimals: 18,
+  symbol: 'WALLET',
+}
+
+const adx_staking: Contract = {
+  chain: 'ethereum',
+  address: '0xb6456b57f03352be48bf101b46c1752a0813491a',
+  underlyings: [ADX],
+  symbol: 'ADX-STAKING',
+  decimal: 18,
+}
+
+const xWALLET: Contract = {
+  chain: 'ethereum',
+  address: '0x47cd7e91c3cbaaf266369fe8518345fc4fc12935',
+  underlyings: [WALLET],
+  symbol: 'xWALLET ',
+  decimal: 18,
+}
+
+export const getContracts = () => {
+  return {
+    contracts: { stakers: [adx_staking, xWALLET] },
+  }
+}
+
+export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
+  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
+    stakers: getADXStakeBalances,
+  })
+
+  return {
+    groups: [{ balances }],
+  }
+}
