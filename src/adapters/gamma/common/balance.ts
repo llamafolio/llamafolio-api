a43e2f663f8df4570a38f22a35096c060c4363d0
0xpeluche <0xpeluche@protonmail.com>
2023-04-14 12:49:16 +0200

    gamma farm

--- src/adapters/gamma/common/balance.ts
+++ src/adapters/gamma/common/balance.ts
@@ -1,8 +1,10 @@
 import { Balance, BalancesContext, Contract } from '@lib/adapter'
 import { abi as erc20Abi } from '@lib/erc20'
+import { getMasterChefLpToken } from '@lib/masterchef/masterchef'
 import { isZero } from '@lib/math'
 import { multicall } from '@lib/multicall'
 import { isSuccess } from '@lib/type'
+import { Pair } from '@lib/uniswap/v2/factory'
 import { BigNumber } from 'ethers'
 
 const abi = {
@@ -16,17 +18,65 @@ const abi = {
     stateMutability: 'view',
     type: 'function',
   },
+  pendingSushi: {
+    inputs: [
+      { internalType: 'uint256', name: '_pid', type: 'uint256' },
+      { internalType: 'address', name: '_user', type: 'address' },
+    ],
+    name: 'pendingSushi',
+    outputs: [{ internalType: 'uint256', name: 'pending', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  userInfo: {
+    inputs: [
+      { internalType: 'uint256', name: '', type: 'uint256' },
+      { internalType: 'address', name: '', type: 'address' },
+    ],
+    name: 'userInfo',
+    outputs: [
+      { internalType: 'uint256', name: 'amount', type: 'uint256' },
+      { internalType: 'int256', name: 'rewardDebt', type: 'int256' },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
 }
 
 export async function getGammaFarmBalances(ctx: BalancesContext, pools: Contract[]): Promise<Balance[]> {
   const balances: Balance[] = []
 
-  const [balancesOfsRes, totalAmountsRes, totalSuppliesRes] = await Promise.all([
-    multicall({
-      ctx,
-      calls: pools.map((pool) => ({ target: pool.address, params: [ctx.address] })),
-      abi: erc20Abi.balanceOf,
-    }),
+  const balancesOfsRes = await multicall({
+    ctx,
+    calls: pools.map((pool) => ({ target: pool.address, params: [ctx.address] })),
+    abi: erc20Abi.balanceOf,
+  })
+
+  for (let poolIdx = 0; poolIdx < pools.length; poolIdx++) {
+    const pool = pools[poolIdx]
+    const underlyings = pool.underlyings as Contract[]
+    const balancesOfRes = balancesOfsRes[poolIdx]
+
+    if (!underlyings || !isSuccess(balancesOfRes)) {
+      continue
+    }
+
+    balances.push({
+      ...pool,
+      amount: BigNumber.from(balancesOfRes.output),
+      underlyings,
+      rewards: undefined,
+      category: 'farm',
+    })
+  }
+
+  return getGammaUnderlyings(ctx, balances)
+}
+
+const getGammaUnderlyings = async (ctx: BalancesContext, pools: Balance[]): Promise<Balance[]> => {
+  const balances: Balance[] = []
+
+  const [totalAmountsRes, totalSuppliesRes] = await Promise.all([
     multicall({
       ctx,
       calls: pools.map((pool) => ({ target: pool.address })),
@@ -42,24 +92,15 @@ export async function getGammaFarmBalances(ctx: BalancesContext, pools: Contract
   for (let poolIdx = 0; poolIdx < pools.length; poolIdx++) {
     const pool = pools[poolIdx]
     const underlyings = pool.underlyings as Contract[]
-    const balancesOfRes = balancesOfsRes[poolIdx]
     const totalAmountRes = totalAmountsRes[poolIdx]
     const totalSupplyRes = totalSuppliesRes[poolIdx]
 
-    if (
-      !underlyings ||
-      !isSuccess(balancesOfRes) ||
-      !isSuccess(totalAmountRes) ||
-      !isSuccess(totalSupplyRes) ||
-      isZero(totalSupplyRes.output)
-    ) {
+    if (!underlyings || !isSuccess(totalAmountRes) || !isSuccess(totalSupplyRes) || isZero(totalSupplyRes.output)) {
       continue
     }
 
     const tokens = underlyings.map((underlying, index) => {
-      const amount = BigNumber.from(balancesOfRes.output)
-        .mul(totalAmountRes.output[`total${index}`])
-        .div(totalSupplyRes.output)
+      const amount = pool.amount.mul(totalAmountRes.output[`total${index}`]).div(totalSupplyRes.output)
       return {
         ...underlying,
         amount,
@@ -68,7 +109,7 @@ export async function getGammaFarmBalances(ctx: BalancesContext, pools: Contract
 
     balances.push({
       ...pool,
-      amount: BigNumber.from(balancesOfRes.output),
+      amount: pool.amount,
       underlyings: tokens,
       rewards: undefined,
       category: 'farm',
@@ -76,4 +117,44 @@ export async function getGammaFarmBalances(ctx: BalancesContext, pools: Contract
   }
 
   return balances
+}
+
+export async function getGammaMasterchefBalances(
+  ctx: BalancesContext,
+  pairs: Pair[],
+  masterchefs: Contract[],
+): Promise<Balance[]> {
+  const balances: Balance[] = []
+  const masterchefPools: Contract[] = []
+
+  for (const masterchef of masterchefs) {
+    masterchefPools.push(
+      ...(await getMasterChefLpToken(ctx, pairs, masterchef)).map((res) => ({ ...res, provider: masterchef.address })),
+    )
+  }
+
+  const poolsBalancesRes = await multicall({
+    ctx,
+    calls: masterchefPools.map((pool) => ({ target: pool.provider, params: [pool.pid, ctx.address] })),
+    abi: abi.userInfo,
+  })
+
+  for (let userIdx = 0; userIdx < poolsBalancesRes.length; userIdx++) {
+    const masterchefPool = masterchefPools[userIdx]
+    const poolBalanceRes = poolsBalancesRes[userIdx]
+
+    if (!isSuccess(poolBalanceRes)) {
+      continue
+    }
+
+    balances.push({
+      ...masterchefPool,
+      underlyings: masterchefPool.underlyings as Contract[],
+      category: 'farm',
+      amount: BigNumber.from(poolBalanceRes.output.amount),
+      rewards: undefined,
+    })
+  }
+
+  return getGammaUnderlyings(ctx, balances)
 }
--- src/adapters/gamma/polygon/index.ts
+++ src/adapters/gamma/polygon/index.ts
@@ -1,13 +1,42 @@
-import { BaseContext, GetBalancesHandler } from '@lib/adapter'
+import { BalancesContext, BaseContext, Contract, GetBalancesHandler } from '@lib/adapter'
 import { resolveBalances } from '@lib/balance'
+import { Pair } from '@lib/uniswap/v2/factory'
 
-import { getGammaFarmBalances } from '../common/balance'
+import { getGammaFarmBalances, getGammaMasterchefBalances } from '../common/balance'
 import { getPoolContractsFromAPI } from '../common/contract'
 
 const API_URLs = [
   'https://wire2.gamma.xyz/polygon/hypervisors/allData',
   'https://wire2.gamma.xyz/quickswap/polygon/hypervisors/allData',
 ]
+
+const masterChef: Contract = {
+  name: 'masterChef',
+  displayName: 'MasterChef',
+  chain: 'polygon',
+  address: '0x570d60a60baa356d47fda3017a190a48537fcd7d',
+}
+
+const masterChef2: Contract = {
+  name: 'masterChef',
+  displayName: 'MasterChef',
+  chain: 'polygon',
+  address: '0x20ec0d06f447d550fc6edee42121bc8c1817b97d',
+}
+
+const masterChef3: Contract = {
+  name: 'masterChef',
+  displayName: 'MasterChef',
+  chain: 'polygon',
+  address: '0xcc54afcecd0d89e0b2db58f5d9e58468e7ad20dc',
+}
+
+const masterChef4: Contract = {
+  name: 'masterChef',
+  displayName: 'MasterChef',
+  chain: 'polygon',
+  address: '0x68678cf174695fc2d27bd312df67a3984364ffdd',
+}
 
 export const getContracts = async (ctx: BaseContext) => {
   const pools = await getPoolContractsFromAPI(ctx, API_URLs)
@@ -17,9 +46,16 @@ export const getContracts = async (ctx: BaseContext) => {
   }
 }
 
+function getGammaPairsBalances(ctx: BalancesContext, pairs: Pair[]) {
+  return Promise.all([
+    getGammaFarmBalances(ctx, pairs),
+    getGammaMasterchefBalances(ctx, pairs, [masterChef, masterChef2, masterChef3, masterChef4]),
+  ])
+}
+
 export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
   const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
-    pools: getGammaFarmBalances,
+    pools: (ctx, pools) => getGammaPairsBalances(ctx, pools as Pair[]),
   })
 
   return {
