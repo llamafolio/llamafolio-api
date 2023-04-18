3aefb3d436488cdcb900712a4a11874f7eb29c4f
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2023-04-18 16:41:03 +0200

    lp (#499)

---
+++ src/adapters/honeyswap/polygon/index.ts
@@ -0,0 +1,36 @@
+import { BaseContext, GetBalancesHandler } from '@lib/adapter'
+import { resolveBalances } from '@lib/balance'
+import { getPairsContracts } from '@lib/uniswap/v2/factory'
+import { getPairsBalances } from '@lib/uniswap/v2/pair'
+
+export const getContracts = async (ctx: BaseContext, props: any) => {
+  const offset = props.pairOffset || 0
+  const limit = 100
+
+  const { pairs, allPairsLength } = await getPairsContracts({
+    ctx,
+    factoryAddress: '0x03daa61d8007443a6584e3d8f85105096543c19c',
+    offset,
+    limit,
+  })
+
+  return {
+    contracts: {
+      pairs,
+    },
+    revalidate: 60 * 60,
+    revalidateProps: {
+      pairOffset: Math.min(offset + limit, allPairsLength),
+    },
+  }
+}
+
+export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
+  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
+    pairs: getPairsBalances,
+  })
+
+  return {
+    groups: [{ balances }],
+  }
+}
