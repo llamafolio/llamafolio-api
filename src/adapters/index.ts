530496541838d467461cb65939be8ec3de461676
0xpeluche <0xpeluche@protonmail.com>
2023-04-19 10:48:35 +0200

    Wigoswap

--- src/adapters/index.ts
+++ src/adapters/index.ts
@@ -144,6 +144,7 @@ import venus from '@adapters/venus'
 import verse from '@adapters/verse'
 import wallet from '@adapters/wallet'
 import wepiggy from '@adapters/wepiggy'
+import wigoswap from '@adapters/wigoswap'
 import wonderland from '@adapters/wonderland'
 import x2y2 from '@adapters/x2y2'
 import yearnFinance from '@adapters/yearn-finance'
@@ -297,6 +298,7 @@ export const adapters: Adapter[] = [
   verse,
   wallet,
   wepiggy,
+  wigoswap,
   wonderland,
   x2y2,
   yearnFinance,
---
+++ src/adapters/wigoswap/fantom/balance.ts
@@ -0,0 +1,65 @@
+import { Balance, BalancesContext, Contract } from '@lib/adapter'
+import { call } from '@lib/call'
+import { getMasterChefPoolsBalances } from '@lib/masterchef/masterchef'
+import { Token } from '@lib/token'
+import { Pair } from '@lib/uniswap/v2/factory'
+import { getPairsBalances } from '@lib/uniswap/v2/pair'
+import { BigNumber, utils } from 'ethers'
+
+const abi = {
+  getPricePerFullShare: {
+    inputs: [],
+    name: 'getPricePerFullShare',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  userInfo: {
+    inputs: [{ internalType: 'address', name: '', type: 'address' }],
+    name: 'userInfo',
+    outputs: [
+      { internalType: 'uint256', name: 'shares', type: 'uint256' },
+      { internalType: 'uint256', name: 'lastDepositedTime', type: 'uint256' },
+      { internalType: 'uint256', name: 'wigoAtLastUserAction', type: 'uint256' },
+      { internalType: 'uint256', name: 'lastUserActionTime', type: 'uint256' },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+}
+
+const wigo: Token = {
+  chain: 'fantom',
+  address: '0xe992beab6659bff447893641a378fbbf031c5bd6',
+  decimals: 18,
+  symbol: 'WIGO',
+}
+
+export async function getWigoBalances(ctx: BalancesContext, pool: Contract): Promise<Balance> {
+  const [{ output: balanceOf }, { output: pricePerFullShare }] = await Promise.all([
+    call({ ctx, target: pool.address, params: [ctx.address], abi: abi.userInfo }),
+    call({ ctx, target: pool.address, abi: abi.getPricePerFullShare }),
+  ])
+
+  return {
+    ...pool,
+    amount: BigNumber.from(balanceOf.shares).mul(pricePerFullShare).div(utils.parseEther('1.0')),
+    underlyings: [wigo],
+    rewards: undefined,
+    category: 'farm',
+  }
+}
+
+export async function getWigoswapPairsBalances(
+  ctx: BalancesContext,
+  pairs: Pair[],
+  masterchef: Contract,
+  rewardToken: Token,
+  rewardTokenName?: string,
+  lpTokenAbi?: boolean,
+) {
+  return Promise.all([
+    getPairsBalances(ctx, pairs),
+    getMasterChefPoolsBalances(ctx, pairs, masterchef, rewardToken, rewardTokenName, lpTokenAbi),
+  ])
+}
