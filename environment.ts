c6073177db9b755205c73af5e2cf753aecb4a983
0xsign <0xsign@protonmail.com>
2023-05-01 20:39:15 +0200

    fix: optional env vars

--- environment.ts
+++ environment.ts
@@ -4,17 +4,17 @@ import { z } from 'zod'
 
 export const environmentSchema = z.object({
   NODE_ENV: z.union([z.literal('development'), z.literal('production'), z.literal('test')]).default('development'),
-  STAGE: z.union([z.literal('dev'), z.literal('prod'), z.literal('local')]),
+  STAGE: z.union([z.literal('dev'), z.literal('prod'), z.literal('local')]).optional(),
   DDB_TABLE_NAME: z.string().optional(),
   PGHOST: z.string(),
   PGUSER: z.string(),
   PGDATABASE: z.string(),
   PGPASSWORD: z.string(),
   PGPORT: z.string(),
-  CUSTOM_PROVIDER: z.string(),
+  CUSTOM_PROVIDER: z.string().optional(),
   LLAMANODES_API_KEY: z.string(),
-  ARBITRUM_RPC: z.string(),
-  OPTIMISM_RPC: z.string(),
+  ARBITRUM_RPC: z.string().optional(),
+  OPTIMISM_RPC: z.string().optional(),
   IS_OFFLINE: z.literal('true').or(z.literal('false')).optional(),
   API_URL: z.string().optional(),
 })
