c665b3aa925b0315b0ba41d43eaa7cf458c1a622
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2023-05-02 15:26:31 +0200

    feat: tonpound lending (#533)

---
+++ src/adapters/tonpound/ethereum/index.ts
@@ -0,0 +1,35 @@
+import { BaseContext, Contract, GetBalancesHandler } from '@lib/adapter'
+import { resolveBalances } from '@lib/balance'
+import {
+  BalanceWithExtraProps,
+  getHealthFactor,
+  getMarketsBalances,
+  getMarketsContracts,
+} from '@lib/compound/v2/lending'
+
+const Comptroller: Contract = {
+  chain: 'ethereum',
+  address: '0x1775286Cbe9db126a95AbF52c58a3214FCA26803',
+}
+
+export const getContracts = async (ctx: BaseContext) => {
+  const markets = await getMarketsContracts(ctx, {
+    comptrollerAddress: Comptroller.address,
+  })
+
+  return {
+    contracts: { markets, Comptroller },
+  }
+}
+
+export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
+  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
+    markets: getMarketsBalances,
+  })
+
+  const healthFactor = await getHealthFactor(balances as BalanceWithExtraProps[])
+
+  return {
+    groups: [{ balances, healthFactor }],
+  }
+}
