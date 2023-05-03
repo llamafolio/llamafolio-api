83e16b1a4494e6929631f4d4fc2477527dd63c6d
0xsign <99134502+0xsign@users.noreply.github.com>
2023-05-03 16:17:16 +0200

    feat: replace millify lib (#540)

--- src/lib/fmt.ts
+++ src/lib/fmt.ts
@@ -16,3 +16,19 @@ export function boolean(val: any): boolean {
       return Boolean(val)
   }
 }
+
+export function millify(amount: number): string {
+  const quantifiers = [
+    [10 ** 9, 'B'],
+    [10 ** 6, 'M'],
+    [10 ** 3, 'k'],
+  ] as [number, string][]
+
+  for (const [denominator, letter] of quantifiers) {
+    if (amount > denominator) {
+      return `${+(amount / denominator).toFixed(2)} ${letter}`
+    }
+  }
+
+  return amount.toFixed(2)
+}
