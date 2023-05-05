551f7623608e3ca6416e1dc45fce4729fd72f64b
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2023-05-05 19:18:54 +0200

    feat: everrise (#558)

---
+++ src/adapters/everrise/bsc/index.ts
@@ -0,0 +1,27 @@
+import { getEverriseBalances } from '@adapters/everrise/common/stake'
+import type { Contract, GetBalancesHandler } from '@lib/adapter'
+import { resolveBalances } from '@lib/balance'
+
+const nftStaker: Contract = {
+  chain: 'bsc',
+  address: '0x23cd2e6b283754fd2340a75732f9ddbb5d11807e',
+  token: '0xc17c30e98541188614df99239cabd40280810ca3',
+  underlyings: ['0xc17c30e98541188614df99239cabd40280810ca3'],
+  rewards: ['0xc17c30e98541188614df99239cabd40280810ca3'],
+}
+
+export const getContracts = () => {
+  return {
+    contracts: { nftStaker },
+  }
+}
+
+export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
+  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
+    nftStaker: getEverriseBalances,
+  })
+
+  return {
+    groups: [{ balances }],
+  }
+}
