175f6056b542fa5657fa1bc1f5005a66329ab0eb
0xPeluche <0xpeluche@proton.me>
2023-05-09 17:37:07 +0200

    fix interaction + logic

---
+++ src/adapters/synthetix/common/balance.ts
@@ -0,0 +1,90 @@
+import type { Balance, BalancesContext, BorrowBalance, Contract, LendBalance, RewardBalance } from '@lib/adapter'
+import { call } from '@lib/call'
+import { BigNumber } from 'ethers'
+
+const abi = {
+  collateral: {
+    constant: true,
+    inputs: [{ internalType: 'address', name: 'account', type: 'address' }],
+    name: 'collateral',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  remainingIssuableSynths: {
+    constant: true,
+    inputs: [{ internalType: 'address', name: 'account', type: 'address' }],
+    name: 'remainingIssuableSynths',
+    outputs: [
+      { internalType: 'uint256', name: 'maxIssuable', type: 'uint256' },
+      { internalType: 'uint256', name: 'alreadyIssued', type: 'uint256' },
+      { internalType: 'uint256', name: 'totalSystemDebt', type: 'uint256' },
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  feesAvailable: {
+    constant: true,
+    inputs: [{ internalType: 'address', name: 'account', type: 'address' }],
+    name: 'feesAvailable',
+    outputs: [
+      { internalType: 'uint256', name: '', type: 'uint256' },
+      { internalType: 'uint256', name: '', type: 'uint256' },
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+}
+
+export async function getSNXBalances(ctx: BalancesContext, contract: Contract): Promise<Balance[]> {
+  const [{ output: userLendBalanceOf }, { output: userDebtBalanceOf }, { output: userPendingRewardBalanceOf }] =
+    await Promise.all([
+      call({
+        ctx,
+        target: contract.token!,
+        params: [ctx.address],
+        abi: abi.collateral,
+      }),
+      call({
+        ctx,
+        target: contract.token!,
+        params: [ctx.address],
+        abi: abi.remainingIssuableSynths,
+      }),
+      call({
+        ctx,
+        target: contract.rewarder,
+        params: [ctx.address],
+        abi: abi.feesAvailable,
+      }),
+    ])
+
+  const lendBalance: LendBalance = {
+    ...contract,
+    amount: BigNumber.from(userLendBalanceOf),
+    underlyings: undefined,
+    rewards: undefined,
+    category: 'lend',
+  }
+
+  const borrowBalance: BorrowBalance = {
+    ...contract,
+    amount: BigNumber.from(userDebtBalanceOf.alreadyIssued),
+    underlyings: contract.underlyings as Contract[],
+    rewards: undefined,
+    category: 'borrow',
+  }
+
+  const rewardBalance: RewardBalance = {
+    ...contract,
+    amount: BigNumber.from(userPendingRewardBalanceOf[1]),
+    underlyings: undefined,
+    rewards: undefined,
+    category: 'reward',
+  }
+
+  return [lendBalance, borrowBalance, rewardBalance]
+}
--- src/adapters/synthetix/ethereum/index.ts
+++ src/adapters/synthetix/ethereum/index.ts
@@ -1,63 +1,24 @@
+import { getSNXBalances } from '@adapters/synthetix/common/balance'
 import type { Contract, GetBalancesHandler } from '@lib/adapter'
 import { resolveBalances } from '@lib/balance'
-import type { Token } from '@lib/token'
 
-import { getLendBorrowBalances } from '../common/lend'
-import { getVestBalances } from '../common/vest'
-
-const SNX: Token = {
+const SNX: Contract = {
   chain: 'ethereum',
-  decimals: 18,
   address: '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f',
-  symbol: 'SNX',
-}
-
-const sUSD: Token = {
-  symbol: 'sUSD',
-  address: '0x57Ab1ec28D129707052df4dF418D58a2D46d5f51',
-  decimals: 18,
-  chain: 'ethereum',
-}
-
-const synthetix: Contract = {
-  name: 'Synthetix',
-  chain: 'ethereum',
-  symbol: 'SNX',
-  decimals: 18,
-  address: '0x08f30ecf2c15a783083ab9d5b9211c22388d0564',
-  underlyings: [SNX],
-}
-
-const feePool: Contract = {
-  name: 'FeePool',
-  chain: 'ethereum',
-  address: '0x3b2f389aee480238a49e3a9985cd6815370712eb',
-}
-
-const rewardEscrow: Contract = {
-  name: 'Reward Escrow v2',
-  chain: 'ethereum',
-  address: '0xAc86855865CbF31c8f9FBB68C749AD5Bd72802e3',
-  underlyings: [SNX],
-}
-
-const liquidatorReward: Contract = {
-  name: 'Liquidator Reward',
-  chain: 'ethereum',
-  address: '0xf79603a71144e415730C1A6f57F366E4Ea962C00',
-  underlyings: [SNX],
+  token: '0xd0dA9cBeA9C3852C5d63A95F9ABCC4f6eA0F9032',
+  underlyings: ['0x10A5F7D9D65bCc2734763444D4940a31b109275f'],
+  rewarder: '0x83105D7CDd2fd9b8185BFF1cb56bB1595a618618',
 }
 
 export const getContracts = async () => {
   return {
-    contracts: { synthetix, feePool, sUSD, rewardEscrow, liquidatorReward },
+    contracts: { SNX },
   }
 }
 
 export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
   const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
-    synthetix: (...args) => getLendBorrowBalances(...args, feePool, sUSD),
-    rewardEscrow: (...args) => getVestBalances(...args, liquidatorReward),
+    SNX: getSNXBalances,
   })
 
   return {
