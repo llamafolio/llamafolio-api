7df350d5e6977fd98de49308eea26967b3683961
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2023-05-17 10:33:04 +0200

    feat: polkastarter lock on bsc/eth (#592)

---
+++ src/adapters/polkastarter/ethereum/index.ts
@@ -0,0 +1,25 @@
+import { getPolkaLockedBalance } from '@adapters/polkastarter/common/lock'
+import type { Contract, GetBalancesHandler } from '@lib/adapter'
+import { resolveBalances } from '@lib/balance'
+
+const locker: Contract = {
+  chain: 'ethereum',
+  address: '0xc24a365a870821eb83fd216c9596edd89479d8d7',
+  token: '0x83e6f1E41cdd28eAcEB20Cb649155049Fac3D5Aa',
+}
+
+export const getContracts = () => {
+  return {
+    contracts: { locker },
+  }
+}
+
+export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
+  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
+    locker: getPolkaLockedBalance,
+  })
+
+  return {
+    groups: [{ balances }],
+  }
+}
