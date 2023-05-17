fdfa46953713a3930ce90723d9b2bb02aaf73d0e
0xsign <0xsign@protonmail.com>
2023-05-17 14:31:17 +0200

    fix: get info optional value

--- src/handlers/getInfoAddress.ts
+++ src/handlers/getInfoAddress.ts
@@ -3,6 +3,12 @@ import { badRequest, serverError, success } from '@handlers/response'
 import { isHex } from '@lib/buf'
 import type { APIGatewayProxyHandler } from 'aws-lambda'
 
+interface AddressInfoResponse {
+  data: {
+    activeSince?: number
+  }
+}
+
 /**
  * Get address info
  */
@@ -32,14 +38,11 @@ export const handler: APIGatewayProxyHandler = async (event, context) => {
       [address.toLowerCase()],
     )
 
-    return success(
-      {
-        data: {
-          activeSince: parseInt(activeSince.rows[0].timestamp),
-        },
-      },
-      { maxAge: 60 * 60 },
-    )
+    const response: AddressInfoResponse = {
+      data: { activeSince: activeSince.rows[0]?.timestamp ? parseInt(activeSince.rows[0].timestamp) : undefined },
+    }
+
+    return success(response, { maxAge: 60 * 60 })
   } catch (error) {
     console.error('Failed to get address info', { error })
     return serverError('Failed to get address info')
