0e3eee16416d0e03027bf490cfa5d5e1a262e83b
0xsign <0xsign@protonmail.com>
2023-05-25 12:16:23 +0200

    fix: rm ethereum until we fully support it

--- src/adapters/coinwind/index.ts
+++ src/adapters/coinwind/index.ts
@@ -1,14 +1,11 @@
 import type { Adapter } from '@lib/adapter'
 
 import * as bsc from './bsc'
-import * as ethereum from './ethereum'
 
 const adapter: Adapter = {
   id: 'coinwind',
   bsc,
-  ethereum,
+  // TODO: Find masterChef contract to export common farm logic on ethereum chain
 }
-
-// TODO: Find masterChef contract to export common farm logic on ethereum chain
 
 export default adapter
