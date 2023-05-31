ab83a43e5c6f7b05f09d3e69f65f90b830b22d46
0xPeluche <0xpeluche@proton.me>
2023-05-31 15:58:33 +0200

    Stader fix

--- src/adapters/stader/bsc/farm.ts
+++ src/adapters/stader/bsc/farm.ts
@@ -1,7 +1,27 @@
 import type { Balance, BalancesContext, Contract } from '@lib/adapter'
+import { mapSuccessFilter, rangeBI } from '@lib/array'
 import { call } from '@lib/call'
+import { multicall } from '@lib/multicall'
 
 const abi = {
+  getUserWithdrawalRequests: {
+    inputs: [{ internalType: 'address', name: '_address', type: 'address' }],
+    name: 'getUserWithdrawalRequests',
+    outputs: [
+      {
+        components: [
+          { internalType: 'uint256', name: 'uuid', type: 'uint256' },
+          { internalType: 'uint256', name: 'amountInBnbX', type: 'uint256' },
+          { internalType: 'uint256', name: 'startTime', type: 'uint256' },
+        ],
+        internalType: 'struct IStakeManager.WithdrawalRequest[]',
+        name: '',
+        type: 'tuple[]',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
   getUserRequestStatus: {
     inputs: [
       { internalType: 'address', name: '_user', type: 'address' },
@@ -17,22 +37,34 @@ const abi = {
   },
 } as const
 
-export async function getStaderFarmBalances(ctx: BalancesContext, contract: Contract): Promise<Balance> {
-  const userRequestStatus = await call({
+export async function getStaderFarmBalances(ctx: BalancesContext, contract: Contract): Promise<Balance[]> {
+  const userRequest = await call({
     ctx,
     target: contract.address,
-    params: [ctx.address, 0n],
+    params: [ctx.address],
+    abi: abi.getUserWithdrawalRequests,
+  })
+
+  const userRequestStatus = await multicall({
+    ctx,
+    calls: rangeBI(0n, BigInt(userRequest.length)).map(
+      (idx) => ({ target: contract.address, params: [ctx.address, idx] } as const),
+    ),
     abi: abi.getUserRequestStatus,
   })
 
-  const [_isClaimable, _amount] = userRequestStatus
+  const balances: Balance[] = mapSuccessFilter(userRequestStatus, (res) => {
+    const [_isClaimable, _amount] = res.output
 
-  return {
-    ...contract,
-    address: contract.token!,
-    amount: _amount,
-    underlyings: undefined,
-    rewards: undefined,
-    category: 'farm',
-  }
+    return {
+      ...contract,
+      address: contract.token!,
+      amount: _amount,
+      underlyings: undefined,
+      rewards: undefined,
+      category: 'farm',
+    }
+  })
+
+  return balances
 }
