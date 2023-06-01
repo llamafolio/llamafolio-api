98462ec3bd43ca168c0f5042fcf4f269c3fb8659
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2023-06-01 18:14:42 +0200

    feat: aave-v3 on mainnet (#628)

---
+++ src/adapters/aave-v3/ethereum/index.ts
@@ -0,0 +1,40 @@
+import type { BaseContext, Contract, GetBalancesHandler } from '@lib/adapter'
+import { resolveBalances } from '@lib/balance'
+
+import { getLendingPoolBalances, getLendingPoolContracts, getLendingPoolHealthFactor } from '../common/lending'
+
+const lendingPool: Contract = {
+  name: 'Pool',
+  displayName: 'Pool',
+  chain: 'ethereum',
+  address: '0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2',
+}
+
+const poolDataProvider: Contract = {
+  chain: 'ethereum',
+  address: '0x7B4EB56E7CD4b454BA8ff71E4518426369a138a3',
+  name: 'Pool Data Provider',
+  displayName: 'Aave: Pool Data Provider V3',
+}
+
+export const getContracts = async (ctx: BaseContext) => {
+  const pools = await getLendingPoolContracts(ctx, lendingPool, poolDataProvider)
+
+  return {
+    contracts: {
+      pools,
+    },
+  }
+}
+
+export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
+  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
+    pools: getLendingPoolBalances,
+  })
+
+  const healthFactor = await getLendingPoolHealthFactor(ctx, lendingPool)
+
+  return {
+    groups: [{ balances, healthFactor }],
+  }
+}
