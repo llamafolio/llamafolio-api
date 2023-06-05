d65e6b65fe1a472fa0d70769a169bc9b54759de8
0xsign <99134502+0xsign@users.noreply.github.com>
2023-06-05 16:21:25 +0200

    feat: stakehound (#635)

---
+++ src/adapters/stakehound/ethereum/index.ts
@@ -0,0 +1,28 @@
+import type { GetBalancesHandler } from '@lib/adapter'
+import { resolveBalances } from '@lib/balance'
+import { getERC20BalanceOf } from '@lib/erc20'
+import type { Token } from '@lib/token'
+
+const stETH: Token = {
+  chain: 'ethereum',
+  address: '0xDFe66B14D37C77F4E9b180cEb433d1b164f0281D',
+  name: 'StakedTokenV2',
+  symbol: 'stETH',
+  decimals: 18,
+}
+
+export const getContracts = async () => {
+  return {
+    contracts: { stETH },
+  }
+}
+
+export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
+  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
+    stETH: (ctx, stETH) => getERC20BalanceOf(ctx, [stETH]),
+  })
+
+  return {
+    groups: [{ balances }],
+  }
+}
