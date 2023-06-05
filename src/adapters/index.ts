b58715d45c0e60514c882bc7465c5062a7d97edd
0xsign <0xsign@protonmail.com>
2023-06-05 16:50:03 +0200

    feat: stafi LSD

--- src/adapters/index.ts
+++ src/adapters/index.ts
@@ -152,6 +152,7 @@ import spiritswap from '@adapters/spiritswap'
 import spookyswap from '@adapters/spookyswap'
 import spool from '@adapters/spool'
 import stader from '@adapters/stader'
+import stafi from '@adapters/stafi'
 import stakeLink from '@adapters/stake.link'
 import stakehound from '@adapters/stakehound'
 import stakewise from '@adapters/stakewise'
@@ -353,6 +354,7 @@ export const adapters: Adapter[] = [
   spookyswap,
   spool,
   stader,
+  stafi,
   stakeLink,
   stakehound,
   stakewise,
---
+++ src/adapters/stafi/ethereum/index.ts
@@ -0,0 +1,28 @@
+import type { GetBalancesHandler } from '@lib/adapter'
+import { resolveBalances } from '@lib/balance'
+import { getERC20BalanceOf } from '@lib/erc20'
+import type { Token } from '@lib/token'
+
+const RETH: Token = {
+  chain: 'ethereum',
+  address: '0x9559aaa82d9649c7a7b220e7c461d2e74c9a3593',
+  name: 'StaFi Staked ETH',
+  symbol: 'RETH',
+  decimals: 18,
+}
+
+export const getContracts = async () => {
+  return {
+    contracts: { RETH },
+  }
+}
+
+export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
+  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
+    RETH: (ctx, RETH) => getERC20BalanceOf(ctx, [RETH]),
+  })
+
+  return {
+    groups: [{ balances }],
+  }
+}
