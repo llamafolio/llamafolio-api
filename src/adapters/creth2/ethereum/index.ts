e5a39529696fe4132f69651b77e13b659bf6b1ec
0xsign <0xsign@protonmail.com>
2023-06-05 16:35:11 +0200

    feat: creth2

---
+++ src/adapters/creth2/ethereum/index.ts
@@ -0,0 +1,28 @@
+import type { GetBalancesHandler } from '@lib/adapter'
+import { resolveBalances } from '@lib/balance'
+import { getERC20BalanceOf } from '@lib/erc20'
+import type { Token } from '@lib/token'
+
+const CRETH2: Token = {
+  chain: 'ethereum',
+  address: '0x49d72e3973900a195a155a46441f0c08179fdb64',
+  name: 'Cream ETH 2',
+  symbol: 'CRETH2',
+  decimals: 18,
+}
+
+export const getContracts = async () => {
+  return {
+    contracts: { CRETH2 },
+  }
+}
+
+export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
+  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
+    CRETH2: (ctx, CRETH2) => getERC20BalanceOf(ctx, [CRETH2]),
+  })
+
+  return {
+    groups: [{ balances }],
+  }
+}
