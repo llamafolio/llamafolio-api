4e706a87aa2967bc55f5debb994317419e1d3f7d
0xPeluche <0xpeluche@proton.me>
2023-06-05 13:42:17 +0200

    Definix (dexes) Lp + Farm

---
+++ src/adapters/definix/bsc/index.ts
@@ -0,0 +1,68 @@
+import type { BalancesContext, BaseContext, Contract, GetBalancesHandler } from '@lib/adapter'
+import { resolveBalances } from '@lib/balance'
+import { getMasterChefPoolsBalances } from '@lib/masterchef/masterchef'
+import type { Token } from '@lib/token'
+import type { Pair } from '@lib/uniswap/v2/factory'
+import { getPairsContracts } from '@lib/uniswap/v2/factory'
+import { getPairsBalances } from '@lib/uniswap/v2/pair'
+
+const masterChef: Contract = {
+  name: 'masterChef',
+  displayName: 'MasterChef',
+  chain: 'bsc',
+  address: '0x6b51e8fdc32ead0b837deb334fcb79e24f3b105a',
+}
+
+const FINIX: Token = {
+  chain: 'bsc',
+  address: '0x0f02b1f5af54e04fb6dd6550f009ac2429c4e30d',
+  symbol: 'FINIX',
+  decimals: 18,
+}
+
+export const getContracts = async (ctx: BaseContext, props: any) => {
+  const offset = props.pairOffset || 0
+  const limit = 100
+
+  const { pairs, allPairsLength } = await getPairsContracts({
+    ctx,
+    factoryAddress: '0x43eBb0cb9bD53A3Ed928Dd662095aCE1cef92D19',
+    offset,
+    limit,
+  })
+
+  return {
+    contracts: {
+      masterChef,
+      pairs,
+    },
+    revalidate: 60 * 60,
+    revalidateProps: {
+      pairOffset: Math.min(offset + limit, allPairsLength),
+    },
+  }
+}
+
+function getSushiswapPairsBalances(
+  ctx: BalancesContext,
+  pairs: Pair[],
+  masterchef: Contract,
+  rewardToken: Token,
+  rewardTokenName?: string,
+  lpTokenAbi?: boolean,
+) {
+  return Promise.all([
+    getPairsBalances(ctx, pairs),
+    getMasterChefPoolsBalances(ctx, pairs, masterchef, rewardToken, rewardTokenName, lpTokenAbi),
+  ])
+}
+
+export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx: BalancesContext, contracts) => {
+  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
+    pairs: (...args) => getSushiswapPairsBalances(...args, masterChef, FINIX, 'Finix'),
+  })
+
+  return {
+    groups: [{ balances }],
+  }
+}
