bc6c707213ae0eaee7052ff29105a401f5227f86
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2023-06-06 10:34:03 +0200

    feat: lido moonbeam (#642)

--- src/adapters/lido/index.ts
+++ src/adapters/lido/index.ts
@@ -1,10 +1,12 @@
 import type { Adapter } from '@lib/adapter'
 
 import * as ethereum from './ethereum'
+import * as moonbeam from './moonbeam'
 
 const adapter: Adapter = {
   id: 'lido',
   ethereum,
+  moonbeam,
 }
 
 export default adapter
