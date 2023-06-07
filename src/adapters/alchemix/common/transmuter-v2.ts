6bd1bfc365aa3293c4281fc0f31137accfff8d84
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2023-06-07 08:54:48 +0200

    feat: alchemix-v2  (#320)
    ---------
    
    Co-authored-by: 0xpeluche <0xpeluche@protonmail.com>
    Co-authored-by: 0xsign <0xsign@protonmail.com>

---
+++ src/adapters/alchemix/common/transmuter-v2.ts
@@ -0,0 +1,93 @@
+import type { Balance, BalancesContext, Contract } from '@lib/adapter'
+import type { Call } from '@lib/multicall'
+import { multicall } from '@lib/multicall'
+
+const abi = {
+  accounts: {
+    inputs: [{ internalType: 'address', name: 'owner', type: 'address' }],
+    name: 'accounts',
+    outputs: [
+      { internalType: 'int256', name: 'debt', type: 'int256' },
+      { internalType: 'address[]', name: 'depositedTokens', type: 'address[]' },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  totalValue: {
+    inputs: [{ internalType: 'address', name: 'owner', type: 'address' }],
+    name: 'totalValue',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+} as const
+
+export async function getTransmutationBalances(
+  ctx: BalancesContext,
+  transmuters: Contract[],
+  reactives: Contract[],
+): Promise<Balance[]> {
+  const synthetics: Balance[] = []
+
+  const calls: Call<typeof abi.accounts>[] = []
+  for (let idx = 0; idx < transmuters.length; idx++) {
+    const transmuter = transmuters[idx]
+    calls.push({ target: transmuter.address, params: [ctx.address] })
+  }
+
+  const [accountsRes, totalValuesRes] = await Promise.all([
+    multicall({ ctx, calls, abi: abi.accounts }),
+    multicall({ ctx, calls, abi: abi.totalValue }),
+  ])
+
+  const reactivesDetailsByAddress: { [key: string]: Contract } = {}
+  for (const reactive of reactives) {
+    reactivesDetailsByAddress[reactive.address.toLowerCase()] = reactive
+  }
+
+  for (let idx = 0; idx < transmuters.length; idx++) {
+    const transmuter = transmuters[idx]
+    const borrow = transmuter.underlyings?.[0]
+    const accountRes = accountsRes[idx]
+    const totalValueRes = totalValuesRes[idx]
+
+    if (!accountRes.success) {
+      continue
+    }
+
+    const [debt, depositedTokens] = accountRes.output
+    const reactiveToken = depositedTokens[0].toLowerCase()
+
+    if (!reactiveToken) {
+      continue
+    }
+
+    const synthetic: Balance = {
+      ...(borrow as Contract),
+      amount: debt,
+      underlyings: undefined,
+      rewards: undefined,
+      category: 'borrow',
+    }
+
+    const reactiveDetails = reactivesDetailsByAddress[reactiveToken.toLowerCase()]
+    const underlyings = reactiveDetails.underlyings?.[0] as Contract
+
+    if (!totalValueRes.success || !underlyings) {
+      continue
+    }
+
+    const reactive: Balance = {
+      ...reactiveDetails,
+      symbol: underlyings.symbol,
+      amount: totalValueRes.output,
+      underlyings: undefined,
+      rewards: undefined,
+      category: 'lend',
+    }
+
+    synthetics.push(synthetic, reactive)
+  }
+
+  return synthetics
+}
--- src/adapters/alchemix/index.ts
+++ src/adapters/alchemix/index.ts
@@ -1,10 +1,12 @@
 import type { Adapter } from '@lib/adapter'
 
 import * as ethereum from './ethereum'
+import * as fantom from './fantom'
 
 const adapter: Adapter = {
   id: 'alchemix',
   ethereum,
+  fantom,
 }
 
 export default adapter
