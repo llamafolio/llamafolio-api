648518f0df37220e9609677a70ff3c06070da9bc
0xsign <99134502+0xsign@users.noreply.github.com>
2023-06-15 15:05:25 +0200

    fix: synthetix borrow underlying (#676)

--- src/adapters/synthetix/ethereum/index.ts
+++ src/adapters/synthetix/ethereum/index.ts
@@ -7,7 +7,7 @@ const SNX: Contract = {
   chain: 'ethereum',
   address: '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f',
   token: '0xd0dA9cBeA9C3852C5d63A95F9ABCC4f6eA0F9032',
-  underlyings: ['0x10A5F7D9D65bCc2734763444D4940a31b109275f'],
+  underlyings: ['0x57Ab1ec28D129707052df4dF418D58a2D46d5f51'],
   rewarder: '0x83105D7CDd2fd9b8185BFF1cb56bB1595a618618',
 }
 
