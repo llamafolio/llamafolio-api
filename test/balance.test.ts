70e56729660feccd4f4d770132d6cfa6be5a3a2c
Omar Aziz <github@omaraziz.dev>
2023-06-28 01:49:53 -0700

    downgrade tokens

--- test/balance.test.ts
+++ test/balance.test.ts
@@ -5,10 +5,12 @@ import { balancesHandler } from '@handlers/getBalancesTokens'
 
 const STAGE = environment.STAGE as Exclude<typeof environment.STAGE, undefined>
 
-main().catch()
+main()
+  .catch(console.error)
+  .finally(() => process.exit(0))
 
 async function main() {
   const balances = await balancesHandler({ address: '0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50' })
   console.log(JSON.stringify(balances, undefined, 2))
-  console.log(balances.chains.map(b => b.balances.length))
+  console.log(balances.chains.map((b) => b.balances.length))
 }
