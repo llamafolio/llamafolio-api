b1ac8da621b36bb34b4ce5cb37a5c402b279062b
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2023-06-28 12:16:33 +0200

    feat: belt-finance stake + farm + lp (#685)

---
+++ src/adapters/belt-finance/bsc/contract.ts
@@ -0,0 +1,155 @@
+import type { BaseContext, Contract } from '@lib/adapter'
+import { mapSuccessFilter, rangeBI } from '@lib/array'
+import { call } from '@lib/call'
+import { multicall } from '@lib/multicall'
+import type { Token } from '@lib/token'
+import { getPairsDetails } from '@lib/uniswap/v2/factory'
+
+const abi = {
+  poolLength: {
+    inputs: [],
+    name: 'poolLength',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  poolInfo: {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    name: 'poolInfo',
+    outputs: [
+      {
+        internalType: 'contract IERC20',
+        name: 'want',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'allocPoint',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'lastRewardBlock',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'accBELTPerShare',
+        type: 'uint256',
+      },
+      {
+        internalType: 'address',
+        name: 'strat',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  token: {
+    inputs: [],
+    name: 'token',
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+} as const
+
+const BELT: Token = {
+  chain: 'bsc',
+  address: '0xe0e514c71282b6f4e823703a39374cf58dc3ea4f',
+  decimals: 18,
+  symbol: 'BELT',
+}
+
+export async function getBeltContracts(ctx: BaseContext, masterchef: Contract, lps: Contract[]): Promise<Contract[]> {
+  const poolLength = await call({ ctx, target: masterchef.address, abi: abi.poolLength })
+
+  const poolInfosRes = await multicall({
+    ctx,
+    calls: rangeBI(0n, poolLength).map((index) => ({ target: masterchef.address, params: [index] } as const)),
+    abi: abi.poolInfo,
+  })
+
+  const contracts: Contract[] = mapSuccessFilter(poolInfosRes, (res, idx) => {
+    const [want, _allocPoint, _lastRewardBlock, _accBELTPerShare, _strat] = res.output
+    return {
+      chain: 'bsc',
+      address: want,
+      token: want,
+      pid: idx,
+      rewards: [BELT],
+    }
+  })
+
+  // Merge with 4belt Lp tokens to get the underlyings
+  const mergedContracts = mergeContractsAndLps(contracts, lps)
+
+  // Research the underlyings associated with farming pools'addresses
+  const underlyingsRes = await multicall({
+    ctx,
+    calls: contracts.map((contract) => ({ target: contract.address })),
+    abi: abi.token,
+  })
+
+  for (const [idx, contract] of contracts.entries()) {
+    const underlying: any = underlyingsRes[idx].success ? [underlyingsRes[idx].output] : undefined
+    contract.underlyings = underlying
+  }
+
+  // Research the underlyings from PancakeSwap LPs
+  const mergedContractsDetails = await getPairsDetails(ctx, mergedContracts)
+
+  for (let i = 0; i < mergedContractsDetails.length; i++) {
+    const contractIndex = mergedContracts.findIndex((c) => c.address === mergedContractsDetails[i].address)
+    if (contractIndex !== -1) {
+      mergedContracts[contractIndex] = Object.assign({}, mergedContracts[contractIndex], mergedContractsDetails[i])
+    }
+  }
+
+  // Format contracts as some pools are using same lpTokens
+  const fmtmergedContracts: Contract[] = mergedContracts.reduce((acc: any, pool) => {
+    const { address, pid, ...rest } = pool
+    if (address in acc) {
+      acc[address].pid.push(pid)
+    } else {
+      acc[address] = { ...rest, pid: [pid] }
+    }
+    return acc
+  }, {})
+
+  return Object.values(fmtmergedContracts).filter((contract) => contract.underlyings && contract.underlyings.length > 0)
+}
+
+function mergeContractsAndLps(contracts: Contract[], lps: Contract[]): Contract[] {
+  return contracts.map((contract) => {
+    const existingLp = lps.find((lp) => lp.address.toLowerCase() === contract.address.toLowerCase())
+
+    if (existingLp) {
+      return {
+        ...existingLp,
+        ...contract,
+      }
+    } else {
+      return contract
+    }
+  })
+}
---
+++ src/adapters/belt-finance/bsc/lp.ts
@@ -0,0 +1,83 @@
+import type { Balance, BalancesContext, Contract } from '@lib/adapter'
+import { mapSuccessFilter, range } from '@lib/array'
+import { abi as erc20Abi } from '@lib/erc20'
+import { multicall } from '@lib/multicall'
+
+const abi = {
+  balances: {
+    name: 'balances',
+    outputs: [{ type: 'uint256', name: '' }],
+    inputs: [{ type: 'int128', name: 'arg0' }],
+    constant: true,
+    payable: false,
+    type: 'function',
+  },
+} as const
+
+export async function getBeltLpBalances(ctx: BalancesContext, lps: Contract[]) {
+  lps = lps.filter((lp) => lp.beltLP === true)
+
+  const userBalancesRes = await multicall({
+    ctx,
+    calls: lps.map((lp) => ({ target: lp.address, params: [ctx.address] } as const)),
+    abi: erc20Abi.balanceOf,
+  })
+
+  const balances: Balance[] = mapSuccessFilter(userBalancesRes, (res, idx) => {
+    const underlyings = lps[idx].underlyings as Contract[]
+
+    return {
+      ...lps[idx],
+      amount: res.output,
+      underlyings,
+      rewards: undefined,
+      category: 'lp',
+    }
+  })
+
+  return getFourBeltUnderlyingsBalances(ctx, balances)
+}
+
+export const getFourBeltUnderlyingsBalances = async (ctx: BalancesContext, poolBalances: Balance[]) => {
+  const balances: Balance[] = []
+
+  const [underlyingsBalancesRes, totalSuppliesRes] = await Promise.all([
+    multicall({
+      ctx,
+      calls: poolBalances.flatMap((poolBalance: Contract) =>
+        range(0, 4).map((idx) => ({ target: poolBalance.pool, params: [idx] } as const)),
+      ),
+      abi: abi.balances,
+    }),
+    multicall({
+      ctx,
+      calls: poolBalances.map((poolBalance: Contract) => ({ target: poolBalance.address } as const)),
+      abi: erc20Abi.totalSupply,
+    }),
+  ])
+
+  let underlyingIdx = 0
+  for (let lpIdx = 0; lpIdx < poolBalances.length; lpIdx++) {
+    const poolBalance = poolBalances[lpIdx]
+    const userBalance = poolBalance.amount
+    const underlyings = poolBalance.underlyings as Balance[]
+    const rewards = poolBalance.rewards as Balance[]
+    const totalSupplyRes = totalSuppliesRes[lpIdx]
+
+    if (!underlyings || !userBalance || !totalSupplyRes.success || totalSupplyRes.output === 0n) {
+      // next lp
+      underlyingIdx += 4
+      continue
+    }
+
+    underlyings.forEach((underlying) => {
+      underlying.amount = (userBalance * (underlyingsBalancesRes[underlyingIdx].output as any)) / totalSupplyRes.output
+
+      underlyingIdx++
+    })
+
+    balances.push({ ...poolBalance, amount: userBalance, underlyings, rewards, category: poolBalance.category })
+  }
+
+  return balances
+}
