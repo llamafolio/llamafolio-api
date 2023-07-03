83af4b9cd341f3078162abc111826cac4c7c4021
0xPeluche <0xpeluche@proton.me>
2023-07-03 12:46:14 +0200

    add IPOR adapter

---
+++ src/adapters/ipor/ethereum/index.ts
@@ -0,0 +1,58 @@
+import { getIporFarmBalances } from '@adapters/ipor/ethereum/farm'
+import { getIporLpBalances } from '@adapters/ipor/ethereum/lp'
+import { getIporStakeBalance } from '@adapters/ipor/ethereum/stake'
+import type { BalancesContext, Contract, GetBalancesHandler } from '@lib/adapter'
+import { resolveBalances } from '@lib/balance'
+
+const assets: Contract[] = [
+  {
+    chain: 'ethereum',
+    address: '0x086d4daab14741b195deE65aFF050ba184B65045',
+    token: '0x8537b194BFf354c4738E9F3C81d67E3371DaDAf8',
+    underlyings: ['0x6B175474E89094C44Da98b954EedeAC495271d0F'],
+  },
+  {
+    chain: 'ethereum',
+    address: '0xC52569b5A349A7055E9192dBdd271F1Bd8133277',
+    token: '0x7c0e72f431FD69560D951e4C04A4de3657621a88',
+    underlyings: ['0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'],
+  },
+  {
+    chain: 'ethereum',
+    address: '0x33C5A44fd6E76Fc2b50a9187CfeaC336A74324AC',
+    token: '0x9Bd2177027edEE300DC9F1fb88F24DB6e5e1edC6',
+    underlyings: ['0xdAC17F958D2ee523a2206206994597C13D831ec7'],
+  },
+]
+
+const farmer: Contract = {
+  chain: 'ethereum',
+  address: '0xcc3fc4c9ba7f8b8aa433bc586d390a70560ff366',
+}
+
+const staker: Contract = {
+  chain: 'ethereum',
+  address: '0xd72915b95c37ae1b16b926f85ad61cca6395409f',
+  underlyings: ['0x1e4746dC744503b53b4A082cB3607B169a289090'],
+}
+
+export const getContracts = () => {
+  return {
+    contracts: { farmer, staker, assets },
+  }
+}
+
+const getIporBalances = async (ctx: BalancesContext, assets: Contract[]) => {
+  return Promise.all([getIporFarmBalances(ctx, assets, farmer), getIporLpBalances(ctx, assets)])
+}
+
+export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
+  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
+    staker: getIporStakeBalance,
+    assets: getIporBalances,
+  })
+
+  return {
+    groups: [{ balances }],
+  }
+}
---
+++ src/adapters/ipor/ethereum/lp.ts
@@ -0,0 +1,61 @@
+import type { Balance, BalancesContext, Contract } from '@lib/adapter'
+import { abi as erc20Abi } from '@lib/erc20'
+import { multicall } from '@lib/multicall'
+import { parseEther } from 'viem'
+
+const abi = {
+  calculateExchangeRate: {
+    inputs: [],
+    name: 'calculateExchangeRate',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+} as const
+
+export async function getIporLpBalances(ctx: BalancesContext, assets: Contract[]): Promise<Balance[]> {
+  const balances: Balance[] = []
+
+  const [userBalancesRes, exchangeRatesRes] = await Promise.all([
+    multicall({
+      ctx,
+      calls: assets.map((asset) => ({ target: asset.token!, params: [ctx.address] } as const)),
+      abi: erc20Abi.balanceOf,
+    }),
+    multicall({
+      ctx,
+      calls: assets.map((asset) => ({ target: asset.address } as const)),
+      abi: abi.calculateExchangeRate,
+    }),
+  ])
+
+  for (let assetIdx = 0; assetIdx < assets.length; assetIdx++) {
+    const asset = assets[assetIdx]
+    const underlying = asset.underlyings?.[0] as Contract
+    const userBalanceRes = userBalancesRes[assetIdx]
+    const exchangeRateRes = exchangeRatesRes[assetIdx]
+
+    if (!underlying || !userBalanceRes.success || !exchangeRateRes.success) {
+      continue
+    }
+
+    balances.push({
+      ...asset,
+      decimals: 18,
+      amount: userBalanceRes.output,
+      underlyings: [
+        { ...underlying, decimals: 18, amount: (userBalanceRes.output * exchangeRateRes.output) / parseEther('1.0') },
+      ],
+      rewards: undefined,
+      category: 'lp',
+    })
+  }
+
+  return balances
+}
