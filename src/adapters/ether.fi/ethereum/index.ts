322090ca1ad08449ac4189149b6153e6914bf1fc
0xPeluche <0xpeluche@proton.me>
2023-07-03 10:14:36 +0200

    Ether.fi now fit with more liquid eth staking

--- src/adapters/ether.fi/ethereum/index.ts
+++ src/adapters/ether.fi/ethereum/index.ts
@@ -6,7 +6,6 @@ import { getEtherBalances } from './stake'
 const staker: Contract = {
   chain: 'ethereum',
   address: '0x7623e9dc0da6ff821ddb9ebaba794054e078f8c4',
-  token: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
 }
 
 export const getContracts = () => {
--- src/adapters/ether.fi/ethereum/stake.ts
+++ src/adapters/ether.fi/ethereum/stake.ts
@@ -1,5 +1,7 @@
 import type { Balance, BalancesContext, Contract } from '@lib/adapter'
-import { call } from '@lib/call'
+import { mapSuccessFilter } from '@lib/array'
+import { multicall } from '@lib/multicall'
+import type { Token } from '@lib/token'
 
 const abi = {
   depositInfo: {
@@ -13,23 +15,58 @@ const abi = {
     stateMutability: 'view',
     type: 'function',
   },
+  userToErc20Balance: {
+    inputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    name: 'userToErc20Balance',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
 } as const
 
-export async function getEtherBalances(ctx: BalancesContext, staker: Contract): Promise<Balance> {
-  const depositInfo = await call({
+const assets: Token[] = [
+  { chain: 'ethereum', address: '0xae78736cd615f374d3085123a210448e74fc6393', decimals: 18, symbol: 'rETH' },
+  { chain: 'ethereum', address: '0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0', decimals: 18, symbol: 'wstETH' },
+  { chain: 'ethereum', address: '0xac3e018457b222d93114458476f3e3416abbe38f', decimals: 18, symbol: 'sfrxETH' },
+  { chain: 'ethereum', address: '0xBe9895146f7AF43049ca1c1AE358B0541Ea49704', decimals: 18, symbol: 'cbETH' },
+  { chain: 'ethereum', address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', decimals: 18, symbol: 'WETH' },
+]
+
+const assetByAddress: { [key: string]: Token } = {}
+assets.forEach((asset) => {
+  assetByAddress[asset.address] = asset
+})
+
+export async function getEtherBalances(ctx: BalancesContext, staker: Contract): Promise<Balance[]> {
+  const userEthBalances = await multicall({
     ctx,
-    target: staker.address,
-    params: [ctx.address],
-    abi: abi.depositInfo,
+    calls: assets.map((asset) => ({ target: staker.address, params: [ctx.address, asset.address] } as const)),
+    abi: abi.userToErc20Balance,
   })
 
-  const [_depositTime, etherBalance, _totalERC20Balance] = depositInfo
-
-  return {
+  return mapSuccessFilter(userEthBalances, (res) => ({
     ...staker,
-    amount: etherBalance,
-    underlyings: undefined,
+    decimals: 18,
+    amount: res.output,
+    underlyings: [assetByAddress[res.input.params[1]]],
     rewards: undefined,
     category: 'stake',
-  }
+  }))
 }
