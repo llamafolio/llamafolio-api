7a524c56677b97e20e548cfc9101ca2030f87b26
0xsign <0xsign@protonmail.com>
2023-07-07 12:08:11 +0200

    feat: wallet adapter check all tokens

--- src/adapters/wallet/index.ts
+++ src/adapters/wallet/index.ts
@@ -1,24 +1,8 @@
-import type { Adapter, BalancesContext, Contract, GetBalancesHandler } from '@lib/adapter'
-import { resolveBalances } from '@lib/balance'
+import type { Adapter, Balance, Contract, GetBalancesHandler } from '@lib/adapter'
 import type { Chain } from '@lib/chains'
 import { chainById, chains } from '@lib/chains'
-import { getBalancesOf } from '@lib/erc20'
-import { providers } from '@lib/providers'
-import type { Token } from '@lib/token'
+import { userBalances } from '@lib/erc20'
 import { chains as tokensByChain } from '@llamafolio/tokens'
-
-async function getCoinBalance(ctx: BalancesContext, token?: Token) {
-  if (!token) {
-    return null
-  }
-
-  const provider = providers[ctx.chain]
-  const amount = await provider.getBalance({
-    address: ctx.address,
-    blockNumber: ctx.blockHeight ? BigInt(ctx.blockHeight) : undefined,
-  })
-  return { ...token, amount }
-}
 
 const getChainHandlers = (chain: Chain) => {
   const getContracts = () => {
@@ -34,12 +18,10 @@ const getChainHandlers = (chain: Chain) => {
     }
   }
 
-  const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
-    const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
-      // @ts-expect-error
-      coin: getCoinBalance,
-      erc20: getBalancesOf,
-    })
+  const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, _contracts) => {
+    // Note: use all balances from llamafolio-tokens directory instead of relying on Transfer interactions
+    const tokenBalances = await userBalances({ chain, walletAddress: ctx.address, tokens: tokensByChain[chain] })
+    const balances: Balance[] = tokenBalances.map((tokenBalance) => ({ ...tokenBalance, category: 'wallet' }))
 
     return {
       groups: [{ balances }],
