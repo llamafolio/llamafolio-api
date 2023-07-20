5664f4a95e1134c7ad277521eaac84f34dc0e5ac
Omar Aziz <github@omaraziz.dev>
2023-07-20 03:45:45 -0700

    fix: typo

--- test/routes.sh
+++ test/routes.sh
@@ -9,115 +9,118 @@ curl --head \
   --silent \
   --show-error \
   --fail \
-  --url https://api.llamafolio.com/adapters
+  --url $API_URL/adapters
 
 curl --head \
   --request GET \
   --silent \
   --show-error \
   --fail \
-  --url https://api.llamafolio.com/balances/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50
+  --url $API_URL/balances/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50
 
 curl --head \
   --request GET \
   --silent \
   --show-error \
   --fail \
-  --url https://api.llamafolio.com/balances/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50/latest
+  --url $API_URL/balances/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50/latest
 
 curl --head \
   --request GET \
   --silent \
   --show-error \
   --fail \
-  --url https://api.llamafolio.com/balances/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50/tokens
+  --url $API_URL/balances/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50/tokens
 
 curl --head \
   --request GET \
   --silent \
   --show-error \
   --fail \
-  --url https://api.llamafolio.com/contracts/0x0d8775f648430679a709e98d2b0cb6250d2887ef
+  --url $API_URL/contracts/0x0d8775f648430679a709e98d2b0cb6250d2887ef
 
-# curl --head \
-#   --request GET \
-#   --silent \
-#   --show-error \
-#   --fail \
-#   --url https://api.llamafolio.com/gas_price/ethereum/chart
+ 
 
 curl --head \
   --request GET \
   --silent \
   --show-error \
   --fail \
-  --url https://api.llamafolio.com/history/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50
+  --url $API_URL/history/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50
 
 curl --head \
   --request GET \
   --silent \
   --show-error \
   --fail \
-  --url https://api.llamafolio.com/info/stats
+  --url $API_URL/info/stats
 
 curl --head \
   --request GET \
   --silent \
   --show-error \
   --fail \
-  --url https://api.llamafolio.com/info/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50
+  --url $API_URL/info/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50
 
 curl --head \
   --request GET \
   --silent \
   --show-error \
   --fail \
-  --url https://api.llamafolio.com/labels/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50
+  --url $API_URL/labels/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50
 
 curl --head \
   --request GET \
   --silent \
   --show-error \
   --fail \
-  --url https://api.llamafolio.com/protocols
+  --url $API_URL/protocols
 
 curl --head \
   --request GET \
   --silent \
   --show-error \
   --fail \
-  --url https://api.llamafolio.com/protocols/lates
-curl --head \
-  --request GET \
-  --silent \
-  --show-error \
-  --fail \
-  --url https://api.llamafolio.com/snapshots/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50/latest
+  --url $API_URL/protocols/latest
 
 curl --head \
   --request GET \
   --silent \
   --show-error \
   --fail \
-  --url https://api.llamafolio.com/sync_status
+  --url $API_URL/snapshots/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50/latest
 
 curl --head \
   --request GET \
   --silent \
   --show-error \
   --fail \
-  --url https://api.llamafolio.com/tokens/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50
+  --url $API_URL/sync_status
 
 curl --head \
   --request GET \
   --silent \
   --show-error \
   --fail \
-  --url https://api.llamafolio.com/tokens/ethereum/0x6b175474e89094c44da98b954eedeac495271d0f
+  --url $API_URL/tokens/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50
 
 curl --head \
   --request GET \
   --silent \
   --show-error \
   --fail \
-  --url https://api.llamafolio.com/holders/0x6b175474e89094c44da98b954eedeac495271d0f
+  --url $API_URL/tokens/ethereum/0x6b175474e89094c44da98b954eedeac495271d0f
+
+curl --head \
+  --request GET \
+  --silent \
+  --show-error \
+  --fail \
+  --url $API_URL/holders/0x6b175474e89094c44da98b954eedeac495271d0f
+
+curl --head \
+  --request GET \
+  --silent \
+  --show-error \
+  --fail \
+  --url $CLOUDFLARE_R2_PUBLIC_URL/nft/llama_nft_collections.json
