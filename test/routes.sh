fbca53b6e05785614eb3300d6bdb001dbc9300d4
Omar Aziz <github@omaraziz.dev>
2023-07-20 03:38:39 -0700

    fix: comment out gas_price curl

--- test/routes.sh
+++ test/routes.sh
@@ -4,121 +4,120 @@ set -eou pipefail
 
 # This script is used to check against lambda endpoints to ensure they are working as expected.
 
-curl --request GET \
+curl --head \
+  --request GET \
   --silent \
-  --output /dev/null \
   --show-error \
   --fail \
-  --url $API_URL/adapters
+  --url https://api.llamafolio.com/adapters
 
-curl --request GET \
+curl --head \
+  --request GET \
   --silent \
-  --output /dev/null \
   --show-error \
   --fail \
-  --url $API_URL/balances/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50
+  --url https://api.llamafolio.com/balances/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50
 
-curl --request GET \
+curl --head \
+  --request GET \
   --silent \
-  --output /dev/null \
   --show-error \
   --fail \
-  --url $API_URL/balances/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50/latest
+  --url https://api.llamafolio.com/balances/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50/latest
 
-curl --request GET \
+curl --head \
+  --request GET \
   --silent \
-  --output /dev/null \
   --show-error \
   --fail \
-  --url $API_URL/balances/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50/tokens
+  --url https://api.llamafolio.com/balances/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50/tokens
 
-curl --request GET \
+curl --head \
+  --request GET \
   --silent \
-  --output /dev/null \
   --show-error \
   --fail \
-  --url $API_URL/contracts/0x0d8775f648430679a709e98d2b0cb6250d2887ef
+  --url https://api.llamafolio.com/contracts/0x0d8775f648430679a709e98d2b0cb6250d2887ef
 
-curl --request GET \
-  --silent \
-  --output /dev/null \
-  --show-error \
-  --fail \
-  --url $API_URL/gas_price/ethereum/chart
+# curl --head \
+#   --request GET \
+#   --silent \
+#   --show-error \
+#   --fail \
+#   --url https://api.llamafolio.com/gas_price/ethereum/chart
 
-curl --request GET \
+curl --head \
+  --request GET \
   --silent \
-  --output /dev/null \
   --show-error \
   --fail \
-  --url $API_URL/history/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50
+  --url https://api.llamafolio.com/history/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50
 
-curl --request GET \
+curl --head \
+  --request GET \
   --silent \
-  --output /dev/null \
   --show-error \
   --fail \
-  --url $API_URL/info/stats
+  --url https://api.llamafolio.com/info/stats
 
-curl --request GET \
+curl --head \
+  --request GET \
   --silent \
-  --output /dev/null \
   --show-error \
   --fail \
-  --url $API_URL/info/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50
+  --url https://api.llamafolio.com/info/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50
 
-curl --request GET \
+curl --head \
+  --request GET \
   --silent \
-  --output /dev/null \
   --show-error \
   --fail \
-  --url $API_URL/labels/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50
+  --url https://api.llamafolio.com/labels/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50
 
-curl --request GET \
+curl --head \
+  --request GET \
   --silent \
-  --output /dev/null \
   --show-error \
   --fail \
-  --url $API_URL/protocols
+  --url https://api.llamafolio.com/protocols
 
-curl --request GET \
+curl --head \
+  --request GET \
   --silent \
-  --output /dev/null \
   --show-error \
   --fail \
-  --url $API_URL/protocols/latest
+  --url https://api.llamafolio.com/protocols/lates
+curl --head \
+  --request GET \
+  --silent \
+  --show-error \
+  --fail \
+  --url https://api.llamafolio.com/snapshots/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50/latest
 
-curl --request GET \
+curl --head \
+  --request GET \
   --silent \
-  --output /dev/null \
   --show-error \
   --fail \
-  --url $API_URL/snapshots/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50/latest
+  --url https://api.llamafolio.com/sync_status
 
-curl --request GET \
+curl --head \
+  --request GET \
   --silent \
-  --output /dev/null \
   --show-error \
   --fail \
-  --url $API_URL/sync_status
+  --url https://api.llamafolio.com/tokens/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50
 
-curl --request GET \
+curl --head \
+  --request GET \
   --silent \
-  --output /dev/null \
   --show-error \
   --fail \
-  --url $API_URL/tokens/0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50
+  --url https://api.llamafolio.com/tokens/ethereum/0x6b175474e89094c44da98b954eedeac495271d0f
 
-curl --request GET \
+curl --head \
+  --request GET \
   --silent \
-  --output /dev/null \
   --show-error \
   --fail \
-  --url $API_URL/tokens/ethereum/0x6b175474e89094c44da98b954eedeac495271d0f
-
-curl --request GET \
-  --silent \
-  --output /dev/null \
-  --show-error \
-  --fail \
-  --url $API_URL/holders/0x6b175474e89094c44da98b954eedeac495271d0f
+  --url https://api.llamafolio.com/holders/0x6b175474e89094c44da98b954eedeac495271d0f
