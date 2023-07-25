50a39706236a8abaa4540fa49661fa2f0a1f2c73
0xPeluche <0xpeluche@proton.me>
2023-07-25 13:13:03 +0200

    Fix rewards Gravita

--- src/adapters/gravita-protocol/ethereum/index.ts
+++ src/adapters/gravita-protocol/ethereum/index.ts
@@ -7,12 +7,7 @@ const StabilityPool: Contract = {
   chain: 'ethereum',
   address: '0x4f39f12064d83f6dd7a2bdb0d53af8be560356a6',
   token: '0x15f74458aE0bFdAA1a96CA1aa779D715Cc1Eefe4',
-  rewards: [
-    '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
-    '0xae78736Cd615f374D3085123A210448E74Fc6393',
-    '0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0',
-    '0xB9D7DdDca9a4AC480991865EfEf82E01273F79C3',
-  ],
+  rewards: ['0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0', '0xae78736Cd615f374D3085123A210448E74Fc6393'],
 }
 
 const borrowerOperations: Contract = {
