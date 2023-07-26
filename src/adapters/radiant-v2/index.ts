41948f5189f6b514488124a047ef61a8eb294bda
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2023-07-26 12:12:36 +0200

    feat: Adapter, Radiant-v2 on BSC (#748)
    * feat, Adapter, Radiant on BSC
    
    * Add healthfactor

--- src/adapters/radiant-v2/index.ts
+++ src/adapters/radiant-v2/index.ts
@@ -1,10 +1,12 @@
 import type { Adapter } from '@lib/adapter'
 
 import * as arbitrum from './arbitrum'
+import * as bsc from './bsc'
 
 const adapter: Adapter = {
   id: 'radiant-v2',
   arbitrum,
+  bsc,
 }
 
 export default adapter
