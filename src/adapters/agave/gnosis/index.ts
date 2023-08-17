bfaa6370d5c297a399bf3266ccc80964d8d00004
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2023-08-17 15:50:27 +0200

    feat: Adater, Agave (#768)

---
+++ src/adapters/agave/gnosis/index.ts
@@ -0,0 +1,37 @@
+import { getAgaveStakeBalance } from '@adapters/agave/gnosis/stake'
+import { getLendingPoolBalances, getLendingPoolContracts, getLendingPoolHealthFactor } from '@lib/aave/v2/lending'
+import type { BaseContext, Contract, GetBalancesHandler } from '@lib/adapter'
+import { resolveBalances } from '@lib/balance'
+
+const lendingPool: Contract = {
+  chain: 'gnosis',
+  address: '0x5e15d5e33d318dced84bfe3f4eace07909be6d9c',
+}
+
+const staker: Contract = {
+  chain: 'gnosis',
+  address: '0x610525b415c1bfaeab1a3fc3d85d87b92f048221',
+  underlyings: ['0x3a97704a1b25f08aa230ae53b352e2e72ef52843'],
+}
+
+export const getContracts = async (ctx: BaseContext) => {
+  const pools = await getLendingPoolContracts(ctx, lendingPool)
+
+  return {
+    contracts: { staker, pools },
+  }
+}
+
+export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
+  const [balances, healthFactor] = await Promise.all([
+    resolveBalances<typeof getContracts>(ctx, contracts, {
+      pools: getLendingPoolBalances,
+      staker: getAgaveStakeBalance,
+    }),
+    getLendingPoolHealthFactor(ctx, lendingPool),
+  ])
+
+  return {
+    groups: [{ balances, healthFactor }],
+  }
+}
