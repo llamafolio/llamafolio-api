598e1573c81863702bc4e12bb46036f3ced37ca4
0xPeluche <0xpeluche@proton.me>
2023-08-21 15:35:09 +0200

    Magnate-finance

---
+++ src/adapters/magnate-finance/base/index.ts
@@ -0,0 +1,36 @@
+import { getMagnateMarketsBalances } from '@adapters/magnate-finance/base/balance'
+import type { BaseContext, Contract, GetBalancesHandler } from '@lib/adapter'
+import { resolveBalances } from '@lib/balance'
+import { getMarketsContracts } from '@lib/compound/v2/lending'
+
+const cEther: Contract = {
+  chain: 'base',
+  address: '0x68725461357B7e5e059A224B3b2fC45F3654c889',
+  symbol: 'cETH',
+  decimals: 18,
+  collateralFactor: 800000000000000000n,
+  underlyings: ['0x4200000000000000000000000000000000000006'],
+  rewards: undefined,
+}
+
+export const getContracts = async (ctx: BaseContext) => {
+  console.log(cEther)
+
+  const markets = await getMarketsContracts(ctx, {
+    comptrollerAddress: '0x9C1925d9fA1E9ba7aa57db36B15E29C07f5d85e2',
+  })
+
+  return {
+    contracts: { markets: [...markets, cEther] },
+  }
+}
+
+export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
+  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
+    markets: getMagnateMarketsBalances,
+  })
+
+  return {
+    groups: [{ balances }],
+  }
+}
