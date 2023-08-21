7209e05a674791fb8b2f9488eb0b6d0ff0f94545
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2023-08-21 15:39:32 +0200

    feat: fix on qiAvax token+ explain exchangeRate (#772)

--- src/adapters/benqi-lending/avalanche/index.ts
+++ src/adapters/benqi-lending/avalanche/index.ts
@@ -2,7 +2,6 @@ import type { BaseContext, GetBalancesHandler } from '@lib/adapter'
 import { resolveBalances } from '@lib/balance'
 import type { BalanceWithExtraProps } from '@lib/compound/v2/lending'
 import { getHealthFactor, getMarketsBalances, getMarketsContracts } from '@lib/compound/v2/lending'
-import { ADDRESS_ZERO } from '@lib/contract'
 
 export const getContracts = async (ctx: BaseContext) => {
   const markets = await getMarketsContracts(ctx, {
@@ -10,7 +9,7 @@ export const getContracts = async (ctx: BaseContext) => {
     comptrollerAddress: '0x486Af39519B4Dc9a7fCcd318217352830E8AD9b4',
     underlyingAddressByMarketAddress: {
       // qiAVAX -> AVAX
-      '0x5c0401e81bc07ca70fad469b451682c0d747ef1c': ADDRESS_ZERO,
+      '0x5c0401e81bc07ca70fad469b451682c0d747ef1c': '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7',
     },
   })
   return {
