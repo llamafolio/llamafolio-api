04bd58c5067e209645dc18da931d692724872ebc
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2023-08-21 16:46:06 +0200

    feat: Fix Stargate (SGETH to WETH) + add Base (#774)

---
+++ src/adapters/stargate/base/index.ts
@@ -0,0 +1,44 @@
+import type { BalancesContext, BaseContext, Contract, GetBalancesHandler } from '@lib/adapter'
+import { resolveBalances } from '@lib/balance'
+import type { Token } from '@lib/token'
+
+import { getStargateLpContracts } from '../common/contract'
+import { getStargateFarmBalances } from '../common/farm'
+import { getStargateLPBalances } from '../common/lp'
+
+const STG: Token = {
+  chain: 'base',
+  address: '0xE3B53AF74a4BF62Ae5511055290838050bf764Df',
+  decimals: 18,
+  symbol: 'STG',
+}
+
+// https://stargateprotocol.gitbook.io/stargate/developers/contract-addresses/mainnet
+const lpStakings: Contract[] = [
+  { chain: 'base', address: '0x4c80e24119cfb836cdf0a6b53dc23f04f7e652ca', rewards: [STG] },
+  { chain: 'base', address: '0x28fc411f9e1c480ad312b3d9c60c22b965015c6b', rewards: [STG] },
+]
+
+const farmStakings: Contract[] = [{ chain: 'base', address: '0x06eb48763f117c7be887296cdcdfad2e4092739c' }]
+
+export const getContracts = async (ctx: BaseContext) => {
+  const pools = await getStargateLpContracts(ctx, lpStakings)
+
+  return {
+    contracts: { pools },
+  }
+}
+
+const stargateBalances = async (ctx: BalancesContext, pools: Contract[]) => {
+  return Promise.all([getStargateLPBalances(ctx, pools), getStargateFarmBalances(ctx, pools, farmStakings)])
+}
+
+export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
+  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
+    pools: stargateBalances,
+  })
+
+  return {
+    groups: [{ balances }],
+  }
+}
--- src/adapters/stargate/common/contract.ts
+++ src/adapters/stargate/common/contract.ts
@@ -11,6 +11,13 @@ const abi = {
   },
 } as const
 
+const ETH: { [key: string]: `0x${string}`[] } = {
+  arbitrum: ['0x82cbecf39bee528b5476fe6d1550af59a9db6fc0', '0x82af49447d8a07e3bd95bd0d56f35241523fbab1'],
+  base: ['0x224d8fd7ab6ad4c6eb4611ce56ef35dec2277f03', '0x4200000000000000000000000000000000000006'],
+  ethereum: ['0x72E2F4830b9E45d52F80aC08CB2bEC0FeF72eD9c', '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'],
+  optimism: ['0xb69c8cbcd90a39d8d3d3ccf0a3e968511c3856a0', '0x4200000000000000000000000000000000000006'],
+}
+
 export async function getStargateLpContracts(ctx: BaseContext, lpStakers: Contract[]): Promise<Contract[]> {
   const contracts: Contract[] = []
 
@@ -28,7 +35,11 @@ export async function getStargateLpContracts(ctx: BaseContext, lpStakers: Contra
       continue
     }
 
-    contracts.push({ ...lpStaker, underlyings: [underlyingRes.output] })
+    // replace SGETH to WETH
+    const fmtUnderlyings =
+      ETH[ctx.chain][0].toLowerCase() === underlyingRes.output.toLowerCase() ? ETH[ctx.chain][1] : underlyingRes.output
+
+    contracts.push({ ...lpStaker, underlyings: [fmtUnderlyings] })
   }
 
   return contracts
