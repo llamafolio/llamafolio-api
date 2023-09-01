af994e39c81d9b3c20fc91a41d4a69364ac0c319
0xsign <0xsign@protonmail.com>
2023-09-01 17:05:22 +0200

    feat: sync status only return max block number

--- src/handlers/getSyncStatus.ts
+++ src/handlers/getSyncStatus.ts
@@ -7,7 +7,7 @@ export const handler: APIGatewayProxyHandler = async () => {
     const clickhouseClient = connect()
 
     const lastSyncedBlocksQueryRes = await clickhouseClient.query({
-      query: `SELECT chain, count() AS count, max(number) AS max FROM evm_indexer.blocks GROUP BY chain;`,
+      query: `SELECT chain, max(number) AS max FROM evm_indexer.blocks GROUP BY chain;`,
     })
 
     const lastSyncedBlocksRes = (await lastSyncedBlocksQueryRes.json()) as {
@@ -17,7 +17,6 @@ export const handler: APIGatewayProxyHandler = async () => {
     const response = lastSyncedBlocksRes.data.map((row) => ({
       chain: parseInt(row.chain),
       max_block_number: parseInt(row.max),
-      count_blocks: parseInt(row.count),
     }))
 
     return success(response, { maxAge: 60 })
