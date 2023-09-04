a381d09fdbc52a397cc957e0cd8d7d6e76625796
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2023-09-04 15:55:16 +0200

    feat: Update extra_finance - Base (#819)

--- src/adapters/extra-finance/index.ts
+++ src/adapters/extra-finance/index.ts
@@ -1,12 +1,14 @@
 import type { Adapter } from '@lib/adapter'
 
+import * as base from './base'
 import * as optimism from './optimism'
 
 const adapter: Adapter = {
   id: 'extra-finance',
   optimism,
+  base,
 }
 
-// TODO: Find how to get Extra emission + Leveraged Farming when contract when contracts have been verified on optimismScan
+// TODO: Find how to get Extra emission + Leveraged Farming when contracts have been verified on optimismScan
 
 export default adapter
