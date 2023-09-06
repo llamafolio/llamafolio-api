4d2618c785ec5a74c08bba3e7e8ee052fb2f95c6
0xsign <0xsign@protonmail.com>
2023-09-06 23:53:47 +0200

    fix: connection pool

--- src/db/clickhouse.ts
+++ src/db/clickhouse.ts
@@ -17,7 +17,15 @@ export function connect() {
       host: process.env.CLICKHOUSE_HOST || 'http://localhost:8123',
       username: process.env.CLICKHOUSE_USER || 'default',
       password: process.env.CLICKHOUSE_PASSWORD || '',
-      max_open_connections: 1,
+      keep_alive: {
+        enabled: true,
+        // should be slightly less than the `keep_alive_timeout` setting in server's `config.xml`
+        // default is 3s there, so 2500 milliseconds seems to be a safe client value in this scenario
+        // another example: if your configuration has `keep_alive_timeout` set to 60s, you could put 59_000 here
+        socket_ttl: 2500,
+        // default: false
+        retry_on_expired_socket: true,
+      },
       clickhouse_settings: {
         async_insert: 1,
         wait_for_async_insert: 1,
