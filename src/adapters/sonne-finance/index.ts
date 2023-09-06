6df028342f6f552ca272fae995670a09d7b5d36e
0xPeluche <0xpeluche@proton.me>
2023-09-06 14:02:28 +0200

    feat: Update Sonne finance --> Base

--- src/adapters/sonne-finance/index.ts
+++ src/adapters/sonne-finance/index.ts
@@ -1,10 +1,12 @@
 import type { Adapter } from '@lib/adapter'
 
+import * as base from './base'
 import * as optimism from './optimism'
 
 const adapter: Adapter = {
   id: 'sonne-finance',
   optimism,
+  base,
 }
 
 export default adapter
