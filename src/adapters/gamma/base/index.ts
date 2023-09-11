8fe91b1493adc90ef62af1b9ec272f5ca4882fe5
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2023-09-11 17:45:53 +0200

    feat: update Gamma alt chains (#855)
    * feat: update Gamma alt chains
    
    * rm log

--- src/adapters/gamma/base/index.ts
+++ src/adapters/gamma/base/index.ts
@@ -4,13 +4,7 @@ import { resolveBalances } from '@lib/balance'
 import { getGammaFarmBalances } from '../common/balance'
 import { getPoolContractsFromAPI } from '../common/contract'
 
-const API_URLs = [
-  'https://wire2.gamma.xyz/arbitrum/hypervisors/allData',
-  'https://wire2.gamma.xyz/zyberswap/arbitrum/hypervisors/allData',
-  'https://wire2.gamma.xyz/sushi/arbitrum/hypervisors/allData',
-  'https://wire2.gamma.xyz/camelot/arbitrum/hypervisors/allData',
-  'https://wire2.gamma.xyz/ramses/arbitrum/hypervisors/allData',
-]
+const API_URLs = ['https://wire2.gamma.xyz/synthswap/base/hypervisors/allData']
 
 export const getContracts = async (ctx: BaseContext) => {
   const pools = await getPoolContractsFromAPI(ctx, API_URLs)
