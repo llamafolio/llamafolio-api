a11d5a929f8929e4738bd89873a76c8257de98e2
0xsign <99134502+0xsign@users.noreply.github.com>
2023-09-11 11:28:46 +0200

    feat: coinbase wrapped staked eth multi chain (#845)

--- src/adapters/coinbase-wrapped-staked-eth/index.ts
+++ src/adapters/coinbase-wrapped-staked-eth/index.ts
@@ -1,10 +1,18 @@
 import type { Adapter } from '@lib/adapter'
 
+import * as arbitrum from './arbitrum'
+import * as base from './base'
 import * as ethereum from './ethereum'
+import * as optimism from './optimism'
+import * as polygon from './polygon'
 
 const adapter: Adapter = {
   id: 'coinbase-wrapped-staked-eth',
+  arbitrum,
+  base,
   ethereum,
+  optimism,
+  polygon,
 }
 
 export default adapter
