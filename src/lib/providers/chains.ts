afb4000c1efef695802b0c35fab6a01288bc2eab
0xsign <99134502+0xsign@users.noreply.github.com>
2023-09-14 08:10:28 +0200

    bump: llamafolio-tokens 0.1.93 (#869)

--- src/lib/providers/chains.ts
+++ src/lib/providers/chains.ts
@@ -14,6 +14,7 @@ import {
   optimism,
   polygon,
   polygonZkEvm,
+  zkSync,
 } from 'viem/chains'
 
 const gnosis = {
@@ -67,4 +68,5 @@ export const viemChainById: { [key in Chain]: _Chain } = {
   optimism,
   polygon,
   'polygon-zkevm': polygonZkEvm,
+  'zksync-era': zkSync,
 } as const
