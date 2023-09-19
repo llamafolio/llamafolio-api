99b6607f633e90f3b9f55f14c227abfed88d7368
0xPeluche <0xpeluche@proton.me>
2023-09-19 11:12:09 +0200

    Solidlizard, add lp & farm & lock

--- src/adapters/solidlizard/arbitrum/pair.ts
+++ src/adapters/solidlizard/arbitrum/pair.ts
@@ -1,181 +1,53 @@
 import type { BaseContext, Contract } from '@lib/adapter'
-import { gql, request } from 'graphql-request'
+import { mapSuccessFilter, rangeBI } from '@lib/array'
+import { call } from '@lib/call'
+import { multicall } from '@lib/multicall'
+import { getPairsDetails } from '@lib/uniswap/v2/factory'
 
-const THE_GRAPH_URL = 'https://api.thegraph.com/subgraphs/name/solidlizardfinance/sliz'
+const abi = {
+  poolsLength: {
+    inputs: [],
+    name: 'poolsLength',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  pools: {
+    inputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    name: 'pools',
+    outputs: [{ internalType: 'address', name: '', type: 'address' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  gauges: {
+    inputs: [{ internalType: 'address', name: '', type: 'address' }],
+    name: 'gauges',
+    outputs: [{ internalType: 'address', name: '', type: 'address' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+} as const
 
-const pairsQuery = gql`
-  {
-    pairs(first: 1000) {
-      id
-      name
-      symbol
-      isStable
-      reserve0
-      reserve1
-      token0Price
-      token1Price
-      totalSupply
-      reserveUSD
-      reserveETH
-      token0 {
-        id
-        symbol
-        name
-        decimals
-        isWhitelisted
-        derivedETH
-      }
-      token1 {
-        id
-        symbol
-        name
-        decimals
-        isWhitelisted
-        derivedETH
-      }
-      gauge {
-        id
-        totalSupply
-        totalSupplyETH
-        expectAPR
-        expectAPRDerived
-        voteWeight
-        totalWeight
-        totalDerivedSupply
-        bribe {
-          id
-        }
-        rewardTokens {
-          apr
-          derivedAPR
-          left
-          finishPeriod
-          token {
-            id
-            symbol
-            decimals
-            derivedETH
-          }
-        }
-      }
-      gaugebribes {
-        id
-        bribeTokens {
-          apr
-          left
-          token {
-            symbol
-          }
-        }
-      }
-    }
-  }
-`
+export async function getSolidlizardContracts(ctx: BaseContext, voter: Contract): Promise<Contract[]> {
+  const poolLength = await call({ ctx, target: voter.address, abi: abi.poolsLength })
 
-export interface PairsResponse {
-  pairs: Pair[]
-}
+  const poolsAddressesRes = await multicall({
+    ctx,
+    calls: rangeBI(0n, poolLength).map((idx) => ({ target: voter.address, params: [idx] }) as const),
+    abi: abi.pools,
+  })
 
-export interface Pair {
-  id: string
-  name: string
-  symbol: string
-  isStable: boolean
-  reserve0: string
-  reserve1: string
-  token0Price: string
-  token1Price: string
-  totalSupply: string
-  reserveUSD: string
-  reserveETH: string
-  token0: Token0Class
-  token1: Token0Class
-  gauge: Gauge | null
-  gaugebribes: Gaugebribes | null
-}
+  const gaugesAddressesRes = await multicall({
+    ctx,
+    calls: mapSuccessFilter(poolsAddressesRes, (res) => ({ target: voter.address, params: [res.output] }) as const),
+    abi: abi.gauges,
+  })
 
-export interface Gauge {
-  id: string
-  totalSupply: string
-  totalSupplyETH: string
-  expectAPR: string
-  expectAPRDerived: string
-  voteWeight: string
-  totalWeight: string
-  totalDerivedSupply: string
-  bribe: Bribe
-  rewardTokens: RewardToken[]
-}
-
-export interface Bribe {
-  id: string
-}
-
-export interface RewardToken {
-  apr: string
-  derivedAPR: string
-  left: string
-  finishPeriod: string
-  token: RewardTokenToken
-}
-
-export interface RewardTokenToken {
-  id: string
-  symbol: string
-  decimals: string
-  derivedETH: string
-}
-
-export interface Gaugebribes {
-  id: string
-  bribeTokens: BribeToken[]
-}
-
-export interface BribeToken {
-  apr: string
-  left: string
-  token: BribeTokenToken
-}
-
-export interface BribeTokenToken {
-  symbol: string
-}
-
-export interface Token0Class {
-  id: string
-  symbol: string
-  name: string
-  decimals: string
-  isWhitelisted: boolean
-  derivedETH: string
-}
-
-export interface GaugeContract extends Contract {
-  token: `0x${string}`
-  bribeAddress?: `0x${string}`
-  feesAddress?: `0x${string}`
-}
-
-export async function getPairsContracts(ctx: BaseContext) {
-  const pairsRes: PairsResponse = await request(THE_GRAPH_URL, pairsQuery)
-
-  const pairs: Contract[] = (pairsRes.pairs || []).map((pair) => ({
+  const pools = mapSuccessFilter(gaugesAddressesRes, (res) => ({
     chain: ctx.chain,
-    address: pair.id,
-    stable: pair.isStable,
-    category: 'lp',
-    underlyings: [pair.token0.id, pair.token1.id],
+    address: res.input.params[0],
+    gauge: res.output,
   }))
 
-  const gauges: GaugeContract[] = (pairsRes.pairs || [])
-    .filter((pair) => pair.gauge)
-    .map((pair) => ({
-      chain: ctx.chain,
-      address: pair.gauge!.id,
-      token: pair.id,
-      stable: pair.isStable,
-      underlyings: [pair.token0.id, pair.token1.id],
-    }))
-
-  return { pairs, gauges }
+  return getPairsDetails(ctx, pools)
 }
