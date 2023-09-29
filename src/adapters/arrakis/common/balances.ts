e95a0060bb2251c60e0fc2a3da108b4897cee793
0xPeluche <0xpeluche@proton.me>
2023-09-29 14:52:03 +0200

    feat: Arrakis v2 eth

--- src/adapters/arrakis/common/balances.ts
+++ src/adapters/arrakis/common/balances.ts
@@ -22,16 +22,37 @@ const abi = {
     stateMutability: 'view',
     type: 'function',
   },
+  claimable_reward: {
+    stateMutability: 'view',
+    type: 'function',
+    name: 'claimable_reward',
+    inputs: [
+      { name: '_user', type: 'address' },
+      { name: '_reward_token', type: 'address' },
+    ],
+    outputs: [{ name: '', type: 'uint256' }],
+    gas: 26593,
+  },
+  totalUnderlyingWithFees: {
+    inputs: [{ internalType: 'contract IArrakisV2', name: 'vault_', type: 'address' }],
+    name: 'totalUnderlyingWithFees',
+    outputs: [
+      { internalType: 'uint256', name: 'amount0', type: 'uint256' },
+      { internalType: 'uint256', name: 'amount1', type: 'uint256' },
+      { internalType: 'uint256', name: 'fee0', type: 'uint256' },
+      { internalType: 'uint256', name: 'fee1', type: 'uint256' },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
 } as const
 
 export async function getLpBalances(ctx: BalancesContext, contracts: Contract[]) {
   const balances: Balance[] = []
 
-  const balancesRaw = await getBalancesOf(ctx, contracts as Token[])
+  const balancesRaw = (await getBalancesOf(ctx, contracts as Token[])).filter((balance) => balance.amount > 0n)
 
-  const nonZeroBalances = balancesRaw.filter((balance) => balance.amount > 0n)
-
-  const calls = nonZeroBalances.map((balance) => ({
+  const calls = balancesRaw.map((balance) => ({
     target: balance.address,
   }))
 
@@ -43,28 +64,26 @@ export async function getLpBalances(ctx: BalancesContext, contracts: Contract[])
   for (let i = 0; i < calls.length; i++) {
     const underlyingBalances = underlyingBalancesRes[i]
     const totalSupply = totalSupplyRes[i]
+
     if (!underlyingBalances.success || !totalSupply.success) {
       continue
     }
 
     const [amount0Current, amount1Current] = underlyingBalances.output
+    ;(balancesRaw[i].underlyings![0] as Balance).amount = (balancesRaw[i].amount * amount0Current) / totalSupply.output
+    ;(balancesRaw[i].underlyings![1] as Balance).amount = (balancesRaw[i].amount * amount1Current) / totalSupply.output
 
-    ;(nonZeroBalances[i].underlyings![0] as Balance).amount =
-      (nonZeroBalances[i].amount * amount0Current) / totalSupply.output
-    ;(nonZeroBalances[i].underlyings![1] as Balance).amount =
-      (nonZeroBalances[i].amount * amount1Current) / totalSupply.output
-
-    nonZeroBalances[i].category = 'lp'
-    balances.push(nonZeroBalances[i])
+    balancesRaw[i].category = 'lp'
+    balances.push(balancesRaw[i])
   }
 
   return balances
 }
 
-export async function getArrakisFarmBalances(ctx: BalancesContext, farmers: Contract[]): Promise<Balance[]> {
+export async function getArrakisV1FarmBalances(ctx: BalancesContext, farmers: Contract[]): Promise<Balance[]> {
   const balances: Balance[] = []
 
-  const [userBalancesRes, underlyingBalancesRes, totalSuppliesRes] = await Promise.all([
+  const [userBalancesRes, underlyingBalancesRes, totalSuppliesRes, userPendingRewardsRes] = await Promise.all([
     multicall({
       ctx,
       calls: farmers.map((farmer) => ({ target: farmer.address, params: [ctx.address] }) as const),
@@ -80,7 +99,18 @@ export async function getArrakisFarmBalances(ctx: BalancesContext, farmers: Cont
       calls: farmers.map((farmer) => ({ target: farmer.address }) as const),
       abi: erc20Abi.totalSupply,
     }),
+    multicall({
+      ctx,
+      calls: farmers.flatMap((farmer) =>
+        farmer.rewards!.map(
+          (reward) => ({ target: farmer.address, params: [ctx.address, (reward as Contract).address] }) as const,
+        ),
+      ),
+      abi: abi.claimable_reward,
+    }),
   ])
+
+  let rewardGlobalIndex = 0
 
   for (const [index, farmer] of farmers.entries()) {
     const underlyings = farmer.underlyings as Contract[]
@@ -98,8 +128,24 @@ export async function getArrakisFarmBalances(ctx: BalancesContext, farmers: Cont
       continue
     }
 
-    const [amount0Current, amount1Current] = underlyingBalances.output
+    const rewardsMapped: any[] = []
 
+    if (farmer.rewards) {
+      for (const reward of farmer.rewards as Contract[]) {
+        const userPendingReward = userPendingRewardsRes[rewardGlobalIndex]
+
+        if (userPendingReward && userPendingReward.success) {
+          rewardsMapped.push({
+            ...reward,
+            amount: userPendingReward.output,
+          })
+        }
+
+        rewardGlobalIndex++
+      }
+    }
+
+    const [amount0Current, amount1Current] = underlyingBalances.output
     const underlyings0 = { ...underlyings[0], amount: (userBalanceRes.output * amount0Current) / totalSupplyRes.output }
     const underlyings1 = { ...underlyings[1], amount: (userBalanceRes.output * amount1Current) / totalSupplyRes.output }
 
@@ -107,7 +153,93 @@ export async function getArrakisFarmBalances(ctx: BalancesContext, farmers: Cont
       ...farmer,
       amount: userBalanceRes.output,
       underlyings: [underlyings0, underlyings1],
-      rewards: undefined,
+      rewards: rewardsMapped.length ? rewardsMapped : undefined,
+      category: 'farm',
+    })
+  }
+
+  return balances
+}
+
+export async function getArrakisV2FarmBalances(
+  ctx: BalancesContext,
+  farmers: Contract[],
+  helper: Contract,
+): Promise<Balance[]> {
+  const balances: Balance[] = []
+
+  const [userBalancesRes, totalSuppliesRes, userPendingRewardsRes, underlyingBalancesRes] = await Promise.all([
+    multicall({
+      ctx,
+      calls: farmers.map((farmer) => ({ target: farmer.address, params: [ctx.address] }) as const),
+      abi: erc20Abi.balanceOf,
+    }),
+    multicall({
+      ctx,
+      calls: farmers.map((farmer) => ({ target: farmer.address }) as const),
+      abi: erc20Abi.totalSupply,
+    }),
+    multicall({
+      ctx,
+      calls: farmers.flatMap((farmer) =>
+        farmer.rewards!.map(
+          (reward) => ({ target: farmer.address, params: [ctx.address, (reward as Contract).address] }) as const,
+        ),
+      ),
+      // calls: farmers.map((farmer) => ({ target: farmer.address, params: [ctx.address] }) as const),
+      abi: abi.claimable_reward,
+    }),
+    multicall({
+      ctx,
+      calls: farmers.map((farmer) => ({ target: helper.address, params: [farmer.token!] }) as const),
+      abi: abi.totalUnderlyingWithFees,
+    }),
+  ])
+
+  let rewardGlobalIndex = 0
+
+  for (const [index, farmer] of farmers.entries()) {
+    const underlyings = farmer.underlyings as Contract[]
+    const userBalanceRes = userBalancesRes[index]
+    const underlyingBalances = underlyingBalancesRes[index]
+    const totalSupplyRes = totalSuppliesRes[index]
+
+    if (
+      !underlyings ||
+      !userBalanceRes.success ||
+      !underlyingBalances.success ||
+      !totalSupplyRes.success ||
+      totalSupplyRes.output === 0n
+    ) {
+      continue
+    }
+
+    const rewardsMapped: any[] = []
+
+    if (farmer.rewards) {
+      for (const reward of farmer.rewards as Contract[]) {
+        const userPendingReward = userPendingRewardsRes[rewardGlobalIndex]
+
+        if (userPendingReward && userPendingReward.success) {
+          rewardsMapped.push({
+            ...reward,
+            amount: userPendingReward.output,
+          })
+        }
+
+        rewardGlobalIndex++
+      }
+    }
+
+    const [amount0Current, amount1Current] = underlyingBalances.output
+    const underlyings0 = { ...underlyings[0], amount: (userBalanceRes.output * amount0Current) / totalSupplyRes.output }
+    const underlyings1 = { ...underlyings[1], amount: (userBalanceRes.output * amount1Current) / totalSupplyRes.output }
+
+    balances.push({
+      ...farmer,
+      amount: userBalanceRes.output,
+      underlyings: [underlyings0, underlyings1],
+      rewards: rewardsMapped.length ? rewardsMapped : undefined,
       category: 'farm',
     })
   }
