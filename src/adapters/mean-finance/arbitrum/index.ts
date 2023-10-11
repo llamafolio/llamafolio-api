fbb27149b9ab252a907280d1323164adfcb14e5c
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2023-10-11 09:36:33 +0200

    Feat: Adapter, Mean finance (#958)
    * feat: Adapter, mean-finance
    
    * eslint

---
+++ src/adapters/mean-finance/arbitrum/index.ts
@@ -0,0 +1,24 @@
+import { getMeanFinanceBalances } from '@adapters/mean-finance/common/balance'
+import type { Contract, GetBalancesHandler } from '@lib/adapter'
+import { resolveBalances } from '@lib/balance'
+
+const persmissionManager: Contract = {
+  chain: 'arbitrum',
+  address: '0x20bdAE1413659f47416f769a4B27044946bc9923',
+}
+
+export const getContracts = () => {
+  return {
+    contracts: { persmissionManager },
+  }
+}
+
+export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
+  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
+    persmissionManager: getMeanFinanceBalances,
+  })
+
+  return {
+    groups: [{ balances }],
+  }
+}
