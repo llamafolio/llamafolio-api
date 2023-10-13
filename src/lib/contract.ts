a6c03d4f320abc083278a1581a6a9d708b481dda
0xsign <0xsign@protonmail.com>
2023-10-13 11:38:00 +0200

    refactor: rm unused buf functions

--- src/lib/contract.ts
+++ src/lib/contract.ts
@@ -1,1 +1,6 @@
 export const ADDRESS_ZERO: `0x${string}` = '0x0000000000000000000000000000000000000000'
+
+export function isHex(string_: string): boolean {
+  const regexp = /^0x[\dA-Fa-f]{40}$/
+  return regexp.test(string_)
+}
