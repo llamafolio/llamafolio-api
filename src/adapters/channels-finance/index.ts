926e2986248eca7e2c087d50812a8742f64ac43a
0xPeluche <0xpeluche@proton.me>
2023-10-31 10:43:17 +0100

    eslint

--- src/adapters/channels-finance/index.ts
+++ src/adapters/channels-finance/index.ts
@@ -1,7 +1,7 @@
 import type { Adapter } from '@lib/adapter'
 
-import * as bsc from './bsc'
 import * as arbitrum from './arbitrum'
+import * as bsc from './bsc'
 
 const adapter: Adapter = {
   id: 'channels-finance',
