b77dddfd5967ed54c0f8e0bc703d1668964c035e
0xPeluche <0xpeluche@proton.me>
2023-11-03 17:13:38 +0100

    linter

--- src/adapters/dhedge/index.ts
+++ src/adapters/dhedge/index.ts
@@ -1,8 +1,8 @@
 import type { Adapter } from '@lib/adapter'
 
+import * as ethereum from './ethereum'
 import * as optimism from './optimism'
 import * as polygon from './polygon'
-import * as ethereum from './ethereum'
 
 const adapter: Adapter = {
   id: 'dhedge',
