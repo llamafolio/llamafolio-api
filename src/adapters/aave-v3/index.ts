f30591c92cff4095cdef7b1fae531f544971dff7
0xPeluche <0xpeluche@proton.me>
2023-11-13 10:25:45 +0100

    Upadate: Aave, Gnosis chain

--- src/adapters/aave-v3/index.ts
+++ src/adapters/aave-v3/index.ts
@@ -5,6 +5,7 @@ import * as avalanche from './avalanche'
 import * as base from './base'
 import * as ethereum from './ethereum'
 import * as fantom from './fantom'
+import * as gnosis from './gnosis'
 import * as optimism from './optimism'
 import * as polygon from './polygon'
 
@@ -17,6 +18,7 @@ const adapter: Adapter = {
   ethereum,
   optimism,
   base,
+  gnosis,
 }
 
 export default adapter
