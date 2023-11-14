0d4dbbc65610d5f5e1023696600116349f0b2102
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2023-11-14 09:44:35 +0100

    Update, Beefy, Gnosis (#1028)

---
+++ src/adapters/beefy/gnosis/index.ts
@@ -0,0 +1,28 @@
+import { getBeefyFarmBalances } from '@adapters/beefy/common/balance'
+import { getBoostBeefyBalances } from '@adapters/beefy/common/boost'
+import { getBeefyPools } from '@adapters/beefy/common/pool'
+import type { BalancesContext, BaseContext, Contract, GetBalancesHandler } from '@lib/adapter'
+import { resolveBalances } from '@lib/balance'
+
+export const getContracts = async (ctx: BaseContext) => {
+  const pools = await getBeefyPools(ctx)
+
+  return {
+    contracts: { pools },
+    revalidate: 60 * 60,
+  }
+}
+
+async function getBeefyBalances(ctx: BalancesContext, pools: Contract[]) {
+  return Promise.all([getBeefyFarmBalances(ctx, pools), getBoostBeefyBalances(ctx, pools)])
+}
+
+export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
+  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
+    pools: getBeefyBalances,
+  })
+
+  return {
+    groups: [{ balances }],
+  }
+}
