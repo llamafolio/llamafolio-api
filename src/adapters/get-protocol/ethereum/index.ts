1ab8529e23267f839759ab7130962651329bde4c
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2023-11-16 17:54:06 +0100

    feat: Adapter, Get-protocol (#1050)

---
+++ src/adapters/get-protocol/ethereum/index.ts
@@ -0,0 +1,25 @@
+import { getGetProtocolBalance } from '@adapters/get-protocol/common/balance'
+import type { Contract, GetBalancesHandler } from '@lib/adapter'
+import { resolveBalances } from '@lib/balance'
+
+const xGET: Contract = {
+  chain: 'ethereum',
+  address: '0x3e49e9c890cd5b015a18ed76e7a4093f569f1a04',
+  token: '0x8a854288a5976036A725879164Ca3e91d30c6A1B',
+}
+
+export const getContracts = () => {
+  return {
+    contracts: { xGET },
+  }
+}
+
+export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
+  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
+    xGET: getGetProtocolBalance,
+  })
+
+  return {
+    groups: [{ balances }],
+  }
+}
