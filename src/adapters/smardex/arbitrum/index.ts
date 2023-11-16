aa96ebaba67b4b8b6851b06aefe58c65fd665910
0xPeluche <0xpeluche@proton.me>
2023-11-16 19:27:13 +0100

    Update, Smardex bsc polygon base arbitrum

--- src/adapters/smardex/arbitrum/index.ts
+++ src/adapters/smardex/arbitrum/index.ts
@@ -1,56 +1,32 @@
 import { getUserPendingSDEX } from '@adapters/smardex/common/farm'
 import { getSmardexMasterChefPoolsContracts } from '@adapters/smardex/common/pool'
-import { getSmarDexStakeBalances } from '@adapters/smardex/ethereum/stake'
 import type { BaseContext, Contract, GetBalancesHandler } from '@lib/adapter'
 import { resolveBalances } from '@lib/balance'
 import { getMasterChefPoolsBalances } from '@lib/masterchef/masterChefBalance'
 import { getPairsContracts } from '@lib/uniswap/v2/factory'
 import { getPairsBalances } from '@lib/uniswap/v2/pair'
 
-const stakers: Contract[] = [
-  {
-    chain: 'ethereum',
-    address: '0xB940D63c2deD1184BbdE059AcC7fEE93654F02bf',
-    underlyings: ['0x5DE8ab7E27f6E7A1fFf3E5B337584Aa43961BEeF'],
-    decimals: 18,
-    symbol: 'stSDEX',
-  },
-  {
-    chain: 'ethereum',
-    address: '0x80497049b005fd236591c3cd431dbd6e06eb1a31',
-    underlyings: ['0x5DE8ab7E27f6E7A1fFf3E5B337584Aa43961BEeF'],
-    decimals: 18,
-    symbol: 'stSDEX',
-  },
-]
-
 const SDEX: Contract = {
-  chain: 'ethereum',
-  address: '0x5DE8ab7E27f6E7A1fFf3E5B337584Aa43961BEeF',
+  chain: 'arbitrum',
+  address: '0xabd587f2607542723b17f14d00d99b987c29b074',
   decimals: 18,
   symbol: 'SDEX',
 }
 
 const masterChef: Contract = {
-  chain: 'ethereum',
-  address: '0xe74A7a544534DA80fBaC4d2475a6fDc03388154f',
-}
-
-const masterChef2: Contract = {
-  chain: 'ethereum',
-  address: '0x7d85c0905a6e1ab5837a0b57cd94a419d3a77523',
+  chain: 'arbitrum',
+  address: '0x53D165DF0414bD02E91747775450934BF2257f69',
 }
 
 export const getContracts = async (ctx: BaseContext, props: any) => {
   const offset = props.pairOffset || 0
   const limit = 100
 
-  const [pools, pools2, { pairs, allPairsLength }] = await Promise.all([
+  const [pools, { pairs, allPairsLength }] = await Promise.all([
     getSmardexMasterChefPoolsContracts(ctx, masterChef),
-    getSmardexMasterChefPoolsContracts(ctx, masterChef2),
     getPairsContracts({
       ctx,
-      factoryAddress: '0x7753F36E711B66a0350a753aba9F5651BAE76A1D',
+      factoryAddress: '0x41A00e3FbE7F479A99bA6822704d9c5dEB611F22',
       offset,
       limit,
     }),
@@ -59,9 +35,7 @@ export const getContracts = async (ctx: BaseContext, props: any) => {
   return {
     contracts: {
       pools,
-      pools2,
       pairs,
-      stakers,
     },
     revalidate: 60 * 60,
     revalidateProps: {
@@ -72,17 +46,10 @@ export const getContracts = async (ctx: BaseContext, props: any) => {
 
 export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
   const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
-    stakers: getSmarDexStakeBalances,
     pairs: getPairsBalances,
     pools: (...args) =>
       getMasterChefPoolsBalances(...args, {
         masterChefAddress: masterChef.address,
-        rewardToken: SDEX,
-        getUserPendingRewards: (...args) => getUserPendingSDEX(...args),
-      }),
-    pools2: (...args) =>
-      getMasterChefPoolsBalances(...args, {
-        masterChefAddress: masterChef2.address,
         rewardToken: SDEX,
         getUserPendingRewards: (...args) => getUserPendingSDEX(...args),
       }),
--- src/adapters/smardex/base/index.ts
+++ src/adapters/smardex/base/index.ts
@@ -1,56 +1,32 @@
 import { getUserPendingSDEX } from '@adapters/smardex/common/farm'
 import { getSmardexMasterChefPoolsContracts } from '@adapters/smardex/common/pool'
-import { getSmarDexStakeBalances } from '@adapters/smardex/ethereum/stake'
 import type { BaseContext, Contract, GetBalancesHandler } from '@lib/adapter'
 import { resolveBalances } from '@lib/balance'
 import { getMasterChefPoolsBalances } from '@lib/masterchef/masterChefBalance'
 import { getPairsContracts } from '@lib/uniswap/v2/factory'
 import { getPairsBalances } from '@lib/uniswap/v2/pair'
 
-const stakers: Contract[] = [
-  {
-    chain: 'ethereum',
-    address: '0xB940D63c2deD1184BbdE059AcC7fEE93654F02bf',
-    underlyings: ['0x5DE8ab7E27f6E7A1fFf3E5B337584Aa43961BEeF'],
-    decimals: 18,
-    symbol: 'stSDEX',
-  },
-  {
-    chain: 'ethereum',
-    address: '0x80497049b005fd236591c3cd431dbd6e06eb1a31',
-    underlyings: ['0x5DE8ab7E27f6E7A1fFf3E5B337584Aa43961BEeF'],
-    decimals: 18,
-    symbol: 'stSDEX',
-  },
-]
-
 const SDEX: Contract = {
-  chain: 'ethereum',
-  address: '0x5DE8ab7E27f6E7A1fFf3E5B337584Aa43961BEeF',
+  chain: 'base',
+  address: '0xFd4330b0312fdEEC6d4225075b82E00493FF2e3f',
   decimals: 18,
   symbol: 'SDEX',
 }
 
 const masterChef: Contract = {
-  chain: 'ethereum',
-  address: '0xe74A7a544534DA80fBaC4d2475a6fDc03388154f',
-}
-
-const masterChef2: Contract = {
-  chain: 'ethereum',
-  address: '0x7d85c0905a6e1ab5837a0b57cd94a419d3a77523',
+  chain: 'base',
+  address: '0xa5D378c05192E3f1F365D6298921879C4D51c5a3',
 }
 
 export const getContracts = async (ctx: BaseContext, props: any) => {
   const offset = props.pairOffset || 0
   const limit = 100
 
-  const [pools, pools2, { pairs, allPairsLength }] = await Promise.all([
+  const [pools, { pairs, allPairsLength }] = await Promise.all([
     getSmardexMasterChefPoolsContracts(ctx, masterChef),
-    getSmardexMasterChefPoolsContracts(ctx, masterChef2),
     getPairsContracts({
       ctx,
-      factoryAddress: '0x7753F36E711B66a0350a753aba9F5651BAE76A1D',
+      factoryAddress: '0xdd4536dD9636564D891c919416880a3e250f975A',
       offset,
       limit,
     }),
@@ -59,9 +35,7 @@ export const getContracts = async (ctx: BaseContext, props: any) => {
   return {
     contracts: {
       pools,
-      pools2,
       pairs,
-      stakers,
     },
     revalidate: 60 * 60,
     revalidateProps: {
@@ -72,17 +46,10 @@ export const getContracts = async (ctx: BaseContext, props: any) => {
 
 export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
   const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
-    stakers: getSmarDexStakeBalances,
     pairs: getPairsBalances,
     pools: (...args) =>
       getMasterChefPoolsBalances(...args, {
         masterChefAddress: masterChef.address,
-        rewardToken: SDEX,
-        getUserPendingRewards: (...args) => getUserPendingSDEX(...args),
-      }),
-    pools2: (...args) =>
-      getMasterChefPoolsBalances(...args, {
-        masterChefAddress: masterChef2.address,
         rewardToken: SDEX,
         getUserPendingRewards: (...args) => getUserPendingSDEX(...args),
       }),
