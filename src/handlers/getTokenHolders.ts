3692aa4e3e720ee30afc2c358e6efd605e55ff6c
0xsign <99134502+0xsign@users.noreply.github.com>
2023-11-17 17:58:43 +0100

    feat: token holders (#1056)

--- src/handlers/getTokenHolders.ts
+++ src/handlers/getTokenHolders.ts
@@ -1,51 +1,70 @@
-import { selectBalancesHolders } from '@db/balances'
+import { selectTokenHoldersBalances } from '@db/balances'
 import { client } from '@db/clickhouse'
 import { badRequest, serverError, success } from '@handlers/response'
-import type { Chain } from '@lib/chains'
-import { isHex } from '@lib/contract'
+import { chainByChainId, getChainId } from '@lib/chains'
+import { parseAddress } from '@lib/fmt'
+import { mulPrice } from '@lib/math'
 import { getTokenPrice } from '@lib/price'
 import type { Token } from '@lib/token'
 import type { APIGatewayProxyHandler } from 'aws-lambda'
 
-interface IHolder {
+interface Holder {
   address: string
   amount: string
   balanceUSD?: number
 }
 
 interface TokenHoldersResponse {
-  holders: IHolder[]
+  holders: Holder[]
+  count: number
+  next: number
 }
 
 export const handler: APIGatewayProxyHandler = async (event, _context) => {
-  const address = event.pathParameters?.address
-
+  // Token address
+  const address = parseAddress(event.pathParameters?.address || '')
   if (!address) {
-    return badRequest('Missing address parameter')
+    return badRequest('Invalid address parameter')
   }
-  if (!isHex(address)) {
-    return badRequest('Invalid address parameter, expected hex')
+
+  const offset = Math.max(parseInt(event.queryStringParameters?.offset ?? '0'), 0)
+  const limit = Math.min(parseInt(event.queryStringParameters?.limit ?? '100'), 100)
+
+  const chainId = getChainId(event.queryStringParameters?.chain || 'ethereum')
+  if (chainId == null) {
+    return badRequest(`Unknown chain ${event.queryStringParameters?.chain}`)
   }
 
   try {
-    const queries = event.queryStringParameters
-
-    const chain = (queries?.chain || 'ethereum').replace(/['"]/g, '') as Chain
-
-    const limit = queries?.limit ?? '100'
-
-    const limitQuery = parseInt(limit) > 100 ? 100 : parseInt(limit)
-
-    const [balances, tokenPrice] = await Promise.all([
-      selectBalancesHolders(client, address, chain, limitQuery),
-      getTokenPrice({ chain, address } as Token),
+    const [tokenHoldersBalances, tokenPrice] = await Promise.all([
+      selectTokenHoldersBalances(client, address, chainId, limit, offset),
+      getTokenPrice({ chain: chainByChainId[chainId].id, address } as Token),
     ])
 
-    const response: TokenHoldersResponse = {
-      holders: [],
+    let count = 0
+    const holders: Holder[] = []
+
+    for (const row of tokenHoldersBalances) {
+      count = parseInt(row.count)
+
+      holders.push({
+        address: row.holder,
+        amount: row.value,
+        balanceUSD:
+          tokenPrice?.price != null && tokenPrice?.decimals != null
+            ? mulPrice(BigInt(row.value), tokenPrice.decimals, tokenPrice.price)
+            : undefined,
+      })
     }
 
-    return success(response, { maxAge: 10 * 60 })
+    const response: TokenHoldersResponse = {
+      holders,
+      count,
+      next: Math.min(offset + limit, count),
+    }
+
+    return success(response)
+    // return success(response, { maxAge: 10 * 60 })
   } catch (e) {
     console.error('Failed to retrieve token holders', e)
     return serverError('Failed to retrieve token holders')
