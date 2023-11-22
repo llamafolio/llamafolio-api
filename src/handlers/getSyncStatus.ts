376637fb6d3ef769c98dd92b2231203159950b5c
0xsign <0xsign@protonmail.com>
2023-11-22 08:26:45 +0100

    feat: sync status v1 timestamp

--- src/handlers/getSyncStatus.ts
+++ src/handlers/getSyncStatus.ts
@@ -1,5 +1,6 @@
 import { client } from '@db/clickhouse'
 import { serverError, success } from '@handlers/response'
+import { unixFromDateTime } from '@lib/fmt'
 import type { APIGatewayProxyHandler } from 'aws-lambda'
 
 export const handler: APIGatewayProxyHandler = async () => {
@@ -27,16 +28,17 @@ export const handler: APIGatewayProxyHandler = async () => {
 export const handlerV1: APIGatewayProxyHandler = async () => {
   try {
     const lastSyncedBlocksQueryRes = await client.query({
-      query: `SELECT chain, max(number) AS max FROM evm_indexer2.blocks GROUP BY chain;`,
+      query: `SELECT "chain", max("number") AS "number", max("timestamp") AS "timestamp" FROM evm_indexer2.blocks GROUP BY "chain";`,
     })
 
     const lastSyncedBlocksRes = (await lastSyncedBlocksQueryRes.json()) as {
-      data: [{ chain: string; count: string; max: string }]
+      data: [{ chain: string; count: string; number: string; timestamp: string }]
     }
 
     const response = lastSyncedBlocksRes.data.map((row) => ({
       chain: parseInt(row.chain),
-      max_block_number: parseInt(row.max),
+      blockNumber: parseInt(row.number),
+      timestamp: unixFromDateTime(row.timestamp),
     }))
 
     return success(response, { maxAge: 60 })
