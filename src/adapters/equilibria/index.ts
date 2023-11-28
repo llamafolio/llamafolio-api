9f37bee1bd6ab738a5b1f4542c1ace15fe7ae5c5
0xPeluche <0xpeluche@proton.me>
2023-11-28 10:04:25 +0100

    eslint

--- src/adapters/equilibria/index.ts
+++ src/adapters/equilibria/index.ts
@@ -1,8 +1,8 @@
 import type { Adapter } from '@lib/adapter'
 
-import * as ethereum from './ethereum'
 import * as arbitrum from './arbitrum'
 import * as bsc from './bsc'
+import * as ethereum from './ethereum'
 import * as optimism from './optimism'
 
 const adapter: Adapter = {
