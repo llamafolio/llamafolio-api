abbeb05cfe0f243dd4bed0a5df924c1b9ae92ca0
0xPeluche <0xpeluche@proton.me>
2023-11-30 12:01:49 +0100

    Feat:Adapter,Soswap,Base

---
+++ src/adapters/soswap/base/index.ts
@@ -0,0 +1,34 @@
+import type { BaseContext, GetBalancesHandler } from '@lib/adapter'
+import { resolveBalances } from '@lib/balance'
+import { getPairsContracts } from '@lib/uniswap/v2/factory'
+import { getPairsBalances } from '@lib/uniswap/v2/pair'
+
+export const getContracts = async (ctx: BaseContext, props: any) => {
+  const offset = props.pairOffset || 0
+  const limit = 100
+
+  const { pairs, allPairsLength } = await getPairsContracts({
+    ctx,
+    factoryAddress: '0x539db2B4FE8016DB2594d7CfbeAb4d2B730b723E',
+    offset,
+    limit,
+  })
+
+  return {
+    contracts: { pairs },
+    revalidate: 60 * 60,
+    revalidateProps: {
+      pairOffset: Math.min(offset + limit, allPairsLength),
+    },
+  }
+}
+
+export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
+  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
+    pairs: getPairsBalances,
+  })
+
+  return {
+    groups: [{ balances }],
+  }
+}
