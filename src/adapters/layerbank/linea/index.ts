7259c8b32d2e9952fe66d4234ed06d581c92a372
0xPeluche <0xpeluche@proton.me>
2023-11-30 10:58:55 +0100

    Feat:Adapter,LayerBank,Linea

---
+++ src/adapters/layerbank/linea/index.ts
@@ -0,0 +1,40 @@
+import { getLayerBankMarketsBalances } from '@adapters/layerbank/linea/balance'
+import {
+  getCollateralFactor,
+  getLayerBankAllMarkets,
+  getLayerBankMarketsInfos,
+} from '@adapters/layerbank/linea/contract'
+import type { BaseContext, Contract, GetBalancesHandler } from '@lib/adapter'
+import { resolveBalances } from '@lib/balance'
+import { getMarketsContracts } from '@lib/compound/v2/market'
+
+const comptroller: Contract = {
+  chain: 'linea',
+  address: '0x009a0b7c38b542208936f1179151cd08e2943833',
+}
+
+export const getContracts = async (ctx: BaseContext) => {
+  const markets = await getMarketsContracts(ctx, {
+    comptrollerAddress: comptroller.address,
+    getAllMarkets: getLayerBankAllMarkets,
+    getMarketsInfos: getLayerBankMarketsInfos,
+    getCollateralFactor: getCollateralFactor,
+  })
+
+  return {
+    contracts: {
+      markets,
+    },
+    revalidate: 60 * 60,
+  }
+}
+
+export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
+  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
+    markets: getLayerBankMarketsBalances,
+  })
+
+  return {
+    groups: [{ balances }],
+  }
+}
