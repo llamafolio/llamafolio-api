6719975927bb2fa8613710c51a598f97aea05629
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2023-12-07 12:21:06 +0100

    Update,Blur,Stake (#1119)

--- src/adapters/blur/ethereum/index.ts
+++ src/adapters/blur/ethereum/index.ts
@@ -1,38 +1,32 @@
 import type { Contract, GetBalancesHandler } from '@lib/adapter'
 import { resolveBalances } from '@lib/balance'
 import { getSingleStakeBalance } from '@lib/stake'
-import type { Token } from '@lib/token'
 
-const weth: Token = {
+const staker: Contract = {
   chain: 'ethereum',
-  address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-  decimals: 18,
-  symbol: 'WETH',
+  address: '0xec2432a227440139ddf1044c3fea7ae03203933e',
+  token: '0x5283D291DBCF85356A21bA090E6db59121208b44',
 }
 
 const blurPool: Contract = {
   chain: 'ethereum',
   address: '0x0000000000A39bb272e79075ade125fd351887Ac',
-  underlyings: [weth],
-  decimals: 18,
+  token: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
 }
 
 export const getContracts = () => {
   return {
-    contracts: { blurPool },
+    contracts: { blurPool, staker },
   }
 }
 
 export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
   const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
     blurPool: getSingleStakeBalance,
+    staker: getSingleStakeBalance,
   })
 
   return {
-    groups: [
-      {
-        balances: balances.map((balance) => ({ ...balance, symbol: weth.symbol, category: 'stake' })),
-      },
-    ],
+    groups: [{ balances }],
   }
 }
