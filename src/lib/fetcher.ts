75476f6fdbb07dc3a6fccb4bc53ab042350100e7
0xsign <0xsign@protonmail.com>
2023-12-07 22:06:00 +0100

    clean: rm unused graphql schema fetcher

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,19 +1,4 @@
 import { raise } from '@lib/error'
-import type { Json } from '@lib/type'
-
-/** this function fetches the schema of any public GraphQL endpoint */
-export async function fetchGraphQLSchema(url: string): Promise<Json> {
-  const response = await fetcher<Json>(url, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      query:
-        'query IntrospectionQuery { __schema { queryType { name } mutationType { name } subscriptionType { name } types { ...FullType } directives { name description locations args { ...InputValue } } } } fragment FullType on __Type { kind name description fields(includeDeprecated: true) { name description args { ...InputValue } type { ...TypeRef } isDeprecated deprecationReason } inputFields { ...InputValue } interfaces { ...TypeRef } enumValues(includeDeprecated: true) { name description isDeprecated deprecationReason } possibleTypes { ...TypeRef } } fragment InputValue on __InputValue { name description type { ...TypeRef } defaultValue } fragment TypeRef on __Type { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name } } } } } } } }',
-      variables: {},
-    }),
-  })
-  return response
-}
 
 export async function fetcher<T>(url: string, options?: RequestInit) {
   const response = await fetch(url, {
