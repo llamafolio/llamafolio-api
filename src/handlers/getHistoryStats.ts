632de6931b61be2a37f5af845a64c40af5d5214f
0xsign <99134502+0xsign@users.noreply.github.com>
2023-12-18 12:27:55 +0100

    feat: history stats (#1141)

---
+++ src/handlers/getHistoryStats.ts
@@ -0,0 +1,31 @@
+import { client } from '@db/clickhouse'
+import { type HistoryStat, selectHistoryStats } from '@db/history'
+import { badRequest, serverError, success } from '@handlers/response'
+import { parseAddresses } from '@lib/fmt'
+import type { APIGatewayProxyHandler } from 'aws-lambda'
+
+interface HistoryStatsResponse {
+  data: HistoryStat[]
+}
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+  const addresses = parseAddresses(event.pathParameters?.address || '')
+  if (addresses.length === 0) {
+    return badRequest('Invalid address parameter')
+  }
+
+  const year = parseInt(event.queryStringParameters?.year || '') || new Date().getFullYear()
+
+  try {
+    const data = await selectHistoryStats(client, addresses, year)
+
+    const historyStatsResponse: HistoryStatsResponse = {
+      data,
+    }
+
+    return success(historyStatsResponse, { maxAge: 60 * 60, swr: 20 * 60 })
+  } catch (error) {
+    console.error('Failed to retrieve history stats', { error, addresses })
+    return serverError('Failed to retrieve history stats', { error, addresses })
+  }
+}
