e71069040c0f624c7d62ceedc4769a3979b0f322
0xPeluche <0xpeluche@proton.me>
2023-12-21 15:34:42 +0100

    Feat: Adapter, Opium

--- src/adapters/opium/ethereum/balance.ts
+++ src/adapters/opium/ethereum/balance.ts
@@ -1,14 +1,8 @@
 import type { Balance, BalancesContext, Contract } from '@lib/adapter'
 import { getBalancesOf } from '@lib/erc20'
-import { getPricesPerShares } from '@lib/pricePerShare'
-
-const abi = {}
+import { getPricesPerSharesBalances } from '@lib/pricePerShare'
 
 export async function getOpiumBalances(ctx: BalancesContext, pools: Contract[]): Promise<Balance[]> {
-  const [balances, pricePerFullShares] = await Promise.all([
-    getBalancesOf(ctx, pools, { getAddress: (contract) => contract.address }),
-    getPricesPerShares(ctx, pools),
-  ])
-
-  console.log(balances, pricePerFullShares)
+  const balances = await getBalancesOf(ctx, pools, { getAddress: (contract) => contract.address })
+  return getPricesPerSharesBalances(ctx, balances, { getCategory: () => 'stake' })
 }
--- src/lib/pricePerShare.ts
+++ src/lib/pricePerShare.ts
@@ -1,6 +1,7 @@
-import type { BalancesContext, Contract } from '@lib/adapter'
+import type { Balance, BalancesContext, Contract } from '@lib/adapter'
 import { mapMultiSuccessFilter } from '@lib/array'
 import { call } from '@lib/call'
+import type { Category } from '@lib/category'
 import { abi as erc20Abi } from '@lib/erc20'
 import { multicall } from '@lib/multicall'
 
@@ -8,33 +9,71 @@ interface OutputResponse {
   output: bigint
 }
 
+type PricePerShareBalance = Balance & {
+  pricePerShare?: number
+}
+
+interface PricePerShareParams {
+  getAddress?: (contract: Contract) => `0x${string}`
+}
+
+interface GetPricePerShareBalanceParams {
+  getAddress?: (contract: Contract) => `0x${string}`
+  getCategory?: () => Category
+  getPricePerShare?: (params: PricePerShareParams) => Promise<number>
+}
+
+interface GetPricesPerSharesBalancesParams {
+  getAddress?: (contract: Contract) => `0x${string}`
+  getCategory?: () => Category
+  getPricesPerShares?: (params: PricePerShareParams) => Promise<number[]>
+}
+
 export async function getPricePerShare(
   ctx: BalancesContext,
   contract: Contract,
-  params = { getAddress: (contract: Contract) => contract.token },
+  { getAddress = (contract: Contract) => contract.token! }: PricePerShareParams = {},
 ): Promise<number> {
-  const target = params.getAddress(contract) || contract.address
+  const target = getAddress(contract) || contract.address
 
   const [tokenBalance, tokenSupply] = await Promise.all([
     call({ ctx, target, params: [contract.address], abi: erc20Abi.balanceOf }),
     call({ ctx, target: contract.address, abi: erc20Abi.totalSupply }),
   ])
 
+  if (tokenBalance === 0n) return 1
   return Number(tokenBalance) / Number(tokenSupply)
+}
+
+export async function getPricePerShareBalance(
+  ctx: BalancesContext,
+  balance: PricePerShareBalance,
+  params: GetPricePerShareBalanceParams,
+): Promise<Balance> {
+  const _getPricePerShare = params.getPricePerShare || getPricePerShare
+  const _getCategory =
+    typeof params.getCategory === 'function' ? params.getCategory() : params.getCategory || balance.category
+  const pricePerShare = balance.pricePerShare || (await _getPricePerShare(ctx, balance))
+  const rawUnderlying = balance.underlyings?.[0] as Contract
+
+  const assetAmount = BigInt(Math.round(Number(balance.amount) * pricePerShare))
+
+  if (!rawUnderlying) return { ...balance, amount: assetAmount, category: _getCategory }
+  return { ...balance, underlyings: [{ ...rawUnderlying, amount: assetAmount }], category: _getCategory }
 }
 
 export async function getPricesPerShares(
   ctx: BalancesContext,
   contracts: Contract[],
-  params = { getAddress: (contract: Contract) => contract.token },
+  { getAddress = (contract: Contract) => contract.token! }: PricePerShareParams = {},
 ): Promise<number[]> {
   const tokenCalls = contracts.map((contract) => {
-    const target = params.getAddress(contract) || contract.address
-    return { target, params: [contract.address] } as const
+    const target = getAddress(contract) || contract.address
+    return { ctx, target, params: [contract.address], abi: erc20Abi.balanceOf } as const
   })
 
   const suppliesCalls = contracts.map((contract) => {
-    return { target: contract.address } as const
+    return { ctx, target: contract.address, abi: erc20Abi.totalSupply } as const
   })
 
   const [tokenBalances, suppliesBalances] = await Promise.all([
@@ -44,10 +83,33 @@ export async function getPricesPerShares(
 
   return mapMultiSuccessFilter(
     tokenBalances.map((_, i) => [tokenBalances[i], suppliesBalances[i]]),
-
     (res) => {
       const [{ output: balance }, { output: supply }] = res.inputOutputPairs as OutputResponse[]
+
+      if (balance === 0n) return 1
       return Number(balance) / Number(supply)
     },
+  )
+}
+
+export async function getPricesPerSharesBalances(
+  ctx: BalancesContext,
+  balances: PricePerShareBalance[],
+  params: GetPricesPerSharesBalancesParams,
+): Promise<Balance[]> {
+  const _getPricesPerShares = params.getPricesPerShares || getPricesPerShares
+  const pricesPerShares = await _getPricesPerShares(ctx, balances)
+
+  return Promise.all(
+    balances.map(async (balance, index) => {
+      const rawUnderlying = balance.underlyings?.[0] as Contract
+      const pricePerShare = pricesPerShares[index] || balance.pricePerShare
+      const assetAmount = BigInt(Math.round(Number(balance.amount) * pricePerShare!))
+      const _getCategory =
+        typeof params.getCategory === 'function' ? params.getCategory() : params.getCategory || balance.category
+
+      if (!rawUnderlying) return { ...balance, amount: assetAmount, category: _getCategory }
+      return { ...balance, underlyings: [{ ...rawUnderlying, amount: assetAmount }], category: _getCategory }
+    }),
   )
 }
