b6ef700eb6f197d36bd3c67491e4668899a4ba84
0xPeluche <0xpeluche@proton.me>
2023-12-22 12:51:34 +0100

    run build

--- src/adapters/unitus/index.ts
+++ src/adapters/unitus/index.ts
@@ -1,8 +1,8 @@
 import type { Adapter } from '@lib/adapter'
 
 import * as arbitrum from './arbitrum'
-import * as ethereum from './ethereum'
 import * as bsc from './bsc'
+import * as ethereum from './ethereum'
 import * as optimism from './optimism'
 import * as polygon from './polygon'
 
