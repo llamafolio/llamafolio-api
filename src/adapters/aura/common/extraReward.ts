d4b985e12673496cdd4abc0c13f9156af54114c0
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2024-01-08 13:46:32 +0100

    Aura (#1162)
    * Fix Aura rewards
    
    * rm zero balance filter (was only for test)

--- src/adapters/aura/common/extraReward.ts
+++ src/adapters/aura/common/extraReward.ts
@@ -1,4 +1,5 @@
 import type { Balance, BalancesContext, Contract } from '@lib/adapter'
+import { mapSuccessFilter } from '@lib/array'
 import { multicall } from '@lib/multicall'
 
 const abi = {
@@ -9,26 +10,47 @@ const abi = {
     stateMutability: 'view',
     type: 'function',
   },
+  earned: {
+    inputs: [{ internalType: 'address', name: 'account', type: 'address' }],
+    name: 'earned',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
 } as const
 
-export const getExtraRewardsBalances = async (ctx: BalancesContext, poolBalance: Balance[]): Promise<Balance[]> => {
+type AuraBalance = Balance & {
+  rewarders: `0x${string}`[]
+}
+
+export const getExtraRewardsBalances = async (ctx: BalancesContext, poolBalance: AuraBalance[]): Promise<Balance[]> => {
   const balanceWithStandardRewards: Balance[] = poolBalance.filter((poolBalance) => {
-    return !poolBalance.rewards || poolBalance.rewards.length === 0
+    return !poolBalance.rewards || poolBalance.rewarders.length === 0
   })
 
   const balanceWithExtraRewards: Balance[] = poolBalance.filter((poolBalance) => {
-    return poolBalance.rewards && poolBalance.rewards.length > 0
+    return poolBalance.rewards && poolBalance.rewarders.length > 0
   })
 
   const extraRewardsBalancesRes = await multicall({
     ctx,
-    calls: balanceWithExtraRewards.map((pool: Contract) => ({ target: pool.rewarder, params: [ctx.address] }) as const),
-    abi: abi.extraEarned,
+    calls: balanceWithExtraRewards
+      .map((pool: Contract) =>
+        pool.rewarders.map((rewarder: `0x${string}`) => ({ target: rewarder, params: [ctx.address] }) as const),
+      )
+      .flat(),
+    abi: abi.earned,
   })
 
-  balanceWithExtraRewards.forEach((pool, idx) => {
-    const extraRewardsBalances = extraRewardsBalancesRes[idx].success ? extraRewardsBalancesRes[idx].output : 0n
-    pool.rewards = [pool.rewards![0], { ...pool.rewards![1], amount: extraRewardsBalances! }]
+  balanceWithExtraRewards.forEach((pool) => {
+    const rewards = pool.rewards as Contract[]
+    const extraRewards = rewards.slice(1)
+
+    const extraRewardsBalances = mapSuccessFilter(extraRewardsBalancesRes, (res, index) => {
+      return { ...extraRewards[index], amount: res.output }
+    })
+
+    pool.rewards = [pool.rewards![0], ...extraRewardsBalances]
   })
 
   return [...balanceWithExtraRewards, ...balanceWithStandardRewards]
