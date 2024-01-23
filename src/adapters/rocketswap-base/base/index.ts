19f212c941afec5da9bdb7931d84e57e090edc5f
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2024-01-23 10:39:35 +0100

    Feat:Adapter,RocketSwap,Base (#1209)

---
+++ src/adapters/rocketswap-base/base/index.ts
@@ -0,0 +1,36 @@
+import type { BaseContext, GetBalancesHandler } from '@lib/adapter'
+import { resolveBalances } from '@lib/balance'
+import { getPairsContracts } from '@lib/uniswap/v2/factory'
+import { getPairsBalances } from '@lib/uniswap/v2/pair'
+
+export const getContracts = async (ctx: BaseContext, props: any) => {
+  const offset = props.pairOffset || 0
+  const limit = 1000
+
+  const { pairs, allPairsLength } = await getPairsContracts({
+    ctx,
+    factoryAddress: '0x1b8128c3a1b7d20053d10763ff02466ca7ff99fc',
+    offset,
+    limit,
+  })
+
+  return {
+    contracts: {
+      pairs,
+    },
+    revalidate: 60 * 60,
+    revalidateProps: {
+      pairOffset: Math.min(offset + limit, allPairsLength),
+    },
+  }
+}
+
+export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
+  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
+    pairs: getPairsBalances,
+  })
+
+  return {
+    groups: [{ balances }],
+  }
+}
