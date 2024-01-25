c2110a3fb6a03a633186b3453398e9e41d60dc50
0xPeluche <0xpeluche@proton.me>
2024-01-25 11:14:24 +0100

    Fix stkWar address + batch all calls in promise.all

--- src/adapters/paladin-warlord/ethereum/balance.ts
+++ src/adapters/paladin-warlord/ethereum/balance.ts
@@ -1,8 +1,11 @@
 import type { Balance, BalancesContext, BaseBalance, BaseContract, Contract } from '@lib/adapter'
+import { mapSuccessFilter } from '@lib/array'
 import { call } from '@lib/call'
+import type { Category } from '@lib/category'
 import { abi as erc20Abi } from '@lib/erc20'
 import { multicall } from '@lib/multicall'
 import type { Token } from '@lib/token'
+import { isNotNullish } from '@lib/type'
 
 const abi = {
   getUserTotalClaimableRewards: {
@@ -63,77 +66,47 @@ const aura: Token = {
   symbol: 'AURA',
 }
 
-const warAuraLocker: Contract = {
-  chain: 'ethereum',
-  address: '0x7B90e043aaC79AdeA0Dbb0690E3c832757207a3B',
-}
-const warCvxLocker: Contract = {
-  chain: 'ethereum',
-  address: '0x700d6d24A55512c6AEC08820B49da4e4193105B3',
-}
 const warRedeemer: Contract = {
   chain: 'ethereum',
   address: '0x4787Ef084c1d57ED87D58a716d991F8A9CD3828C',
 }
 
-export async function getWarlordBalance(ctx: BalancesContext, war: Contract): Promise<Balance> {
-  const warBalance = await call({
-    ctx,
-    abi: erc20Abi.balanceOf,
-    target: war.address,
-    params: [ctx.address],
-  })
-  const warTotalSupply = await call({
-    ctx,
-    abi: erc20Abi.totalSupply,
-    target: war.address,
-  })
-  const [cvxLocked, auraLocked] = await multicall({
-    ctx,
-    abi: abi.getCurrentLockedTokens,
-    calls: [warCvxLocker.address, warAuraLocker.address].map(
-      (target) =>
-        ({
-          target,
-        }) as const,
-    ),
-  })
-  const [auraQueued, cvxQueued] = await multicall({
-    ctx,
-    abi: abi.queuedForWithdrawal,
-    calls: [aura.address, cvx.address].map(
-      (token) =>
-        ({
-          target: warRedeemer.address,
-          params: [token],
-        }) as const,
-    ),
-  })
+export async function getWarlordBalance(ctx: BalancesContext, war: Contract, lockers: Contract[]): Promise<Balance> {
+  const [warBalance, warTotalSupply, tokensLocked, tokensQueued] = await Promise.all([
+    call({
+      ctx,
+      abi: erc20Abi.balanceOf,
+      target: war.address,
+      params: [ctx.address],
+    }),
+    call({
+      ctx,
+      abi: erc20Abi.totalSupply,
+      target: war.address,
+    }),
+    multicall({
+      ctx,
+      abi: abi.getCurrentLockedTokens,
+      calls: lockers.map((locker) => ({ target: locker.address })),
+    }),
+    multicall({
+      ctx,
+      calls: [aura, cvx].map((token) => ({ target: warRedeemer.address, params: [token.address] }) as const),
+      abi: abi.queuedForWithdrawal,
+    }),
+  ])
 
-  let cvxUserBalanceAmount: bigint = 0n
-  let auraUserBalanceAmount: bigint = 0n
-  if (cvxLocked.success && cvxQueued.success) {
-    cvxUserBalanceAmount = ((cvxLocked.output - cvxQueued.output) * warBalance) / warTotalSupply
-  }
-  if (auraLocked.success && auraQueued.success) {
-    auraUserBalanceAmount = ((auraLocked.output - auraQueued.output) * warBalance) / warTotalSupply
+  const [auraLocked, cvxLocked] = mapSuccessFilter(tokensLocked, (res) => (res.output > 0n ? res.output : 0n))
+  const [auraQueued, cvxQueued] = mapSuccessFilter(tokensQueued, (res) => (res.output > 0n ? res.output : 0n))
+
+  const auraUserBalance: BaseBalance = {
+    ...lockers[0],
+    amount: ((auraLocked - auraQueued) * warBalance) / warTotalSupply,
   }
 
   const cvxUserBalance: BaseBalance = {
-    amount: cvxUserBalanceAmount,
-    token: cvx.address,
-    chain: ctx.chain,
-    address: cvx.address,
-    symbol: cvx.symbol,
-    decimals: cvx.decimals,
-  }
-  const auraUserBalance: BaseBalance = {
-    amount: auraUserBalanceAmount,
-    token: aura.address,
-    chain: ctx.chain,
-    address: aura.address,
-    symbol: aura.symbol,
-    decimals: aura.decimals,
+    ...lockers[1],
+    amount: ((cvxLocked - cvxQueued) * warBalance) / warTotalSupply,
   }
 
   return {
@@ -145,85 +118,70 @@ export async function getWarlordBalance(ctx: BalancesContext, war: Contract): Pr
   }
 }
 
-async function getRewardsForUser(ctx: BalancesContext, stkWAR: Contract): Promise<BaseBalance[]> {
-  const rewards = await call({
+async function getRewardsForUser(ctx: BalancesContext, stkWAR: Contract): Promise<Balance[]> {
+  const rewards = stkWAR.rewards as Contract[]
+
+  const rewardBalances = await call({
     ctx,
     abi: abi.getUserTotalClaimableRewards,
     target: stkWAR.address,
     params: [ctx.address],
   })
 
-  return rewards.map((reward) => ({
-    amount: reward.claimableAmount,
-    claimable: reward.claimableAmount,
-    token: reward.reward,
-    chain: ctx.chain,
-    address: reward.reward,
-    category: 'reward',
-  }))
+  return rewards
+    .map((rewardToken, index) => {
+      const { reward, claimableAmount } = rewardBalances[index]
+      if (reward.toLowerCase() !== rewardToken.address.toLowerCase()) {
+        return null
+      }
+
+      return { ...(rewardToken as Balance), amount: claimableAmount, category: 'reward' as Category }
+    })
+    .filter(isNotNullish)
 }
 
-export async function getStakedWarlordBalance(ctx: BalancesContext, stkWAR: Contract): Promise<Balance> {
-  const stkWARBalance = await call({
-    ctx,
-    abi: erc20Abi.balanceOf,
-    target: stkWAR.address,
-    params: [ctx.address],
-  })
-  const warTotalSupply = await call({
-    ctx,
-    abi: erc20Abi.totalSupply,
-    target: (stkWAR!.underlyings![0] as BaseContract).address,
-  })
-  const [cvxLocked, auraLocked] = await multicall({
-    ctx,
-    abi: abi.getCurrentLockedTokens,
-    calls: [warCvxLocker.address, warAuraLocker.address].map(
-      (target) =>
-        ({
-          target,
-        }) as const,
-    ),
-  })
-  const [auraQueued, cvxQueued] = await multicall({
-    ctx,
-    abi: abi.queuedForWithdrawal,
-    calls: [aura.address, cvx.address].map(
-      (token) =>
-        ({
-          target: warRedeemer.address,
-          params: [token],
-        }) as const,
-    ),
-  })
+export async function getStakedWarlordBalance(
+  ctx: BalancesContext,
+  stkWAR: Contract,
+  lockers: Contract[],
+): Promise<Balance> {
+  const [stkWARBalance, warTotalSupply, tokensLocked, tokensQueued, rewards] = await Promise.all([
+    call({
+      ctx,
+      abi: erc20Abi.balanceOf,
+      target: stkWAR.address,
+      params: [ctx.address],
+    }),
+    call({
+      ctx,
+      abi: erc20Abi.totalSupply,
+      target: (stkWAR!.underlyings![0] as BaseContract).address,
+    }),
+    multicall({
+      ctx,
+      calls: lockers.map((locker) => ({ target: locker.address }) as const),
+      abi: abi.getCurrentLockedTokens,
+    }),
+    multicall({
+      ctx,
+      calls: [aura, cvx].map((token) => ({ target: warRedeemer.address, params: [token.address] }) as const),
+      abi: abi.queuedForWithdrawal,
+    }),
+    getRewardsForUser(ctx, stkWAR),
+  ])
 
-  let cvxUserBalanceAmount: bigint = 0n
-  let auraUserBalanceAmount: bigint = 0n
-  if (cvxLocked.success && cvxQueued.success) {
-    cvxUserBalanceAmount = ((cvxLocked.output - cvxQueued.output) * stkWARBalance) / warTotalSupply
-  }
-  if (auraLocked.success && auraQueued.success) {
-    auraUserBalanceAmount = ((auraLocked.output - auraQueued.output) * stkWARBalance) / warTotalSupply
+  const [auraLocked, cvxLocked] = mapSuccessFilter(tokensLocked, (res) => (res.output > 0n ? res.output : 0n))
+  const [auraQueued, cvxQueued] = mapSuccessFilter(tokensQueued, (res) => (res.output > 0n ? res.output : 0n))
+
+  const auraUserBalance: BaseBalance = {
+    ...lockers[0],
+    amount: ((auraLocked - auraQueued) * stkWARBalance) / warTotalSupply,
   }
 
   const cvxUserBalance: BaseBalance = {
-    amount: cvxUserBalanceAmount,
-    token: cvx.address,
-    chain: ctx.chain,
-    address: cvx.address,
-    symbol: cvx.symbol,
-    decimals: cvx.decimals,
+    ...lockers[1],
+    amount: ((cvxLocked - cvxQueued) * stkWARBalance) / warTotalSupply,
   }
-  const auraUserBalance: BaseBalance = {
-    amount: auraUserBalanceAmount,
-    token: aura.address,
-    chain: ctx.chain,
-    address: aura.address,
-    symbol: aura.symbol,
-    decimals: aura.decimals,
-  }
-
-  const rewards = await getRewardsForUser(ctx, stkWAR)
 
   return {
     ...stkWAR,
