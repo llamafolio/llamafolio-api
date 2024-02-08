187cdae5a6d444e250ea395c522df8af5f752433
0xPeluche <0xpeluche@proton.me>
2024-02-08 16:24:14 +0100

    Fix benqi lock wasnt working anymore

--- src/adapters/swaap/index.ts
+++ src/adapters/swaap/index.ts
@@ -1,8 +1,8 @@
 import type { Adapter } from '@lib/adapter'
 
-import * as polygon from './polygon'
-import * as ethereum from './ethereum'
 import * as arbitrum from './arbitrum'
+import * as ethereum from './ethereum'
+import * as polygon from './polygon'
 
 const adapter: Adapter = {
   id: 'swaap',
