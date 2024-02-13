417da61453ad8fd2bacbd62ea28334cd35c0d0c1
0xPeluche <0xpeluche@proton.me>
2024-02-13 10:17:05 +0100

    feat:Adapter,Unicly,ethereum,dexes

---
+++ src/adapters/unicly/ethereum/index.ts
@@ -0,0 +1,38 @@
+import type { AdapterConfig, BaseContext, GetBalancesHandler } from '@lib/adapter'
+import { resolveBalances } from '@lib/balance'
+import { getPairsContracts } from '@lib/uniswap/v2/factory'
+import { getPairsBalances } from '@lib/uniswap/v2/pair'
+
+export const getContracts = async (ctx: BaseContext, props: any) => {
+  const offset = props.pairOffset || 0
+  const limit = 1000
+
+  const { pairs, allPairsLength } = await getPairsContracts({
+    ctx,
+    factoryAddress: '0xbacc776b231c571a7e6ab7bc2c8a099e07153377',
+    offset,
+    limit,
+  })
+
+  return {
+    contracts: { pairs },
+    revalidate: 60 * 60,
+    revalidateProps: {
+      pairOffset: Math.min(offset + limit, allPairsLength),
+    },
+  }
+}
+
+export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
+  const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
+    pairs: getPairsBalances,
+  })
+
+  return {
+    groups: [{ balances }],
+  }
+}
+
+export const config: AdapterConfig = {
+  startDate: 1617228000,
+}
