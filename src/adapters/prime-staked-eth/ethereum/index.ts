2b751d0444149b7abb123a9d4d20310c448d1bf0
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2024-02-15 15:19:09 +0100

    Prime eth (#1283)
    * Feat:Adapter,Prime-staked-eth, ethereum
    
    * small fix

--- src/adapters/prime-staked-eth/ethereum/index.ts
+++ src/adapters/prime-staked-eth/ethereum/index.ts
@@ -1,24 +1,24 @@
-import { getKelpBalances } from '@adapters/kelp-dao/ethereum/balance'
+import { getPrimeBalances } from '@adapters/prime-staked-eth/ethereum/balance'
 import type { AdapterConfig, Contract, GetBalancesHandler } from '@lib/adapter'
 import { resolveBalances } from '@lib/balance'
 
-const rsETH: Contract = {
+const primeETH: Contract = {
   chain: 'ethereum',
-  address: '0xa1290d69c65a6fe4df752f95823fae25cb99e5a7',
+  address: '0x6ef3D766Dfe02Dc4bF04aAe9122EB9A0Ded25615',
   token: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-  symbol: 'rsETH',
+  symbol: 'primeETH',
   category: 'stake',
 }
 
 export const getContracts = () => {
   return {
-    contracts: { rsETH },
+    contracts: { primeETH },
   }
 }
 
 export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, contracts) => {
   const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
-    rsETH: getKelpBalances,
+    primeETH: getPrimeBalances,
   })
 
   return {
@@ -27,5 +27,5 @@ export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, 
 }
 
 export const config: AdapterConfig = {
-  startDate: 1703116800,
+  startDate: 1707177600,
 }
