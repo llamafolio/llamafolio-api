529a0b5681f1ea48344e14e41f04268cd37d40f9
0xPeluche <0xpeluche@proton.me>
2024-02-29 10:51:35 +0100

    fix_gains_network

--- src/adapters/gains-network/polygon/index.ts
+++ src/adapters/gains-network/polygon/index.ts
@@ -4,7 +4,7 @@ import type { Token } from '@lib/token'
 
 import { getGainsBalances } from '../common/farm'
 import { getGainsLockerBalances } from '../common/locker'
-import { getsgDAIBalances, getsgGNSBalances } from '../common/stake'
+import { getGainsNetworkStakeBalance, getsgGNSBalance } from '../common/stake'
 
 const DAI: Token = {
   chain: 'polygon',
@@ -30,7 +30,7 @@ const locker: Contract = {
   symbol: 'gNFT-DAI',
 }
 
-const sgDAI: Contract = {
+const GNSStaking: Contract = {
   chain: 'polygon',
   address: '0xfb06a737f549eb2512eb6082a808fc7f16c0819d',
   token: '0xe5417af564e4bfda1c483642db72007871397896',
@@ -41,11 +41,16 @@ const sGNS: Contract = {
   chain: 'polygon',
   address: '0x8c74b2256ffb6705f14ada8e86fbd654e0e2beca',
   token: '0xE5417Af564e4bFDA1c483642db72007871397896',
+  rewards: [
+    '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063',
+    '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619',
+    '0x3c499c542cef5e3811e1192ce70d8cc03d5c3359',
+  ],
 }
 
 export const getContracts = () => {
   return {
-    contracts: { gDAI, locker, sgDAI, sGNS },
+    contracts: { gDAI, locker, GNSStaking, sGNS },
   }
 }
 
@@ -53,8 +58,8 @@ export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, 
   const balances = await resolveBalances<typeof getContracts>(ctx, contracts, {
     gDAI: getGainsBalances,
     locker: getGainsLockerBalances,
-    sgDAI: getsgDAIBalances,
-    sGNS: getsgGNSBalances,
+    GNSStaking: getGainsNetworkStakeBalance,
+    sGNS: getsgGNSBalance,
   })
 
   return {
--- src/handlers/getBalances.ts
+++ src/handlers/getBalances.ts
@@ -1,11 +1,8 @@
-import { type LatestProtocolBalances, selectLatestProtocolsBalancesByFromAddresses } from '@db/balances'
+import { selectLatestProtocolsBalancesByFromAddresses, type LatestProtocolBalances } from '@db/balances'
 import { client } from '@db/clickhouse'
 import { badRequest, serverError, success } from '@handlers/response'
-import type { BalancesContext } from '@lib/adapter'
 import { BALANCE_UPDATE_THRESHOLD_SEC } from '@lib/balance'
-import { getRPCClient } from '@lib/chains'
 import { parseAddresses, unixFromDate } from '@lib/fmt'
-import { sendSlackMessage } from '@lib/slack'
 import type { APIGatewayProxyHandler } from 'aws-lambda'
 
 type Status = 'stale' | 'success'
@@ -43,22 +40,22 @@ export const handler: APIGatewayProxyHandler = async (event) => {
   } catch (error) {
     console.error('Failed to retrieve balances', { error, addresses })
 
-    await Promise.all(
-      addresses.map(async (address) => {
-        const balancesContext: BalancesContext = {
-          chain: 'ethereum',
-          adapterId: '',
-          client: getRPCClient({ chain: 'ethereum' }),
-          address,
-        }
+    // await Promise.all(
+    //   addresses.map(async (address) => {
+    //     const balancesContext: BalancesContext = {
+    //       chain: 'ethereum',
+    //       adapterId: '',
+    //       client: getRPCClient({ chain: 'ethereum' }),
+    //       address,
+    //     }
 
-        await sendSlackMessage(balancesContext, {
-          level: 'error',
-          title: 'Failed to retrieve balances',
-          message: (error as any).message,
-        })
-      }),
-    )
+    //     await sendSlackMessage(balancesContext, {
+    //       level: 'error',
+    //       title: 'Failed to retrieve balances',
+    //       message: (error as any).message,
+    //     })
+    //   }),
+    // )
 
     return serverError('Failed to retrieve balances', { error, addresses })
   }
