725e7a7ef7e0f65a15ac94830aade21bc5ce5bf1
0xPeluche <0xpeluche@proton.me>
2024-03-07 15:02:10 +0100

    update Aave-v3 ethereum, add stake

--- src/adapters/aave-v3/ethereum/index.ts
+++ src/adapters/aave-v3/ethereum/index.ts
@@ -1,7 +1,22 @@
+import { getAaveStakeBalances } from '@adapters/aave-v3/common/stake'
 import type { AdapterConfig, BaseContext, Contract, GetBalancesHandler } from '@lib/adapter'
 import { resolveBalances } from '@lib/balance'
 
 import { getLendingPoolBalances, getLendingPoolContracts, getLendingPoolHealthFactor } from '../common/lending'
+
+const balancer_vault: Contract = {
+  chain: 'ethereum',
+  address: '0xba12222222228d8ba445958a75a0704d566bf2c8',
+}
+
+const stkAAVEwstETHBPTv2: Contract = {
+  chain: 'ethereum',
+  address: '0x9eda81c21c273a82be9bbc19b6a6182212068101',
+  token: '0x3de27EFa2F1AA663Ae5D458857e731c129069F29',
+  underlyings: ['0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0', '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9'],
+  poolId: '0x3de27efa2f1aa663ae5d458857e731c129069f29000200000000000000000588',
+  rewards: ['0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9'],
+}
 
 const lendingPool: Contract = {
   name: 'Pool',
@@ -23,6 +38,7 @@ export const getContracts = async (ctx: BaseContext) => {
   return {
     contracts: {
       pools,
+      stkAAVEwstETHBPTv2,
     },
   }
 }
@@ -31,6 +47,7 @@ export const getBalances: GetBalancesHandler<typeof getContracts> = async (ctx, 
   const [balances, healthFactor] = await Promise.all([
     resolveBalances<typeof getContracts>(ctx, contracts, {
       pools: getLendingPoolBalances,
+      stkAAVEwstETHBPTv2: (...args) => getAaveStakeBalances(...args, balancer_vault),
     }),
     getLendingPoolHealthFactor(ctx, lendingPool),
   ])
