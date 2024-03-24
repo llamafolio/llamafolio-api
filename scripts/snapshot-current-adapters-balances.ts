e222204151c40085bf34f4418c7ba3fe4fd9316b
0xsign <0xsign@protonmail.com>
2024-03-24 08:07:16 +0100

    feat: chunks checkpoints

--- scripts/snapshot-current-adapters-balances.ts
+++ scripts/snapshot-current-adapters-balances.ts
@@ -1,30 +1,23 @@
+/* eslint-disable security/detect-non-literal-fs-filename */
 import fs from 'node:fs'
 import path from 'node:path'
 import url from 'node:url'
 
-import { adapterById, adapters } from '@adapters'
+import { adapters } from '@adapters'
 import type { ClickHouseClient } from '@clickhouse/client'
 import { formatBalance } from '@db/balances'
 import { client } from '@db/clickhouse'
 import { getAllContractsInteractions, groupContracts } from '@db/contracts'
 import environment from '@environment'
-import {
-  type Adapter,
-  type AdapterHandler,
-  type Balance,
-  type BalancesContext,
-  type Contract,
-  type PricedBalance,
-  revalidateAllContracts,
-} from '@lib/adapter'
+import type { Adapter, AdapterHandler, Balance, BalancesContext, Contract, PricedBalance } from '@lib/adapter'
 import { groupBy, sliceIntoChunks } from '@lib/array'
 import { fmtBalanceBreakdown, resolveHealthFactor, sanitizeBalances, sanitizePricedBalances } from '@lib/balance'
-import { type Chain, chainById, chains, getRPCClient, type IChainInfo } from '@lib/chains'
+import { chainById, chains, getRPCClient, type IChainInfo } from '@lib/chains'
 import { ADDRESS_ZERO } from '@lib/contract'
 import { toYYYYMMDD, unixFromYYYYMMDD, unixToDateTime } from '@lib/fmt'
 import logger from '@lib/logger'
 import { getPricedBalances } from '@lib/price'
-import { sleep, timeout } from '@lib/promise'
+import { timeout } from '@lib/promise'
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
 
@@ -38,27 +31,95 @@ type AdapterBalance = Balance & {
 
 type AdapterJobStatus = 'success' | 'error' | 'pending'
 
-interface AdapterJob {
-  id: string
-  chain: Chain
+interface JobStatus {
+  // YYYY-MM-DD
+  date: string
   status: AdapterJobStatus
   error?: string
 }
 
-interface JobStatus {
-  // YYYY-MM-DD
-  date: string
-  adapters: AdapterJob[]
-}
-
-function saveSnapshotJobStatus(data: JobStatus) {
-  const src = path.join(__dirname, '..', 'internal', 'current_balances_snapshots', 'status.json')
+function saveSnapshotJobStatus(adapterId: string, chain: string, date: string, data: JobStatus) {
+  const src = path.join(
+    __dirname,
+    '..',
+    'internal',
+    'current_balances_snapshots',
+    adapterId,
+    chain,
+    date,
+    'status.json',
+  )
   return fs.writeFileSync(src, JSON.stringify(data, null, 2), 'utf8')
 }
 
-export function getSnapshotJobStatus() {
+function saveSnapshotJobContracts(
+  adapterId: string,
+  chain: string,
+  date: string,
+  data: { [address: string]: Contract[] },
+) {
+  const src = path.join(
+    __dirname,
+    '..',
+    'internal',
+    'current_balances_snapshots',
+    adapterId,
+    chain,
+    date,
+    'contracts.json',
+  )
+  return fs.writeFileSync(src, JSON.stringify(data, null, 2), 'utf8')
+}
+
+function rmSnapshotJobContracts(adapterId: string, chain: string, date: string) {
   try {
-    const src = path.join(__dirname, '..', 'internal', 'current_balances_snapshots', 'status.json')
+    const src = path.join(
+      __dirname,
+      '..',
+      'internal',
+      'current_balances_snapshots',
+      adapterId,
+      chain,
+      date,
+      'contracts.json',
+    )
+    return fs.unlinkSync(src)
+  } catch (error) {
+    return null
+  }
+}
+
+function getSnapshotJobContracts(adapterId: string, chain: string, date: string) {
+  try {
+    const src = path.join(
+      __dirname,
+      '..',
+      'internal',
+      'current_balances_snapshots',
+      adapterId,
+      chain,
+      date,
+      'contracts.json',
+    )
+    const buff = fs.readFileSync(src, 'utf8')
+    return JSON.parse(buff) as { [address: string]: Contract[] }
+  } catch (error) {
+    return null
+  }
+}
+
+function getSnapshotJobStatus(adapterId: string, chain: string, date: string) {
+  try {
+    const src = path.join(
+      __dirname,
+      '..',
+      'internal',
+      'current_balances_snapshots',
+      adapterId,
+      chain,
+      date,
+      'status.json',
+    )
     const buff = fs.readFileSync(src, 'utf8')
     return JSON.parse(buff) as JobStatus
   } catch (error) {
@@ -92,12 +153,14 @@ async function processAdapter({
   chain,
   today,
   now,
+  allContractsInteractions,
 }: {
   adapter: Adapter
   chainAdapter: AdapterHandler
   chain: IChainInfo
   today: string
   now: Date
+  allContractsInteractions: { [address: string]: Contract[] } | null
 }) {
   const startTime = Date.now()
 
@@ -117,15 +180,20 @@ async function processAdapter({
 
   const cache = new Map<string, any>()
 
-  // Update contracts
-  await revalidateAllContracts(client, adapter, chain.id)
+  // Get contracts interactions
+  if (!allContractsInteractions) {
+    // Update contracts
+    // await revalidateAllContracts(client, adapter, chain.id)
 
-  // Wait for async insert to be processed
-  await sleep(5_000)
+    // Wait for async insert to be processed
+    // await sleep(5_000)
 
-  _logger.info(`Date: ${today}`)
+    _logger.info(`Date: ${today}`)
 
-  const allContractsInteractions = await getAllContractsInteractions(client, adapter.id, chain.chainId)
+    allContractsInteractions = await getAllContractsInteractions(client, adapter.id, chain.chainId)
+
+    saveSnapshotJobContracts(adapter.id, chain.id, today, allContractsInteractions)
+  }
 
   const users: string[] = []
   for (const user in allContractsInteractions) {
@@ -134,103 +202,128 @@ async function processAdapter({
 
   _logger.info(`Updating users: ${users.length}`)
 
-  const balances: AdapterBalance[] = []
   const usersChunks = sliceIntoChunks(users, 100)
 
-  const runAdapter = async (address: `0x${string}`, contracts: Contract[]) => {
-    const ctx: BalancesContext = {
-      chain: chain.id,
-      address,
-      adapterId: adapter.id,
-      failThrough: true,
-      client: rpcClient,
-      cache,
-    }
-
-    const balancesConfig = await chainAdapter.getBalances(ctx, groupContracts(contracts) || [])
-
-    for (let groupIdx = 0; groupIdx < balancesConfig.groups.length; groupIdx++) {
-      const group = balancesConfig.groups[groupIdx]
-      for (const balance of group.balances) {
-        // use token when available
-        balance.address = (balance.token || balance.address).toLowerCase()
-        // metadata
-        balance.groupIdx = groupIdx
-        balance.adapterId = adapter.id
-        balance.timestamp = now
-        balance.healthFactor = group.healthFactor
-        balance.fromAddress = address
-        balance.chain = chain.id
-
-        balances.push(balance)
-      }
-    }
-  }
-
-  let chunkIdx = 0
+  let chunkChunkIdx = 0
 
   // Process users by chunks
-  for await (const users of usersChunks) {
-    _logger.info(`Batch ${chunkIdx} / ${usersChunks.length}`)
+  // Dump data every few chunk to still move forward in case of failure
+  const usersChunksChunks = sliceIntoChunks(usersChunks, 10)
 
-    // Get users balances
+  for (const _usersChunks of usersChunksChunks) {
+    const balances: AdapterBalance[] = []
+
     await Promise.all(
-      users.map((user) => {
-        // skip address zero
-        if (user === ADDRESS_ZERO) {
-          return
-        }
+      _usersChunks.map(async (users, chunkIdx) => {
+        _logger.info(`Batch ${chunkChunkIdx * 10 + chunkIdx} / ${usersChunks.length}`)
 
-        const contractsInteractions = allContractsInteractions[user] || []
+        // Get users balances
+        return Promise.all(
+          users.map((user) => {
+            // skip address zero
+            if (user === ADDRESS_ZERO) {
+              return
+            }
 
-        // 1 min timeout / users chunk
-        return timeout(runAdapter(user as `0x${string}`, contractsInteractions), 60 * 1000)
+            const runAdapter = async () => {
+              const ctx: BalancesContext = {
+                chain: chain.id,
+                address: user,
+                adapterId: adapter.id,
+                failThrough: true,
+                client: rpcClient,
+                cache,
+              }
+
+              const contractsInteractions = allContractsInteractions![user] || []
+
+              const balancesConfig = await chainAdapter.getBalances(ctx, groupContracts(contractsInteractions) || [])
+
+              for (let groupIdx = 0; groupIdx < balancesConfig.groups.length; groupIdx++) {
+                const group = balancesConfig.groups[groupIdx]
+                for (const balance of group.balances) {
+                  // use token when available
+                  balance.address = (balance.token || balance.address).toLowerCase()
+                  // metadata
+                  balance.groupIdx = groupIdx
+                  balance.adapterId = adapter.id
+                  balance.timestamp = now
+                  balance.healthFactor = group.healthFactor
+                  balance.fromAddress = user
+                  balance.chain = chain.id
+
+                  balances.push(balance)
+                }
+              }
+            }
+
+            // 2 min timeout / users chunk
+            return timeout(runAdapter(), 120 * 1000)
+          }),
+        )
       }),
     )
 
-    chunkIdx++
-  }
+    chunkChunkIdx++
 
-  _logger.info(`Active users: ${balances.length}`)
+    _logger.info(`Chunk balances: ${balances.length}`)
 
-  const sanitizedBalances = sanitizeBalances(balances)
+    const sanitizedBalances = sanitizeBalances(balances)
 
-  const pricedBalances = await getPricedBalances(sanitizedBalances)
+    // TODO: cache prices across chunks
+    const pricedBalances = await getPricedBalances(sanitizedBalances)
 
-  const sanitizedPricedBalances = sanitizePricedBalances(pricedBalances as PricedBalance[])
+    const sanitizedPricedBalances = sanitizePricedBalances(pricedBalances as PricedBalance[])
 
-  const dbBalances: any[] = []
+    const dbBalances: any[] = []
 
-  // Group back
-  const balancesByUser = groupBy(sanitizedPricedBalances, 'fromAddress')
+    // Group back
+    const balancesByUser = groupBy(sanitizedPricedBalances, 'fromAddress')
 
-  for (const user in balancesByUser) {
-    const balancesByGroupIdx = groupBy(balancesByUser[user], 'groupIdx')
+    for (const user in balancesByUser) {
+      const balancesByGroupIdx = groupBy(balancesByUser[user], 'groupIdx')
 
-    for (const groupIdx in balancesByGroupIdx) {
-      const balancesWithBreakdown = balancesByGroupIdx[groupIdx].map(fmtBalanceBreakdown)
-      const formattedBalances = balancesWithBreakdown.map(formatBalance)
-      const healthFactor = balancesByGroupIdx[groupIdx]?.[0]?.healthFactor || resolveHealthFactor(formattedBalances)
+      for (const groupIdx in balancesByGroupIdx) {
+        const balancesWithBreakdown = balancesByGroupIdx[groupIdx].map(fmtBalanceBreakdown)
+        const formattedBalances = balancesWithBreakdown.map(formatBalance)
+        const healthFactor = balancesByGroupIdx[groupIdx]?.[0]?.healthFactor || resolveHealthFactor(formattedBalances)
 
-      for (let idx = 0; idx < formattedBalances.length; idx++) {
-        const balance = formattedBalances[idx]
-        dbBalances.push({
-          ...balance,
-          chain: chain.chainId,
-          holder: user.toLowerCase(),
-          adapterId: adapter.id,
-          timestamp: unixToDateTime(unixFromYYYYMMDD(today)),
-          healthFactor,
-          groupIdx,
-          idx,
-          version: chainAdapter.config?.version || 0,
-          sign: 1,
-        })
+        for (let idx = 0; idx < formattedBalances.length; idx++) {
+          const balance = formattedBalances[idx]
+          dbBalances.push({
+            ...balance,
+            chain: chain.chainId,
+            holder: user.toLowerCase(),
+            adapterId: adapter.id,
+            timestamp: unixToDateTime(unixFromYYYYMMDD(today)),
+            healthFactor,
+            groupIdx,
+            idx,
+            version: chainAdapter.config?.version || 0,
+            sign: 1,
+          })
+        }
       }
     }
-  }
 
-  await saveDBBalancesSnapshots(client, chain.chainId, adapter.id, today, chainAdapter.config?.version || 0, dbBalances)
+    await saveDBBalancesSnapshots(
+      client,
+      chain.chainId,
+      adapter.id,
+      today,
+      chainAdapter.config?.version || 0,
+      dbBalances,
+    )
+
+    // remove processed users
+    for (const users of _usersChunks) {
+      for (const user of users) {
+        delete allContractsInteractions![user]
+      }
+    }
+
+    saveSnapshotJobContracts(adapter.id, chain.id, today, allContractsInteractions)
+  }
 
   const endTime = Date.now()
   _logger.info(`Completed in ${endTime - startTime}ms`)
@@ -241,56 +334,67 @@ async function main() {
 
   const now = new Date()
 
-  const adapterJobs: AdapterJob[] = []
-
-  // Default status: do all adapters
   for (const adapter of adapters) {
-    for (const chain of chains) {
-      if (adapter[chain.id]) {
-        adapterJobs.push({
-          chain: chain.id,
-          id: adapter.id,
-          status: 'pending',
-        })
-      }
-    }
-  }
-
-  const outputDir = path.join(__dirname, '..', 'internal', 'current_balances_snapshots')
-  if (!fs.existsSync(outputDir)) {
-    fs.mkdirSync(outputDir, { recursive: true })
-  }
-
-  // Start where process left
-  const snapshotJobStatus = getSnapshotJobStatus()
-  // Merge status
-  if (snapshotJobStatus != null) {
-    for (let i = 0; i < snapshotJobStatus.adapters.length; i++) {
-      adapterJobs[i].status = snapshotJobStatus.adapters[i].status
-    }
-  }
-
-  for await (const adapterJob of adapterJobs) {
-    if (adapterJob.status !== 'pending') {
+    if (adapter.id === 'wallet') {
       continue
     }
 
-    const adapter = adapterById[adapterJob.id]
-    const chainAdapter = adapter?.[adapterJob.chain]
-    if (adapter && chainAdapter && adapter.id !== 'wallet') {
-      try {
-        await processAdapter({ adapter, chainAdapter, chain: chainById[adapterJob.chain], today, now })
-
-        adapterJob.status = 'success'
-      } catch (error) {
-        adapterJob.status = 'error'
-        adapterJob.error = (error as any).message
+    for (const chain of chains) {
+      const chainAdapter = adapter[chain.id]
+      if (!chainAdapter) {
+        continue
       }
 
-      saveSnapshotJobStatus({
-        date: today,
-        adapters: adapterJobs,
-      })
+      // base dir
+      const outputDir = path.join(
+        __dirname,
+        '..',
+        'internal',
+        'current_balances_snapshots',
+        adapter.id,
+        chain.id,
+        today,
+      )
+      if (!fs.existsSync(outputDir)) {
+        fs.mkdirSync(outputDir, { recursive: true })
+      }
+
+      let snapshotJobStatus = getSnapshotJobStatus(adapter.id, chain.id, today)
+      if (!snapshotJobStatus) {
+        snapshotJobStatus = {
+          date: today,
+          status: 'pending',
+        }
+      }
+
+      try {
+        // Eventually restart job where it stopped
+        const allContractsInteractions = getSnapshotJobContracts(adapter.id, chain.id, today)
+
+        // Done
+        if (snapshotJobStatus && snapshotJobStatus.status !== 'pending') {
+          continue
+        }
+
+        await processAdapter({
+          adapter,
+          chainAdapter,
+          chain: chainById[chain.id],
+          today,
+          now,
+          allContractsInteractions,
+        })
+
+        snapshotJobStatus.status = 'success'
+      } catch (error) {
+        snapshotJobStatus.status = 'error'
+        snapshotJobStatus.error = (error as any).message
+      }
+
+      rmSnapshotJobContracts(adapter.id, chain.id, today)
+
+      // snapshot taken for adapter-chain
+      saveSnapshotJobStatus(adapter.id, chain.id, today, snapshotJobStatus)
     }
   }
 }
