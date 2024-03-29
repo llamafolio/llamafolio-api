244314eec9a05bffa7811670b16152d04639e2c9
0xsign <0xsign@protonmail.com>
2024-03-29 14:08:51 +0100

    fix: zod boolean env

--- environment.ts
+++ environment.ts
@@ -13,7 +13,10 @@ export const environmentSchema = z.object({
   DEFILLAMA_LABELS_API_KEY: z.string().optional(),
   DEFILLAMA_PRICE_API_KEY: z.string().optional(),
   ANKR_API_KEY: z.string().optional().default(''),
-  IS_OFFLINE: z.enum(['true', 'false']).transform((value) => value === 'true'),
+  IS_OFFLINE: z
+    .string()
+    .optional()
+    .transform((value) => value === 'true'),
   API_URL: z.string().optional(),
   AWS_GATEWAY_API_ID_DEV: z.string().optional(),
   AWS_GATEWAY_API_ID_PROD: z.string().optional(),
@@ -35,7 +38,10 @@ export const environmentSchema = z.object({
   // Slack
   SLACK_TOKEN: z.string().optional(),
   SLACK_CHANNEL_ID: z.string().optional(),
-  BALANCES_DDB: z.enum(['true', 'false']).transform((value) => value === 'true'),
+  BALANCES_DDB: z
+    .string()
+    .optional()
+    .transform((value) => value === 'true'),
 })
 
 export type Environment = z.infer<typeof environmentSchema>
